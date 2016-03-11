4033dd644b5cafe6f2d42012ceac75eeeb03088f
Markus Uhlin <markus.uhlin@bredband.net>
2016-03-11 08:06:22 +0100

    did in previous commit 	WIN32 one-time initialization
    did in current commit
    	interpreter for config files and themes
    
    doing in future commit
    	library utilities

---
+++ src/interpreter.h
@@ -0,0 +1,33 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+enum setting_type {
+    TYPE_BOOLEAN,
+    TYPE_INTEGER,
+    TYPE_STRING
+};
+
+typedef bool (*Interpreter_vFunc)(const char *);
+typedef int (*Interpreter_instFunc)(const char *, const char *);
+
+struct Interpreter_in {
+    char			*path;
+    char			*line;
+    long int			 line_num;
+    Interpreter_vFunc		 validator_func;
+    Interpreter_instFunc	 install_func;
+};
+
+void Interpreter(const struct Interpreter_in *);
+
+#include "dataClassify.h" /* sw_isspace() */
+
+static SW_INLINE void
+adv_while_isspace(const char **ptr)
+{
+    while (sw_isspace(**ptr)) {
+	(*ptr)++;
+    }
+}
+
+#endif
