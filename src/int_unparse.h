0d5514f223d8ffccb8ffa4ea49b633d7cc4bcc08
Markus Uhlin <markus.uhlin@bredband.net>
2016-03-12 17:03:53 +0100

    int_unparse header initial commit

---
+++ src/int_unparse.h
@@ -0,0 +1,13 @@
+#ifndef INT_UNPARSE_H
+#define INT_UNPARSE_H
+
+/* This is a shared structure definition that is used in both theme.h and userConf.h */
+
+struct integer_unparse_context {
+    char	*setting_name;  /* Name of the setting or item */
+    long int	 lo_limit;      /* Low limit */
+    long int	 hi_limit;      /* High limit */
+    long int	 fallback_default;	/* If the range is exceeded  --  use this value */
+};
+
+#endif
