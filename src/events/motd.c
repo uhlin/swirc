c511fcd97ac5f8a7cefdc60be2c079bafbf108b0
Markus Uhlin <markus.uhlin@bredband.net>
2016-04-11 22:09:14 +0200

    event_motd()

---
+++ src/events/motd.c
@@ -0,0 +1,16 @@
+#include "common.h"
+
+#include "../config.h"
+#include "../irc.h"
+
+#include "motd.h"
+
+void
+event_motd(struct irc_message_compo *compo)
+{
+    if (config_bool_unparse("skip_motd", false)) {
+	return;
+    }
+
+    irc_extract_msg(compo, g_status_window, 1);
+}
---
+++ src/events/motd.h
@@ -0,0 +1,6 @@
+#ifndef MOTD_H
+#define MOTD_H
+
+void event_motd(struct irc_message_compo *);
+
+#endif
