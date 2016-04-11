3343e5463218795a1c906fd8ad07fc7b93e16814
Markus Uhlin <markus.uhlin@bredband.net>
2016-04-11 21:13:44 +0200

    event_error()

---
+++ src/events/error.c
@@ -0,0 +1,31 @@
+/* Copyright (C) 2014, 2016 Markus Uhlin. All rights reserved. */
+
+#include "common.h"
+
+#include "../irc.h"
+#include "../network.h"
+#include "../printtext.h"
+
+#include "error.h"
+
+void
+event_error(struct irc_message_compo *compo)
+{
+    char *cp = &compo->params[0];
+
+    if (*cp == ':') {
+	cp++;
+    }
+
+    if (*cp) {
+	struct printtext_context ctx = {
+	    .window     = g_active_window,
+	    .spec_type  = TYPE_SPEC1,
+	    .include_ts = TRUE,
+	};
+
+	printtext(&ctx, "%s", cp);
+    }
+
+    g_on_air = FALSE;
+}
---
+++ src/events/error.h
@@ -0,0 +1,6 @@
+#ifndef GUARD_ERROR_H
+#define GUARD_ERROR_H
+
+void event_error(struct irc_message_compo *);
+
+#endif
