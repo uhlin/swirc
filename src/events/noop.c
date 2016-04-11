2c7a25aa1440de0a074d3d4dfcd14d2729fc7e1c
Markus Uhlin <markus.uhlin@bredband.net>
2016-04-11 21:14:21 +0200

    event_noop()

---
+++ src/events/noop.c
@@ -0,0 +1,20 @@
+#include "common.h"
+
+#include "../irc.h"
+#include "../printtext.h"
+
+#include "noop.h"
+
+void
+event_noop(struct irc_message_compo *compo)
+{
+    struct printtext_context ctx = {
+	.window     = g_status_window,
+	.spec_type  = TYPE_SPEC3,
+	.include_ts = true,
+    };
+
+    printtext(&ctx, "Got event %s. Currently a noop (aka no operation)", compo->command);
+    printtext(&ctx, "params = %s", compo->params);
+    printtext(&ctx, "prefix = %s", compo->prefix ? compo->prefix : "none");
+}
---
+++ src/events/noop.h
@@ -0,0 +1,6 @@
+#ifndef NOOP_H
+#define NOOP_H
+
+void event_noop(struct irc_message_compo *);
+
+#endif
