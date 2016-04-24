66e81b1e1926b91b9ddce377881e2cc846824d38
Markus Uhlin <markus.uhlin@bredband.net>
2016-04-25 00:00:46 +0200

    add PRINTFLIKE() to function declaration of Strdup_printf

--- src/strdup_printf.h
+++ src/strdup_printf.h
@@ -3,7 +3,7 @@
 
 /*lint -printf(1, Strdup_printf) */
 
-char	*Strdup_printf  (const char *fmt, ...);
+char	*Strdup_printf  (const char *fmt, ...) PRINTFLIKE(1);
 char	*Strdup_vprintf (const char *fmt, va_list);
 
 #endif
