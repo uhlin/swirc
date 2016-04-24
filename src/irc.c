d1868fef060c8390f4784172d03dc006aec31cf9
Markus Uhlin <markus.uhlin@bredband.net>
2016-04-24 23:35:09 +0200

    event 475

--- src/irc.c
+++ src/irc.c
@@ -133,6 +133,7 @@ static struct numeric_events_tag {
     { "433", "ERR_NICKNAMEINUSE",       NO_WINDOW,      0, event_nicknameInUse },
     { "439", "",                        STATUS_WINDOW,  1, NULL },
     { "470", "",                        NO_WINDOW,      0, event_channel_forward },
+    { "475", "ERR_BADCHANNELKEY",       ACTIVE_WINDOW,  2, NULL },
     { "615", "",                        NO_WINDOW,      0, event_whois_modes },
     { "616", "",                        NO_WINDOW,      0, event_whois_host },
     { "671", "",                        NO_WINDOW,      0, event_whois_ssl },
