9a1c69963b3150138ca40b275a417087f6d7d59b
Markus Uhlin <markus.uhlin@bredband.net>
2016-07-29 18:38:28 +0200

    redesign

--- src/commands/msg.c
+++ src/commands/msg.c
@@ -30,13 +30,24 @@
 #include "common.h"
 
 #include "../dataClassify.h"
-#include "../irc.h"
-#include "../network.h"
+#include "../io-loop.h"
 #include "../printtext.h"
 #include "../strHand.h"
-#include "../theme.h"
 
 #include "msg.h"
+
+static void
+PrintAndFree(const char *msg, char *cp)
+{
+    struct printtext_context ptext_ctx = {
+	.window	    = g_active_window,
+	.spec_type  = TYPE_SPEC1_FAILURE,
+	.include_ts = true,
+    };
+
+    printtext(&ptext_ctx, "%s", msg);
+    if (cp) free(cp);
+}
 
 /* usage: /msg <recipient> <message> */
 void
@@ -45,40 +56,28 @@ cmd_msg(const char *data)
     char *dcopy = sw_strdup(data);
     char *recipient, *message;
     char *state = "";
-    struct printtext_context ctx = {
-	.window	    = g_active_window,
-	.spec_type  = TYPE_SPEC1_FAILURE,
-	.include_ts = true,
-    };
 
     if (Strings_match(dcopy, "")
 	|| Strfeed(dcopy, 1) != 1
 	|| (recipient = strtok_r(dcopy, "\n", &state)) == NULL
 	|| (message = strtok_r(NULL, "\n", &state)) == NULL) {
-	printtext(&ctx, "/msg: missing arguments");
-	free(dcopy);
+	PrintAndFree("/msg: missing arguments", dcopy);
 	return;
     } else if (!is_valid_nickname(recipient) && !is_irc_channel(recipient)) {
-	printtext(&ctx, "/msg: neither a nickname or irc channel");
-	free(dcopy);
+	PrintAndFree("/msg: neither a nickname or irc channel", dcopy);
+	return;
+    } else if (window_by_label(recipient) == NULL && is_valid_nickname(recipient)) {
+	if (spawn_chat_window(recipient, "") != 0) {
+	    PrintAndFree("/msg: fatal: cannot spawn chat window!", dcopy);
+	    return;
+	}
+
+	transmit_user_input(recipient, message);
+    } else if (window_by_label(recipient) == NULL && is_irc_channel(recipient)) {
+	PrintAndFree("/msg: not on that channel", dcopy);
 	return;
     } else {
-	if (window_by_label(recipient) == NULL && spawn_chat_window(recipient, "") != 0) {
-	    printtext(&ctx, "/msg: fatal: cannot spawn chat window!");
-	    free(dcopy);
-	    return;
-	} else {
-	    ctx.window	  = window_by_label(recipient);
-	    ctx.spec_type = TYPE_SPEC_NONE;
-	    if (net_send("PRIVMSG %s :%s", recipient, message) < 0)
-		g_on_air = false;
-	    else
-		printtext(&ctx, "%s%s%s%c%s %s",
-			  Theme("nick_s1"),
-			  COLOR1, g_my_nickname, NORMAL,
-			  Theme("nick_s2"),
-			  message);
-	    free(dcopy);
-	}
+	transmit_user_input(recipient, message);
+	free(dcopy);
     }
 }
