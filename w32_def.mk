193ea9aa95f9e405082d3eca21c53d3edf5da8fd
Markus Uhlin <markus.uhlin@bredband.net>
2016-09-20 22:14:20 +0200

    define NDEBUG=1

--- w32_def.mk
+++ w32_def.mk
@@ -3,6 +3,6 @@ Q=@
 RM=@del /q
 TEMPFILES=*.obj
 CC=cl
-CFLAGS=-DPDC_EXP_EXTRAS=1 -DWIN32=1 -DWIN32_LEAN_AND_MEAN=1 -MD -W3 -nologo
+CFLAGS=-DNDEBUG=1 -DPDC_EXP_EXTRAS=1 -DWIN32=1 -DWIN32_LEAN_AND_MEAN=1 -MD -W3 -nologo
 LDFLAGS=
 LDLIBS=libcrypto-37.lib libssl-38.lib pdcurses.lib ws2_32.lib
