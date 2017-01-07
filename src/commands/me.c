b312fdb28d015b9494189f1903e0bb934dc78487
Markus Uhlin <markus.uhlin@bredband.net>
2017-01-07 15:34:01 +0100

    80 columns

--- src/commands/me.c
+++ src/commands/me.c
@@ -19,12 +19,14 @@ cmd_me(const char *data)
 
     if (Strings_match(data, "")) {
 	printtext(&ctx, "/me: missing arguments");
-    } else if (Strings_match_ignore_case(g_active_window->label, g_status_window_label)) {
+    } else if (Strings_match_ignore_case(g_active_window->label,
+					 g_status_window_label)) {
 	printtext(&ctx, "/me: cannot send to status window!");
     } else {
 	ctx.spec_type = TYPE_SPEC_NONE;
 
-	if (net_send("PRIVMSG %s :\001ACTION %s\001", g_active_window->label, data) < 0)
+	if (net_send("PRIVMSG %s :\001ACTION %s\001",
+	    g_active_window->label, data) < 0)
 	    g_on_air = false;
 	else
 	    printtext(&ctx, " - %s %s", g_my_nickname, data);
