bf59eb97b02c275c3c7f5db9dd9311efed7e4c01
Markus Uhlin <markus.uhlin@bredband.net>
2017-05-11 20:00:51 +0200

    handle leading colon in g_user_modes

--- src/statusbar.c
+++ src/statusbar.c
@@ -1,5 +1,5 @@
 /* Swirc statusbar
-   Copyright (C) 2012-2014, 2016 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2017 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -94,7 +94,7 @@ get_nick_and_server()
     if (g_my_nickname && g_server_hostname) {
 	(void) sw_strcpy(buf, g_my_nickname, sizeof buf);
 	(void) sw_strcat(buf, "(", sizeof buf);
-	(void) sw_strcat(buf, g_user_modes, sizeof buf);
+	(void) sw_strcat(buf, g_user_modes[0] == ':' ? &g_user_modes[1] : &g_user_modes[0], sizeof buf);
 	(void) sw_strcat(buf, ")", sizeof buf);
 	(void) sw_strcat(buf, "@", sizeof buf);
 	(void) sw_strcat(buf, g_server_hostname, sizeof buf);
