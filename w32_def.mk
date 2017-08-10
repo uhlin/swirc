770533523c9803778ca479dd68f65a2373594ae8
Markus Uhlin <markus.uhlin@bredband.net>
2017-08-10 16:20:06 +0200

    user32.lib

--- w32_def.mk
+++ w32_def.mk
@@ -7,6 +7,6 @@ CFLAGS=-DNDEBUG=1 -DPDC_EXP_EXTRAS=1 -DWIN32=1 -DWIN32_LEAN_AND_MEAN=1 -MT -W3 -
 LDFLAGS=
 NAME_libcrypto=libcrypto-41
 NAME_libssl=libssl-43
-LDLIBS=$(NAME_libcrypto).lib $(NAME_libssl).lib libcurl.lib pdcurses.lib ws2_32.lib
+LDLIBS=$(NAME_libcrypto).lib $(NAME_libssl).lib libcurl.lib pdcurses.lib user32.lib ws2_32.lib
 CURL_VERSION=7.54.1
 LIBRESSL_VERSION=2.5.4
