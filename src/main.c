ef9c330d40f291f9b65f7743470bf9d46026bb94
Markus Uhlin <markus.uhlin@bredband.net>
2017-09-11 13:47:56 +0200

    temp version

--- src/main.c
+++ src/main.c
@@ -69,7 +69,7 @@
 /* Things with external linkage
    ============================ */
 
-const char g_swircVersion[] = "v1.7";
+const char g_swircVersion[] = "v1.8-dev";
 const char g_swircYear[]    = "2012-2017";
 const char g_swircAuthor[]  = "Markus Uhlin";
 const char g_swircWebAddr[] = "https://dataswamp.org/~markus/swirc/";
