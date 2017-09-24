0c4ff1073e8f8657ec086d96a5ccd56816b1ab40
Markus Uhlin <markus.uhlin@bredband.net>
2017-09-24 15:24:19 +0200

    ircv3 client capability negotiation

---
+++ src/events/cap.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+#include "../irc.h"
+
+#include "cap.h"
+
+void
+event_cap(struct irc_message_compo *compo)
+{
+    (void) compo;
+}
---
+++ src/events/cap.h
@@ -0,0 +1,6 @@
+#ifndef CAP_H
+#define CAP_H
+
+void event_cap(struct irc_message_compo *);
+
+#endif
