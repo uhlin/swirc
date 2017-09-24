5b00978966d7501e6094173e53aaa96a56af76dd
Markus Uhlin <markus.uhlin@bredband.net>
2017-09-24 15:18:11 +0200

    event_authenticate()

---
+++ src/events/auth.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+#include "../irc.h"
+
+#include "auth.h"
+
+void
+event_authenticate(struct irc_message_compo *compo)
+{
+    (void) compo;
+}
---
+++ src/events/auth.h
@@ -0,0 +1,6 @@
+#ifndef AUTH_H
+#define AUTH_H
+
+void event_authenticate(struct irc_message_compo *);
+
+#endif
