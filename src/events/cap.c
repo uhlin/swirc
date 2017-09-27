e97543b1d798165cee03000f392c87f59a2727ff
Markus Uhlin <markus.uhlin@bredband.net>
2017-09-27 22:51:38 +0200

    new functions

--- src/events/cap.c
+++ src/events/cap.c
@@ -1,8 +1,24 @@
 #include "common.h"
 
+#include "../config.h"
 #include "../irc.h"
+#include "../strHand.h"
 
 #include "cap.h"
+
+bool
+is_sasl_mechanism_supported(const char *mechanism)
+{
+    return (mechanism && Strings_match(mechanism, "PLAIN"));
+}
+
+const char *
+get_sasl_mechanism(void)
+{
+    const char *mechanism = Config("sasl_mechanism");
+
+    return (Strings_match(mechanism, "") ? "PLAIN" : mechanism);
+}
 
 void
 event_cap(struct irc_message_compo *compo)
--- src/events/cap.h
+++ src/events/cap.h
@@ -1,6 +1,8 @@
 #ifndef CAP_H
 #define CAP_H
 
-void event_cap(struct irc_message_compo *);
+bool		 is_sasl_mechanism_supported(const char *mechanism);
+const char	*get_sasl_mechanism(void);
+void		 event_cap(struct irc_message_compo *);
 
 #endif
