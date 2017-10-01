8ca2efd9af1cd3b912b1fede6c2fd9ac9486651f
Markus Uhlin <markus.uhlin@bredband.net>
2017-10-01 20:45:43 +0200

    handle_sasl_auth_fail()

--- src/events/auth.c
+++ src/events/auth.c
@@ -59,3 +59,15 @@ event_authenticate(struct irc_message_compo *compo)
 	free(msg);
     }
 }
+
+/* Examples:
+     :server 902 <nick> :You must use a nick assigned to you
+     :server 904 <nick> :SASL authentication failed
+     :server 905 <nick> :SASL message too long
+     :server 907 <nick> :You have already authenticated using SASL */
+void
+handle_sasl_auth_fail(struct irc_message_compo *compo)
+{
+    irc_extract_msg(compo, g_status_window, 1, true);
+    abort_authentication();
+}
--- src/events/auth.h
+++ src/events/auth.h
@@ -2,5 +2,6 @@
 #define AUTH_H
 
 void event_authenticate(struct irc_message_compo *);
+void handle_sasl_auth_fail(struct irc_message_compo *);
 
 #endif
