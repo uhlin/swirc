b932c225b43062b1fb3acbe4f0b093f48cbd7563
Markus Uhlin <markus.uhlin@bredband.net>
2017-11-04 20:31:53 +0100

    MACHINE=x64

--- w32_def.mk
+++ w32_def.mk
@@ -5,6 +5,7 @@ TEMPFILES=*.obj
 CC=cl
 CFLAGS=-DNDEBUG=1 -DPDC_EXP_EXTRAS=1 -DWIN32=1 -DWIN32_LEAN_AND_MEAN=1 -MT -W3 -nologo
 LDFLAGS=
+MACHINE=x64
 NAME_libcrypto=libcrypto-41
 NAME_libssl=libssl-43
 LDLIBS=$(NAME_libcrypto).lib $(NAME_libssl).lib libcurl.lib pdcurses.lib user32.lib ws2_32.lib
