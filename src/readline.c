f2c3231ff871d671d9ed9c5f2fb9e1aca5cf6e90
Markus Uhlin <markus.uhlin@bredband.net>
2018-01-28 00:15:13 +0100

    disable code

--- src/readline.c
+++ src/readline.c
@@ -1,5 +1,5 @@
 /* Read user input
-   Copyright (C) 2012-2017 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2018 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -417,6 +417,7 @@ handle_key(volatile struct readline_session_context *ctx, wint_t wc)
  * @param buf In data
  * @return The converted part (dynamically allocated)
  */
+#if 0
 static char *
 finalize_out_string(const wchar_t *buf)
 {
@@ -441,6 +442,7 @@ finalize_out_string(const wchar_t *buf)
 
     return (out);
 }
+#endif
 
 /**
  * Apply window-options to the readline panels.
