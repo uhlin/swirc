f8b10f0ee02346b5b17d85e7f6adbb14415f3a54
Markus Uhlin <markus.uhlin@bredband.net>
2018-05-08 18:05:00 +0200

    update definitions

--- w32_def.mk
+++ w32_def.mk
@@ -1,26 +1,34 @@
-E=@echo
-Q=@
-RM=@del /q
-TEMPFILES=*.obj
 CC=cl
-CFLAGS=\
-	-DNDEBUG=1\
+CFLAGS=-DNDEBUG=1\
 	-DPDC_EXP_EXTRAS=1\
 	-DWIN32=1\
 	-DWIN32_LEAN_AND_MEAN=1\
 	-MD\
 	-W3\
 	-nologo
-LDFLAGS=
+
+# Versions
+CURL_VERSION=7.56.1
+LIBRESSL_VERSION=2.6.3
+PDCURSES_VERSION=3.4
+
+# E and Q
+E=@echo
+Q=@
+
 MACHINE=x64
 NAME_libcrypto=crypto-42
 NAME_libssl=ssl-44
-LDLIBS=\
-	$(NAME_libcrypto).lib\
+
+LDFLAGS=-LIBPATH:curl-$(CURL_VERSION)/$(MACHINE)\
+	-LIBPATH:libressl-$(LIBRESSL_VERSION)-windows/$(MACHINE)\
+	-LIBPATH:pdcurses-$(PDCURSES_VERSION)/$(MACHINE)
+LDLIBS=$(NAME_libcrypto).lib\
 	$(NAME_libssl).lib\
 	libcurl.lib\
 	pdcurses.lib\
 	user32.lib\
 	ws2_32.lib
-CURL_VERSION=7.56.1
-LIBRESSL_VERSION=2.6.3
+
+RM=@del /q
+# TEMPFILES=*.obj
