a8d5b21f165bb22d4cf1884dc593ffcb29dac9d4
Markus Uhlin <markus.uhlin@bredband.net>
2018-07-22 21:12:35 +0200

    move ifdef __cplusplus

--- src/options.h
+++ src/options.h
@@ -1,6 +1,10 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 enum {
     UNRECOGNIZED_OPTION = '?',
     OPTION_ARG_MISSING  = ':'
@@ -9,10 +13,6 @@ enum {
 extern int	 g_option_index;
 extern int	 g_option_save;
 extern char	*g_option_arg;
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 int options(int argc, char *argv[], const char *optstring);
 
