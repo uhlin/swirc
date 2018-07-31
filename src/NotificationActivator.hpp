c27d1e81f2363c5a1182fdc8ff0d464a82519194
Markus Uhlin <markus.uhlin@bredband.net>
2018-07-31 15:58:46 +0200

    NotificationActivator header file

---
+++ src/NotificationActivator.hpp
@@ -0,0 +1,40 @@
+#ifndef NOTIFICATION_ACTIVATOR_HPP
+#define NOTIFICATION_ACTIVATOR_HPP
+
+#include "ToastActivator.h"
+
+using namespace ABI::Windows::Data::Xml::Dom;
+using namespace Microsoft::WRL::Wrappers;
+using namespace Microsoft::WRL;
+
+/**
+ * Implement a handler for toast activation, so that when the user
+ * clicks on your toast, your app can do something. (The UUID CLSID
+ * must be unique to your app.)
+ */
+class
+    DECLSPEC_UUID("62337340-CB78-4AE9-A524-685424C52DC7") NotificationActivator
+    WrlSealed
+    WrlFinal : public
+    RuntimeClass<RuntimeClassFlags<ClassicCom>, INotificationActivationCallback>
+{
+public:
+    virtual HRESULT STDMETHODCALLTYPE Activate(
+	_In_ LPCWSTR appUserModelId,
+	_In_ LPCWSTR invokedArgs,
+	_In_reads_(dataCount) const NOTIFICATION_USER_INPUT_DATA* data,
+	ULONG dataCount) override
+    {
+	/*
+	 * TODO: Handle activation
+	 */
+	return S_OK;
+    }
+};
+
+/*
+ * Flag class as COM creatable
+ */
+CoCreatableClass(NotificationActivator);
+
+#endif
