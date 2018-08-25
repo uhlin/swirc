441f32388ee476ff188abb5ff1370ad7c8a78fce
Markus Uhlin <markus.uhlin@bredband.net>
2018-08-25 21:31:58 +0200

    new OBJS

--- src/build.w32.mk
+++ src/build.w32.mk
@@ -4,6 +4,7 @@ TGTS=$(TGTS) swirc.exe
 OBJS=$(OBJS)\
 	$(SRC_DIR)DesktopNotificationManagerCompat.obj\
 	$(SRC_DIR)DesktopToastsApp.obj\
+	$(SRC_DIR)SpawnMessageLoop.obj\
 	$(SRC_DIR)assertAPI.obj\
 	$(SRC_DIR)b64_decode.obj\
 	$(SRC_DIR)b64_encode.obj\
