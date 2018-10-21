e40713a0e5660d70e6108ca85fe576064feb40ca
Markus Uhlin <markus.uhlin@bredband.net>
2018-10-21 15:43:49 +0200

    ifdef __cplusplus

--- src/filePred.h
+++ src/filePred.h
@@ -1,9 +1,17 @@
 #ifndef FILE_PREDICATES_H
 #define FILE_PREDICATES_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool	file_exists     (const char *path);
 bool	is_directory    (const char *path);
 bool	is_device       (const char *path);
 bool	is_regular_file (const char *path);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
