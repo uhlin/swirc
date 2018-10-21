e6fe3f422c1343effcc108a4b1d1f1e460f2d726
Markus Uhlin <markus.uhlin@bredband.net>
2018-10-21 15:43:01 +0200

    add code to event_account()

--- src/events/account.cpp
+++ src/events/account.cpp
@@ -1,8 +1,13 @@
 #include "common.h"
 
+#include "../dataClassify.h"
 #include "../irc.h"
+#include "../printtext.h"
+#include "../strHand.h"
+#include "../theme.h"
 
 #include "account.h"
+#include "names.h"
 
 /* event_account
 
@@ -12,5 +17,45 @@
 void
 event_account(struct irc_message_compo *compo)
 {
-    (void) compo;
+    char *accountname = & (compo->params[0]);
+    char *last = (char *) "";
+    char *nick, *user, *host;
+    char *prefix = & (compo->prefix[1]);
+    struct printtext_context ctx(NULL, TYPE_SPEC1_SPEC2, true);
+
+    if ((nick = strtok_r(prefix, "!@", &last)) == NULL ||
+	(user = strtok_r(NULL, "!@", &last)) == NULL ||
+	(host = strtok_r(NULL, "!@", &last)) == NULL)
+	return;
+
+    if (*accountname == ':')
+	accountname++;
+
+    const bool logged_out = strings_match(accountname, "*");
+
+    for (int i = 1; i <= g_ntotal_windows; i++) {
+	PIRC_WINDOW window = window_by_refnum(i);
+
+	if (window && is_irc_channel(window->label) &&
+	    event_names_htbl_lookup(nick, window->label) != NULL) {
+	    ctx.window = window;
+
+	    if (logged_out) {
+		printtext(&ctx, "%s%s%c %s%s@%s%s has logged out "
+		    "of their account",
+		    COLOR2, nick, NORMAL,
+		    LEFT_BRKT, user, host, RIGHT_BRKT);
+	    } else {
+		/**
+		 * Logged in
+		 */
+
+		printtext(&ctx, "%s%s%c %s%s@%s%s has logged into "
+		    "a new account %s%s%c",
+		    COLOR1, nick, NORMAL,
+		    LEFT_BRKT, user, host, RIGHT_BRKT,
+		    COLOR4, accountname, NORMAL);
+	    }
+	}
+    }
 }
