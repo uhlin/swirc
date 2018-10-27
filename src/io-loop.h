2f4678582dfdfc14c040734b205f2c6888af29e6
Markus Uhlin <markus.uhlin@bredband.net>
2018-10-27 18:46:18 +0200

    update header

--- src/io-loop.h
+++ src/io-loop.h
@@ -5,17 +5,17 @@
 
 typedef void (*CMD_HANDLER_FN)(const char *);
 
-extern wchar_t g_push_back_buf[2705];
-extern bool g_io_loop;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-char *get_prompt          (void);
-void  cmd_help            (const char *data);
-void  enter_io_loop       (void);
-void  transmit_user_input (const char *win_label, const char *input);
+extern wchar_t g_push_back_buf[2705];
+extern bool g_io_loop;
+
+char	*get_prompt(void);
+void	 cmd_help(const char *);
+void	 enter_io_loop(void);
+void	 transmit_user_input(const char *, const char *input);
 
 #ifdef __cplusplus
 }
