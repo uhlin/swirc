8c81eb384d350839b5c78ae73812b8454f86d927
Markus Uhlin <markus.uhlin@bredband.net>
2018-10-27 17:22:38 +0200

    80 columns

--- src/config.h
+++ src/config.h
@@ -13,20 +13,20 @@ typedef struct tagCONF_HTBL_ENTRY {
     struct tagCONF_HTBL_ENTRY *next;
 } CONF_HTBL_ENTRY, *PCONF_HTBL_ENTRY;
 
-void config_init(void);
-void config_deinit(void);
+void	config_init(void);
+void	config_deinit(void);
 
 /*lint -sem(Config_mod, r_null) */
 
-bool		 config_bool_unparse    (const char *setting_name, bool fallback_default);
-char		*Config_mod             (const char *setting_name);
-const char	*Config                 (const char *setting_name);
-int		 config_item_install    (const char *name, const char *value);
-int		 config_item_undef      (const char *name);
-long int	 config_integer_unparse (struct integer_unparse_context *);
-void		 config_create          (const char *path, const char *mode);
-void		 config_do_save         (const char *path, const char *mode);
-void		 config_readit          (const char *path, const char *mode);
+bool		 config_bool_unparse(const char *, bool);
+char		*Config_mod(const char *);
+const char	*Config(const char *);
+int		 config_item_install(const char *name, const char *value);
+int		 config_item_undef(const char *name);
+long int	 config_integer_unparse(struct integer_unparse_context *);
+void		 config_create(const char *path, const char *mode);
+void		 config_do_save(const char *path, const char *mode);
+void		 config_readit(const char *path, const char *mode);
 
 void cmd_set(const char *);
 
