1a1ca7b2da78cbb05591e83d973efd44b9f60d0a
Markus Uhlin <markus.uhlin@bredband.net>
2018-10-28 01:58:34 +0200

    update header

--- src/commands/connect.h
+++ src/commands/connect.h
@@ -1,12 +1,12 @@
 #ifndef CMD_CONNECT_H
 #define CMD_CONNECT_H
 
-void do_connect     (const char *server, const char *port);
-void set_ssl_on     (void);
-void set_ssl_off    (void);
-bool is_ssl_enabled (void);
+void	do_connect(const char *server, const char *port);
+void	set_ssl_on(void);
+void	set_ssl_off(void);
+bool	is_ssl_enabled(void);
 
-void cmd_connect    (const char *);
-void cmd_disconnect (const char *);
+void	cmd_connect(const char *);
+void	cmd_disconnect(const char *);
 
 #endif
