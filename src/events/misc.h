976a40be211dd20481a2eb3272c85da1efd04689
Markus Uhlin <markus.uhlin@bredband.net>
2018-11-07 00:28:28 +0100

    ifdef __cplusplus

--- src/events/misc.h
+++ src/events/misc.h
@@ -1,6 +1,10 @@
 #ifndef MISC_H
 #define MISC_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void event_allaround_extract_find_colon   (struct irc_message_compo *);
 void event_allaround_extract_remove_colon (struct irc_message_compo *);
 void event_bounce                         (struct irc_message_compo *);
@@ -11,5 +15,9 @@ void event_local_and_global_users         (struct irc_message_compo *);
 void event_nicknameInUse                  (struct irc_message_compo *);
 void event_userModeIs                     (struct irc_message_compo *);
 void event_youAreOper                     (struct irc_message_compo *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- src/events/welcome-unix.h
+++ src/events/welcome-unix.h
@@ -1,9 +1,17 @@
 #ifndef WELCOME_UNIX_H
 #define WELCOME_UNIX_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool event_welcome_is_signaled  (void);
 void event_welcome_cond_init    (void);
 void event_welcome_cond_destroy (void);
 void event_welcome_signalit     (void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
