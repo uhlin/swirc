d383bf40bc2a2883a4762cc8e8083f93abdb24c0
Markus Uhlin <markus.uhlin@bredband.net>
2018-11-11 20:33:19 +0100

    Curl 7.62.0 and LibreSSL 2.8.2

--- src/get_file.js
+++ src/get_file.js
@@ -13,7 +13,7 @@ function get_file(url, name)
     stream.SaveToFile(name);
 }
 
-get_file("http://nifty-networks.net/swirc/curl-7.60.0.cab", "curl-7.60.0.cab");
-get_file("http://nifty-networks.net/swirc/libressl-2.7.3-windows.cab", "libressl-2.7.3-windows.cab");
+get_file("http://nifty-networks.net/swirc/curl-7.62.0.cab", "curl-7.62.0.cab");
+get_file("http://nifty-networks.net/swirc/libressl-2.8.2-windows.cab", "libressl-2.8.2-windows.cab");
 get_file("http://nifty-networks.net/swirc/pdcurses-3.6.cab", "pdcurses-3.6.cab");
 get_file("http://nifty-networks.net/swirc/swirc-royal.ico", "swirc-royal.ico");
