89cb8339fce73f129f1567e21d569df2b9550771
Markus Uhlin <markus.uhlin@bredband.net>
2018-12-16 23:36:31 +0100

    split tests

--- tests/sw_strcpy.c
+++ tests/sw_strcpy.c
@@ -9,35 +9,47 @@ static char buf[12] = "";
 static int ret = 0;
 
 static void
-handlesInvalidArguments(void **state)
+handlesInvalidArguments_test1(void **state)
 {
     ret = sw_strcpy(NULL, "some string", sizeof buf);
     assert_int_equal(ret, EINVAL);
     assert_string_equal(buf, "");
+}
 
+static void
+handlesInvalidArguments_test2(void **state)
+{
     ret = sw_strcpy(buf, NULL, sizeof buf);
     assert_int_equal(ret, EINVAL);
     assert_string_equal(buf, "");
+}
 
+static void
+handlesInvalidArguments_test3(void **state)
+{
     ret = sw_strcpy(buf, "some string", 0);
     assert_int_equal(ret, EINVAL);
     assert_string_equal(buf, "");
 }
 
 static void
-doesBoundsChecking(void **state)
+doesBoundsChecking_test1(void **state)
 {
     ret = sw_strcpy(buf, "some string!", sizeof buf);
     assert_int_equal(ret, ERANGE);
     assert_string_equal(buf, "");
+}
 
+static void
+doesBoundsChecking_test2(void **state)
+{
     ret = sw_strcpy(buf, "some string...", sizeof buf);
     assert_int_equal(ret, ERANGE);
     assert_string_equal(buf, "");
 }
 
 static void
-canCopy(void **state)
+canCopy_test1(void **state)
 {
     ret = sw_strcpy(buf, "some string", sizeof buf);
     assert_int_equal(ret, 0);
@@ -48,9 +60,12 @@ int
 main()
 {
     const struct CMUnitTest tests[] = {
-	cmocka_unit_test(handlesInvalidArguments),
-	cmocka_unit_test(doesBoundsChecking),
-	cmocka_unit_test(canCopy),
+	cmocka_unit_test(handlesInvalidArguments_test1),
+	cmocka_unit_test(handlesInvalidArguments_test2),
+	cmocka_unit_test(handlesInvalidArguments_test3),
+	cmocka_unit_test(doesBoundsChecking_test1),
+	cmocka_unit_test(doesBoundsChecking_test2),
+	cmocka_unit_test(canCopy_test1),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
