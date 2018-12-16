5dd5c748b7d48e2c0b0fce0465660a83178b8cf9
Markus Uhlin <markus.uhlin@bredband.net>
2018-12-16 23:44:23 +0100

    split out tests

--- tests/sw_strcat.c
+++ tests/sw_strcat.c
@@ -9,35 +9,47 @@ static char buf[7] = "foo";
 static int ret = 0;
 
 static void
-handlesInvalidArguments(void **state)
+handlesInvalidArguments_test1(void **state)
 {
     ret = sw_strcat(NULL, "bar", sizeof buf);
     assert_int_equal(ret, EINVAL);
     assert_string_equal(buf, "foo");
+}
 
+static void
+handlesInvalidArguments_test2(void **state)
+{
     ret = sw_strcat(buf, NULL, sizeof buf);
     assert_int_equal(ret, EINVAL);
     assert_string_equal(buf, "foo");
+}
 
+static void
+handlesInvalidArguments_test3(void **state)
+{
     ret = sw_strcat(buf, "bar", 0);
     assert_int_equal(ret, EINVAL);
     assert_string_equal(buf, "foo");
 }
 
 static void
-doesBoundsChecking(void **state)
+doesBoundsChecking_test1(void **state)
 {
     ret = sw_strcat(buf, "bar!", sizeof buf);
     assert_int_equal(ret, ERANGE);
     assert_string_equal(buf, "foo");
+}
 
+static void
+doesBoundsChecking_test2(void **state)
+{
     ret = sw_strcat(buf, "barbaz", sizeof buf);
     assert_int_equal(ret, ERANGE);
     assert_string_equal(buf, "foo");
 }
 
 static void
-canConcatenate(void **state)
+canConcatenate_test1(void **state)
 {
     ret = sw_strcat(buf, "...", sizeof buf);
     assert_int_equal(ret, 0);
@@ -48,9 +60,12 @@ int
 main()
 {
     const struct CMUnitTest tests[] = {
-	cmocka_unit_test(handlesInvalidArguments),
-	cmocka_unit_test(doesBoundsChecking),
-	cmocka_unit_test(canConcatenate),
+	cmocka_unit_test(handlesInvalidArguments_test1),
+	cmocka_unit_test(handlesInvalidArguments_test2),
+	cmocka_unit_test(handlesInvalidArguments_test3),
+	cmocka_unit_test(doesBoundsChecking_test1),
+	cmocka_unit_test(doesBoundsChecking_test2),
+	cmocka_unit_test(canConcatenate_test1),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
