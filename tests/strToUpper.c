1cba1f675e1f096001bd269511725bc943b956d6
Markus Uhlin <markus.uhlin@bredband.net>
2018-12-30 00:02:40 +0100

    test strToUpper()

---
+++ tests/strToUpper.c
@@ -0,0 +1,46 @@
+#include "common.h"
+
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "strHand.h"
+
+static void
+canUpperString_test1(void **state)
+{
+    char str[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ";
+    const char expected[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    assert_string_not_equal(str, expected);
+    assert_string_equal(strToUpper(str), expected);
+}
+
+static void
+leavesStringUnchanged_test1(void **state)
+{
+    char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char expected[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    assert_string_equal(strToUpper(str), expected);
+}
+
+static void
+leavesStringUnchanged_test2(void **state)
+{
+    char str[] = "0123456789...";
+    const char expected[] = "0123456789...";
+
+    assert_string_equal(strToUpper(str), expected);
+}
+
+int
+main()
+{
+    const struct CMUnitTest tests[] = {
+	cmocka_unit_test(canUpperString_test1),
+	cmocka_unit_test(leavesStringUnchanged_test1),
+	cmocka_unit_test(leavesStringUnchanged_test2),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
