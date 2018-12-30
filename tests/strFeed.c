a240c48f88f37154f58d8cbd90f8c54ede3d4e3d
Markus Uhlin <markus.uhlin@bredband.net>
2018-12-30 04:57:42 +0100

    Added more tests

--- tests/strFeed.c
+++ tests/strFeed.c
@@ -71,6 +71,40 @@ canFeedString_test6(void **state)
     assert_string_equal(str, expected);
 }
 
+static void
+leavesStringUnchanged_test1(void **state)
+{
+    char str[] = "\t\t\t\t\t";
+    const char *expected = "\t\t\t\t\t";
+
+    if (strFeed(str, 5) != 0)
+	fail();
+    assert_string_equal(str, expected);
+}
+
+static void
+leavesStringUnchanged_test2(void **state)
+{
+    char str[] = "0123456789";
+    const char *expected = "0123456789";
+
+    if (strFeed(str, 10) != 0)
+	fail();
+    assert_string_equal(str, expected);
+}
+
+static void
+leavesStringUnchanged_test3(void **state)
+{
+    char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const char *expected =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (strFeed(str, 26) != 0)
+	fail();
+    assert_string_equal(str, expected);
+}
+
 int
 main()
 {
@@ -81,6 +115,9 @@ main()
 	cmocka_unit_test(canFeedString_test4),
 	cmocka_unit_test(canFeedString_test5),
 	cmocka_unit_test(canFeedString_test6),
+	cmocka_unit_test(leavesStringUnchanged_test1),
+	cmocka_unit_test(leavesStringUnchanged_test2),
+	cmocka_unit_test(leavesStringUnchanged_test3),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
