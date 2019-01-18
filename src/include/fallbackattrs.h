98c9aca2cfd90ac483bd222876dba2ee721e9360
Markus Uhlin <markus.uhlin@bredband.net>
2019-01-18 20:49:49 +0100

    Set SW_INLINE and SW_NORET

--- src/include/fallbackattrs.h
+++ src/include/fallbackattrs.h
@@ -2,5 +2,9 @@
 #define NO_SIDE_EFFECT
 #define PRINTFLIKE(arg_no)
 #define PTR_ARGS_NONNULL
-#define SW_INLINE
+#define SW_INLINE inline
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+#define SW_NORET _Noreturn
+#else
 #define SW_NORET
+#endif
