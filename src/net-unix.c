d8e9166a9e1c7277ef9df867792e59dac357242c
Markus Uhlin <markus.uhlin@bredband.net>
2019-01-27 17:57:42 +0100

    Updated the copyright years

--- src/net-unix.c
+++ src/net-unix.c
@@ -1,5 +1,5 @@
 /* Networking for UNIX
-   Copyright (C) 2014-2018 Markus Uhlin. All rights reserved.
+   Copyright (C) 2014-2019 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
