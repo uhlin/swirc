7b670c41b2f62296821a911680a7e1d766ffe693
Markus Uhlin <markus.uhlin@bredband.net>
2019-01-27 18:02:52 +0100

    Updated the copyright years

--- src/printtext.c
+++ src/printtext.c
@@ -1,5 +1,5 @@
 /* Prints and handles text
-   Copyright (C) 2012-2018 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2019 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
