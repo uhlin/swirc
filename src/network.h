269f99e0f1ae7170b03012f5123aedef96c2b087
Markus Uhlin <markus.uhlin@bredband.net>
2019-02-17 20:32:44 +0100

    Added PRINTFLIKE() to typedef of NET_SEND_FN

--- src/network.h
+++ src/network.h
@@ -27,7 +27,7 @@ struct network_connect_context {
     char *nickname;
 };
 
-typedef PTR_ARGS_NONNULL int (*NET_SEND_FN)(const char *, ...);
+typedef PTR_ARGS_NONNULL int (*NET_SEND_FN)(const char *, ...) PRINTFLIKE(1);
 typedef PTR_ARGS_NONNULL int (*NET_RECV_FN)(struct network_recv_context *, char *, int);
 
 typedef enum {
