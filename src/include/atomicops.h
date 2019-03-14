b7a6745c3a945f80b2811099ec7f72d0138603ab
Markus Uhlin <markus.uhlin@bredband.net>
2019-03-14 19:06:45 +0100

    Disabled elif block

--- src/include/atomicops.h
+++ src/include/atomicops.h
@@ -3,8 +3,8 @@
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L && \
 	!defined(__STDC_NO_ATOMICS__)
 #include "atomic/c11.h"
-#elif defined(__cplusplus)
-#include "atomic/c++.h"
+//#elif defined(__cplusplus)
+//#include "atomic/c++.h"
 #elif defined(__GNUC__) && __GNUC__ >= 5
 #include "atomic/gcc.h"
 #elif defined(BSD)
