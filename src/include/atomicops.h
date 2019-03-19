8c7b3ec24584deb64ae7c4686d84c6037d76775f
Markus Uhlin <markus.uhlin@bredband.net>
2019-03-19 15:30:24 +0100

    Deleted C11 atomics

--- src/include/atomicops.h
+++ src/include/atomicops.h
@@ -1,11 +1,6 @@
 #ifndef ATOMIC_OPERATIONS_HEADER
 #define ATOMIC_OPERATIONS_HEADER
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L && \
-	!defined(__STDC_NO_ATOMICS__)
-#include "atomic/c11.h"
-//#elif defined(__cplusplus)
-//#include "atomic/c++.h"
-#elif defined(__GNUC__) && __GNUC__ >= 5
+#if defined(__GNUC__) && __GNUC__ >= 5
 #include "atomic/gcc.h"
 #elif defined(BSD)
 #include "atomic/bsd.h"
