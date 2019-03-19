f7f926bc0459ea11b4870fdc46c629c7f38d56a0
Markus Uhlin <markus.uhlin@bredband.net>
2019-03-19 20:55:28 +0100

    Assert size of bool is within limits

--- src/include/atomic/w32.h
+++ src/include/atomic/w32.h
@@ -6,12 +6,14 @@
 static SW_INLINE bool
 atomic_load_bool(volatile bool *obj)
 {
+    static_assert(sizeof (bool) == 1, "bool not 8 bits");
     return (_InterlockedCompareExchange8((volatile char *) obj, false, false));
 }
 
 static SW_INLINE bool
 atomic_swap_bool(volatile bool *obj, bool desired)
 {
+    static_assert(sizeof (bool) == 1, "bool not 8 bits");
     return (_InterlockedExchange8((volatile char *) obj, desired));
 }
 
