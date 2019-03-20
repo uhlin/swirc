efdb37a2908ea7ec6f850f64dacca328fb2aadb8
Markus Uhlin <markus.uhlin@bredband.net>
2019-03-20 13:46:54 +0100

    Avoiding name conflicts

--- src/include/atomic/bsd.h
+++ src/include/atomic/bsd.h
@@ -1,14 +1,19 @@
 #ifndef ATOMIC_BSD_H
 #define ATOMIC_BSD_H
 
+#define atomic_load_bool(obj) \
+	__swirc_atomic_load_bool(obj)
+#define atomic_swap_bool(obj, desired) \
+	__swirc_atomic_swap_bool((obj), (desired))
+
 static SW_INLINE bool
-atomic_load_bool(volatile bool *obj)
+__swirc_atomic_load_bool(volatile bool *obj)
 {
     return (__sync_val_compare_and_swap(obj, false, false));
 }
 
 static SW_INLINE bool
-atomic_swap_bool(volatile bool *obj, bool desired)
+__swirc_atomic_swap_bool(volatile bool *obj, bool desired)
 {
     return (__sync_lock_test_and_set(obj, desired));
 }
