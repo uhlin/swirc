bf04296fd4faf2a9c9b975af44bed1bfec626bba
Markus Uhlin <markus.uhlin@bredband.net>
2019-03-20 13:50:32 +0100

    SW_INLINE -> inline

--- src/include/atomic/bsd.h
+++ src/include/atomic/bsd.h
@@ -6,13 +6,13 @@
 #define atomic_swap_bool(obj, desired) \
 	__swirc_atomic_swap_bool((obj), (desired))
 
-static SW_INLINE bool
+static inline bool
 __swirc_atomic_load_bool(volatile bool *obj)
 {
     return (__sync_val_compare_and_swap(obj, false, false));
 }
 
-static SW_INLINE bool
+static inline bool
 __swirc_atomic_swap_bool(volatile bool *obj, bool desired)
 {
     return (__sync_lock_test_and_set(obj, desired));
