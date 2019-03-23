5230a6f8899a17655f1a92558f06dd8ee307051b
Markus Uhlin <markus.uhlin@bredband.net>
2019-03-23 01:24:58 +0100

    Updated the copyright years

--- src/events/names.c
+++ src/events/names.c
@@ -1,5 +1,5 @@
 /* Handle event names (353) and event EOF names (366)
-   Copyright (C) 2015-2018 Markus Uhlin. All rights reserved.
+   Copyright (C) 2015-2019 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
