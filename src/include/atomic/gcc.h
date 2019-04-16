2d73fd83a5c8dfb6344ea1899153bc507f6c06d2
Markus Uhlin <markus.uhlin@bredband.net>
2019-04-16 08:48:49 +0200

    Suppress warnings

--- src/include/atomic/gcc.h
+++ src/include/atomic/gcc.h
@@ -1,6 +1,10 @@
 #ifndef ATOMIC_GCC_H
 #define ATOMIC_GCC_H
 
+#ifdef __SUNPRO_C
+#pragma error_messages (off, E_ARG_INCOMPATIBLE_WITH_ARG_L)
+#endif
+
 static SW_INLINE bool
 atomic_load_bool(volatile bool *obj)
 {
@@ -12,5 +16,9 @@ atomic_swap_bool(volatile bool *obj, bool desired)
 {
     return (__atomic_exchange_n(obj, desired, __ATOMIC_SEQ_CST));
 }
+
+#ifdef __SUNPRO_C
+#pragma error_messages (default, E_ARG_INCOMPATIBLE_WITH_ARG_L)
+#endif
 
 #endif
