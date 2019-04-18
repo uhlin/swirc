2b819ceb3e770c1b5b27f2dec49632719ea12712
Markus Uhlin <markus.uhlin@bredband.net>
2019-04-19 00:07:36 +0200

    Updated the copyright years

--- src/terminal.c
+++ src/terminal.c
@@ -1,5 +1,5 @@
 /* OS-independent terminal stuff
-   Copyright (C) 2012-2014 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2019 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
