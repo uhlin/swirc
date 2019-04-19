0e2ddf5996549053e537e7ea815ab062ad5471ca
Markus Uhlin <markus.uhlin@bredband.net>
2019-04-19 03:24:49 +0200

    Clear toasts command

---
+++ src/commands/cleartoasts.cpp
@@ -0,0 +1,16 @@
+#include "common.h"
+#ifdef WIN32
+#include "../ToastsAPI.hpp"
+#endif
+#include "cleartoasts.h"
+
+/* usage: /cleartoasts */
+void
+cmd_cleartoasts(const char *data)
+{
+#if defined(UNIX)
+	(void) 0;
+#elif defined(WIN32)
+	Toasts::ClearToasts();
+#endif
+}
---
+++ src/commands/cleartoasts.h
@@ -0,0 +1,14 @@
+#ifndef CMD_CLEARTOASTS_H
+#define CMD_CLEARTOASTS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cmd_cleartoasts(const char *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
