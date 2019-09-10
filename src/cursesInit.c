6901fa2e10e6f2f1b9aae316926a0e884ff16a0a
Markus Uhlin <markus.uhlin@bredband.net>
2019-09-10 02:08:28 +0200

    init_color_pairs: attempt to initialize more pairs

--- src/cursesInit.c
+++ src/cursesInit.c
@@ -1,5 +1,5 @@
 /* Initialization of the Ncurses library
-   Copyright (C) 2012-2017 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2019 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -101,6 +101,11 @@ init_color_pairs()
 		}
 	    }
 	}
+    }
+
+    for (size_t n = 1; n < numColors; n++) {
+	if (init_pair(++pair_n, colors[n], colors[n]) == ERR)
+	    return (pair_n - 1);
     }
 
     return pair_n;
