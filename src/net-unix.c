180ffd0c6b92a1addf45ee43dcf427111bc14071
Markus Uhlin <markus.uhlin@bredband.net>
2019-09-18 03:56:04 +0200

    net_recv_plain: improved function

--- src/net-unix.c
+++ src/net-unix.c
@@ -55,31 +55,26 @@ net_recv_plain(struct network_recv_context *ctx,
     const int      maxfdp1 = ctx->sock + 1;
     fd_set         readset;
     int            bytes_received;
-    struct timeval tv;
+    struct timeval tv = {
+	.tv_sec  = ctx->sec,
+	.tv_usec = ctx->microsec,
+    };
 
     FD_ZERO(&readset);
     FD_SET(ctx->sock, &readset);
 
-    tv.tv_sec  = ctx->sec;
-    tv.tv_usec = ctx->microsec;
-
     errno = 0;
 
-    if (select(maxfdp1, &readset, NULL, NULL, &tv) == -1) {
+    if (select(maxfdp1, &readset, NULL, NULL, &tv) == -1)
 	return (errno == EINTR ? 0 : -1);
-    } else if (!FD_ISSET(ctx->sock, &readset)) { /* No data to recv() */
-	return (0);
-    } else if (bytes_received = recv(ctx->sock,recvbuf,recvbuf_size,ctx->flags),
-	       bytes_received == -1) {
+    else if (!FD_ISSET(ctx->sock, &readset)) /* No data to recv() */
+	return 0;
+    else if (bytes_received = recv(ctx->sock,recvbuf,recvbuf_size,ctx->flags),
+	     bytes_received == -1)
 	return (errno==EAGAIN || errno==EWOULDBLOCK || errno==EINTR ? 0 : -1);
-    } else if (bytes_received == 0) { /* Disconnected! */
-	return (-1);
-    } else {
-	return (bytes_received);
-    }
-
-    /*NOTREACHED*/ sw_assert_not_reached();
-    /*NOTREACHED*/ return (-1);
+    else if (bytes_received == 0) /* Disconnected! */
+	return -1;
+    return bytes_received;
 }
 
 int
