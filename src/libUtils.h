72b61f2022412930cc6d06b13381f7fca079927a
Markus Uhlin <markus.uhlin@bredband.net>
2019-10-20 20:07:16 +0200

    Define MIN() and MAX() if needed

--- src/libUtils.h
+++ src/libUtils.h
@@ -32,6 +32,14 @@ void	*xrealloc(void *ptr, size_t newSize);
 }
 #endif
 
+#ifndef MAX
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#endif
+
+#ifndef MIN
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#endif
+
 /* Inline function definitions
    =========================== */
 
