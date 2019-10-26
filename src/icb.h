990436029f6c56fd25c81c22ce581caaefaccda5
Markus Uhlin <markus.uhlin@bredband.net>
2019-10-26 22:36:34 +0200

    Indenting

--- src/icb.h
+++ src/icb.h
@@ -1,9 +1,9 @@
 #ifndef ICB_H
 #define ICB_H
 
-#define ICB_FIELD_SEP "\001"
-#define ICB_MESSAGE_MAX 253
-#define ICB_PACKET_MAX 255
+#define ICB_FIELD_SEP		"\001"
+#define ICB_MESSAGE_MAX		253
+#define ICB_PACKET_MAX		255
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,16 +11,16 @@ extern "C" {
 
 extern volatile bool g_icb_processing_names;
 
-void icb_irc_proxy(const int length, const char type, const char *pktdata);
-void icb_process_event_eof_names(void);
-void icb_send_boot(const char *victim);
-void icb_send_group(const char *group);
-void icb_send_name(const char *new_nick);
-void icb_send_noop(void);
-void icb_send_open_msg(const char *text);
-void icb_send_pm(const char *to_who, const char *text);
-void icb_send_topic(const char *new_topic);
-void icb_send_users(const char *arg);
+void	 icb_irc_proxy(const int length, const char type, const char *pktdata);
+void	 icb_process_event_eof_names(void);
+void	 icb_send_boot(const char *victim);
+void	 icb_send_group(const char *group);
+void	 icb_send_name(const char *new_nick);
+void	 icb_send_noop(void);
+void	 icb_send_open_msg(const char *text);
+void	 icb_send_pm(const char *to_who, const char *text);
+void	 icb_send_topic(const char *new_topic);
+void	 icb_send_users(const char *arg);
 
 #ifdef __cplusplus
 }
