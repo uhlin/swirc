10d78115d7f7961fb7c13e41cc41adc2f3dccd0c
Markus Uhlin <markus.uhlin@bredband.net>
2019-11-20 21:46:56 +0100

    Make header usable from C++

--- src/include/base64.h
+++ src/include/base64.h
@@ -4,7 +4,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int b64_decode(char const *src, uint8_t *target, size_t targsize);
 int b64_encode(uint8_t const *src, size_t srclength, char *target, size_t targsize);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
