0a94ddd842a16f75ce523fcef062650629b722c9
Markus Uhlin <markus.uhlin@bredband.net>
2020-01-18 12:10:30 +0100

    Updated copyright years

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /* main.cpp  --  starts execution
-   Copyright (C) 2012-2019 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2020 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -71,7 +71,7 @@
    ============================ */
 
 const char g_swircVersion[] = "v3.1.1";
-const char g_swircYear[]    = "2012-2019";
+const char g_swircYear[]    = "2012-2020";
 const char g_swircAuthor[]  = "Markus Uhlin";
 const char g_swircWebAddr[] = "https://www.nifty-networks.net/swirc/";
 
