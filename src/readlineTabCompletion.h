5e6e1fd663b2b5fa4d10db3fa6e6a150b261cea6
Markus Uhlin <markus.uhlin@bredband.net>
2020-02-10 01:35:13 +0100

    ifdef __cplusplus

--- src/readlineTabCompletion.h
+++ src/readlineTabCompletion.h
@@ -1,10 +1,18 @@
 #ifndef READLINE_TAB_COMPLETION_H
 #define READLINE_TAB_COMPLETION_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 PTAB_COMPLETION readline_tab_comp_ctx_new(void);
 void readline_tab_comp_ctx_destroy(PTAB_COMPLETION ctx);
 void readline_tab_comp_ctx_reset(PTAB_COMPLETION ctx);
 
 void readline_handle_tab(volatile struct readline_session_context *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
