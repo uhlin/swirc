489c280da4b22b01b8a0f1cc7d3a210a969fc3e5
Markus Uhlin <markus.uhlin@bredband.net>
2020-02-29 18:00:59 +0100

    Added prototype for hUndef()

--- src/theme.c
+++ src/theme.c
@@ -1,5 +1,5 @@
 /* Support for themes
-   Copyright (C) 2012-2019 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2020 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -165,7 +165,9 @@ hash(const char *item_name)
     return (hashval % ARRAY_SIZE(hash_table));
 }
 
-static PTR_ARGS_NONNULL void
+static void hUndef(PTHEME_HTBL_ENTRY) PTR_ARGS_NONNULL;
+
+static void
 hUndef(PTHEME_HTBL_ENTRY entry)
 {
     PTHEME_HTBL_ENTRY *indirect = & (hash_table[hash(entry->name)]);
