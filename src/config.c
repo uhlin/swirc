c22211ed68a5bbf417a7d3ffc57a6b28dbea733a
Markus Uhlin <markus.uhlin@bredband.net>
2020-02-29 18:02:37 +0100

    Added prototype for hUndef()

--- src/config.c
+++ src/config.c
@@ -1,5 +1,5 @@
 /* User configuration
-   Copyright (C) 2012-2019 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2020 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -139,7 +139,9 @@ hash(const char *setting_name)
     return (hashval % ARRAY_SIZE(hash_table));
 }
 
-static PTR_ARGS_NONNULL void
+static void hUndef(PCONF_HTBL_ENTRY) PTR_ARGS_NONNULL;
+
+static void
 hUndef(PCONF_HTBL_ENTRY entry)
 {
     PCONF_HTBL_ENTRY *indirect = & (hash_table[hash(entry->name)]);
