cfd71123c483ddfd3e88e5eee8c6b9842864350e
Markus Uhlin <markus.uhlin@bredband.net>
2020-03-01 05:53:04 +0100

    Added function edit_debian_changelog

---
+++ bash/edit_debian_changelog.sh
@@ -0,0 +1,26 @@
+edit_debian_changelog () {
+	local author="Markus Uhlin"
+	local email="markus.uhlin@bredband.net"
+	local timestamp="$(date -R)"
+	local tmpFile1="$(mktemp)"
+	local tmpFile2="$(mktemp)"
+
+	printf "  - Editing %s..." "$1"
+
+	if [ -f "$1" ]; then
+		cat <<EOF > "$tmpFile1"
+swirc ($VERSION) unstable; urgency=medium
+
+  * New version
+
+ -- $author <$email>  $timestamp
+
+EOF
+		{ cat "$tmpFile1"; cat "$1"; } > "$tmpFile2"
+		mv -f "$tmpFile2" "$1" || { echo "error"; exit 1; }
+		rm -f "$tmpFile1"      || { echo "error"; exit 1; }
+		echo "ok"
+	else
+		echo "not found"
+	fi
+}
