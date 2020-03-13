a29344435eef445d65895a531e4ac7598d035beb
Markus Uhlin <markus.uhlin@bredband.net>
2020-03-13 23:16:12 +0100

    Added begin/end decls pair

--- src/cursesInit.h
+++ src/cursesInit.h
@@ -1,18 +1,12 @@
 #ifndef CURSES_INIT_H
 #define CURSES_INIT_H
 
+__SWIRC_BEGIN_DECLS
 extern bool		g_no_colors;
 extern short int	g_initialized_pairs;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /* returns OK or ERR */
 int curses_init(void);
-
-#ifdef __cplusplus
-}
-#endif
+__SWIRC_END_DECLS
 
 #endif
