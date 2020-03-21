a8bec7ae23bb70af4cc9a7583a5f0b186e889583
Markus Uhlin <markus.uhlin@bredband.net>
2020-03-21 13:59:16 +0100

    Added begin/end decls

--- src/events/whois.h
+++ src/events/whois.h
@@ -1,6 +1,7 @@
 #ifndef WHOIS_H
 #define WHOIS_H
 
+__SWIRC_BEGIN_DECLS
 void event_whois_ssl      (struct irc_message_compo *);
 void event_whois_cert     (struct irc_message_compo *);
 void event_whois_away     (struct irc_message_compo *);
@@ -15,5 +16,6 @@ void event_whois_host     (struct irc_message_compo *);
 void event_whois_conn     (struct irc_message_compo *);
 void event_whois_modes    (struct irc_message_compo *);
 void event_whoReply       (struct irc_message_compo *);
+__SWIRC_END_DECLS
 
 #endif
