e01bb411f95dbe8dcc6d1238306fe40f19709ae9
Markus Uhlin <markus.uhlin@bredband.net>
2020-03-21 17:17:49 +0100

    event_whois_service: rewrite

--- src/events/whois.cpp
+++ src/events/whois.cpp
@@ -526,6 +526,41 @@ event_whois_server(struct irc_message_compo *compo)
     }
 }
 
+/* event_whois_service: 307
+
+   Example:
+     :irc.server.com 307 <issuer> <target> :user has identified to services */
+void
+event_whois_service(struct irc_message_compo *compo)
+{
+    PRINTTEXT_CONTEXT ctx;
+
+    printtext_context_init(&ctx, g_active_window, TYPE_SPEC1, true);
+
+    try {
+	char *msg = NULL;
+	char *state = const_cast<char *>("");
+
+	if (strFeed(compo->params, 2) != 2)
+	    throw std::runtime_error("strFeed");
+
+	(void) strtok_r(compo->params, "\n", &state);
+	(void) strtok_r(NULL, "\n", &state);
+
+	if ((msg = strtok_r(NULL, "\n", &state)) == NULL)
+	    throw std::runtime_error("no message");
+	if (*msg == ':')
+	    msg++;
+	if (*msg)
+	    printtext(&ctx, "%s %s", Theme("whois_service"), msg);
+    } catch (const std::runtime_error &e) {
+	ctx.window = g_status_window;
+	ctx.spec_type = TYPE_SPEC1_WARN;
+	printtext(&ctx, "event_whois_service(%s): error: %s",
+	    compo->command, e.what());
+    }
+}
+
 /* event_whois_ssl: 275, 671
 
    Examples:
@@ -564,45 +599,6 @@ event_whois_ssl(struct irc_message_compo *compo)
 	ctx.window    = g_active_window;
 	ctx.spec_type = TYPE_SPEC1;
 	printtext(&ctx, "%s %s %s", Theme("whois_ssl"), tnick, msg);
-    }
-}
-
-/* event_whois_service: 307
-
-   Example:
-     :irc.server.com 307 <issuer> <target> :user has identified to services */
-void
-event_whois_service(struct irc_message_compo *compo)
-{
-    PRINTTEXT_CONTEXT ctx;
-    char *msg;
-    char *state = "";
-
-    printtext_context_init(&ctx, g_status_window, TYPE_SPEC1_WARN, true);
-
-    if (strFeed(compo->params, 2) != 2) {
-	printtext(&ctx, "On issuing event %s: strFeed(..., 2) != 2",
-		  compo->command);
-	return;
-    }
-
-    (void) strtok_r(compo->params, "\n", &state);
-    (void) strtok_r(NULL, "\n", &state);
-
-    if ((msg = strtok_r(NULL, "\n", &state)) == NULL) {
-	printtext(&ctx, "On issuing event %s: Unable to extract message",
-		  compo->command);
-	return;
-    }
-
-    if (*msg == ':') {
-	msg++;
-    }
-
-    if (*msg) {
-	ctx.window    = g_active_window;
-	ctx.spec_type = TYPE_SPEC1;
-	printtext(&ctx, "%s %s", Theme("whois_service"), msg);
     }
 }
 
