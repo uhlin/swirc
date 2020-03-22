6f4b55aa284005e1f96538020872f349a6a41b74
Markus Uhlin <markus.uhlin@bredband.net>
2020-03-22 13:34:17 +0100

    banlist.c -> banlist.cpp

--- src/events/banlist.cpp
+++ src/events/banlist.cpp
@@ -1,5 +1,5 @@
 /* Event 367 (RPL_BANLIST) and 368 (RPL_ENDOFBANLIST)
-   Copyright (C) 2016-2018 Markus Uhlin. All rights reserved.
+   Copyright (C) 2016-2018, 2020 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
