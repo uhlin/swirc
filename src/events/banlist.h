944371fe656ef85fe36f7d5cce78d67ce01df274
Markus Uhlin <markus.uhlin@bredband.net>
2020-03-22 13:32:23 +0100

    Added begin/end decls

--- src/events/banlist.h
+++ src/events/banlist.h
@@ -7,7 +7,9 @@
 #define event_eof_inviteList event_eof_banlist
 #define event_eof_exceptList event_eof_banlist
 
+__SWIRC_BEGIN_DECLS
 void event_banlist     (struct irc_message_compo *);
 void event_eof_banlist (struct irc_message_compo *);
+__SWIRC_END_DECLS
 
 #endif
