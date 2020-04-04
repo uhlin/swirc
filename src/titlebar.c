88730fd0bb9470f6b913657bcb00709b02d3ddb6
Markus Uhlin <markus.uhlin@bredband.net>
2020-04-05 00:12:45 +0200

    Updated copyright years

--- src/titlebar.c
+++ src/titlebar.c
@@ -1,5 +1,5 @@
 /* Swirc titlebar
-   Copyright (C) 2012-2013, 2016 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2018 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
