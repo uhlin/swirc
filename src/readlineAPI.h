e20ef789a6b4141e4e711b6ba49203a74bff3117
Markus Uhlin <markus.uhlin@bredband.net>
2021-01-23 11:55:20 +0100

    Deleted variable names

--- src/readlineAPI.h
+++ src/readlineAPI.h
@@ -16,12 +16,12 @@
 __SWIRC_BEGIN_DECLS
 NORETURN void readline_error(int error, const char *msg);
 
-void	readline_mvwaddch (WINDOW *, int row, int col, wint_t wc);
-void	readline_mvwinsch (WINDOW *, int row, int col, wint_t wc);
-void	readline_waddch   (WINDOW *, wint_t wc);
-void	readline_waddnstr (WINDOW *, const wchar_t *s, ptrdiff_t n);
-void	readline_winsch   (WINDOW *, wint_t wc);
-void	readline_winsnstr (WINDOW *, const wchar_t *s, ptrdiff_t n);
+void	readline_mvwaddch(WINDOW *, int row, int col, wint_t);
+void	readline_mvwinsch(WINDOW *, int row, int col, wint_t);
+void	readline_waddch(WINDOW *, wint_t);
+void	readline_waddnstr(WINDOW *, const wchar_t *, ptrdiff_t);
+void	readline_winsch(WINDOW *, wint_t);
+void	readline_winsnstr(WINDOW *, const wchar_t *, ptrdiff_t);
 __SWIRC_END_DECLS
 
 #endif
