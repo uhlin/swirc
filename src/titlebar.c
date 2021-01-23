83127e3d83ef9e3c374f00a0dd547957e41f13aa
Markus Uhlin <markus.uhlin@bredband.net>
2021-01-23 11:21:12 +0100

    titlebar: do not dynamically allocate 'reject'

--- src/titlebar.c
+++ src/titlebar.c
@@ -1,5 +1,5 @@
 /* Swirc titlebar
-   Copyright (C) 2012-2018 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2021 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -40,6 +40,9 @@
 #include "terminal.h"
 #include "theme.h"
 #include "titlebar.h"
+
+static const char reject[] =
+    TXT_BLINK TXT_BOLD TXT_COLOR TXT_NORMAL TXT_REVERSE TXT_UNDERLINE;
 
 static PANEL *titlebar_pan = NULL;
 
@@ -88,8 +91,6 @@ titlebar(const char *fmt, ...)
     WINDOW    *win    = panel_window(titlebar_pan);
     chtype     blank  = ' ';
     short int  pair_n = get_pair_num();
-    char      *reject = strdup_printf(
-	"%c%c%c%c%c%c", BLINK, BOLD, COLOR, NORMAL, REVERSE, UNDERLINE);
 
     va_start(ap, fmt);
     fmt_copy = strdup_vprintf(fmt, ap);
@@ -105,7 +106,6 @@ titlebar(const char *fmt, ...)
     }
 
     free(fmt_copy);
-    free(reject);
 }
 
 void
