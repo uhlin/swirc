0030865c7d2c2efe2c4c2677b14f40826c06aad6
Markus Uhlin <markus.uhlin@bredband.net>
2021-01-23 11:44:35 +0100

    vprinttext: changed indent

--- src/printtext.c
+++ src/printtext.c
@@ -1,5 +1,5 @@
 /* Prints and handles text
-   Copyright (C) 2012-2020 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2021 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -1489,8 +1489,8 @@ vprinttext(PPRINTTEXT_CONTEXT ctx, const char *fmt, va_list ap)
     }
 
     if (! (ctx->window->scroll_mode)) {
-	printtext_puts(
-	    panel_window(ctx->window->pan), pout->text, pout->indent, -1, NULL);
+	printtext_puts(panel_window(ctx->window->pan), pout->text, pout->indent,
+	    -1, NULL);
     }
 
     if (ctx->window->logging) {
