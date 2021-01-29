c9d56545af49edc85969c6dc916a9b24754cc0d5
Markus Uhlin <markus.uhlin@bredband.net>
2021-01-29 05:11:04 +0100

    Revert "Rethought net_ssl_send()"
    This reverts commit 143f5c0dc0fd709d6a65fb86cd7051957daa710c.

--- src/network-openssl.c
+++ src/network-openssl.c
@@ -173,7 +173,6 @@ int
 net_ssl_send(const char *fmt, ...)
 {
     char	*buf    = NULL;
-    char	*bufptr = NULL;
     int		 buflen = 0;
     int		 n_sent = 0;
     va_list	 ap;
@@ -194,38 +193,26 @@ net_ssl_send(const char *fmt, ...)
 	return -1;
     }
 
-    bufptr = buf;
     buflen = (int) strlen(buf);
-    debug("net_ssl_send: buflen = %d", buflen);
+    ERR_clear_error();
 
-    while (buflen > 0) {
-	ERR_clear_error();
-	const int ret = SSL_write(ssl, bufptr, buflen);
-
-	if (ret > 0) {
-	    /*
-	     * good
-	     */
-	    n_sent += ret;
-	    bufptr += ret;
-	    buflen -= ret;
-	} else {
-	    switch (SSL_get_error(ssl, ret)) {
-	    case SSL_ERROR_WANT_READ:
-	    case SSL_ERROR_WANT_WRITE:
-		debug("net_ssl_send: want read / want write");
-		continue;
-	    }
-
-	    free(buf);
-	    return -1;
-	}
+    if ((n_sent = SSL_write(ssl, buf, buflen)) > 0) {
+	free(buf);
+	return n_sent;
     }
 
-    debug("net_ssl_send: buflen = %d", buflen);
-    debug("net_ssl_send: n_sent = %d", n_sent);
     free(buf);
-    return n_sent;
+
+    switch (SSL_get_error(ssl, n_sent)) {
+    case SSL_ERROR_NONE:
+	return 0;
+    case SSL_ERROR_WANT_READ:
+    case SSL_ERROR_WANT_WRITE:
+	err_log(0, "net_ssl_send: operation did not complete");
+	return 0;
+    }
+
+    return -1;
 }
 
 int
