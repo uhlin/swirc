99fd1939d367928f5c8166f6508e551dde0e7dad
Markus Uhlin <markus.uhlin@bredband.net>
2021-02-28 20:27:20 +0100

    apply_readline_options: call wtimeout()

--- src/readline.c
+++ src/readline.c
@@ -1,5 +1,5 @@
 /* Read user input
-   Copyright (C) 2012-2020 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2021 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -101,6 +101,9 @@ apply_readline_options(WINDOW *win)
     if (is_scrollok(win)) {
 	SCROLLOK(win, 0);
     }
+
+    const int wait_time_millisec = 270;
+    wtimeout(win, wait_time_millisec);
 }
 
 /**
