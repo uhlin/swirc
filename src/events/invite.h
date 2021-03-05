e9a897dba0902c6c97cf17a12af01b1cb73dbf76
Markus Uhlin <markus.uhlin@bredband.net>
2021-03-05 14:58:05 +0100

    Added begin/end decls

--- src/events/invite.h
+++ src/events/invite.h
@@ -1,7 +1,9 @@
 #ifndef GUARD_INVITE_H
 #define GUARD_INVITE_H
 
+__SWIRC_BEGIN_DECLS
 void event_inviting(struct irc_message_compo *);
 void event_invite(struct irc_message_compo *);
+__SWIRC_END_DECLS
 
 #endif
