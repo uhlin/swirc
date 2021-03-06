59739e4f0ff05816111f3e7a00580e0e1d685b8b
Markus Uhlin <markus.uhlin@bredband.net>
2021-03-06 14:34:29 +0100

    Updated copyright years

--- src/irc.c
+++ src/irc.c
@@ -1,5 +1,5 @@
 /* Handle and interpret IRC events
-   Copyright (C) 2014-2020 Markus Uhlin. All rights reserved.
+   Copyright (C) 2014-2021 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
