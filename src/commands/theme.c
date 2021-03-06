267f6d42d3d11b182b1f9cd98ed89a2ea52328dc
Markus Uhlin <markus.uhlin@bredband.net>
2021-03-06 03:14:43 +0100

    set_theme: improved function

--- src/commands/theme.c
+++ src/commands/theme.c
@@ -377,29 +377,48 @@ set_theme(const char *name)
     char buf[2300] = "";
 
     printtext_context_init(&ctx, g_active_window, TYPE_SPEC1_FAILURE, true);
-    (void) sw_strcpy(buf, g_home_dir, sizeof buf);
-    (void) sw_strcat(buf, SLASH,      sizeof buf);
-    (void) sw_strcat(buf, name,       sizeof buf);
-    (void) sw_strcat(buf, g_theme_filesuffix, sizeof buf);
-    if (!is_regular_file(buf)) {
+
+    if ((errno = sw_strcpy(buf, g_home_dir, sizeof buf)) != 0 ||
+	(errno = sw_strcat(buf, SLASH, sizeof buf)) != 0 ||
+	(errno = sw_strcat(buf, name, sizeof buf)) != 0 ||
+	(errno = sw_strcat(buf, g_theme_filesuffix, sizeof buf)) != 0) {
+	printtext(&ctx, "error building name path (errno=%d)", errno);
+	return;
+    } else if (!is_regular_file(buf)) {
 	printtext(&ctx, "non-existent");
 	return;
     }
+
     theme_deinit();
     theme_init();
     theme_readit(buf, "r");
-    (void) config_item_undef("theme");
-    (void) config_item_install("theme", name);
-    BZERO(buf, sizeof buf);
-    (void) sw_strcpy(buf, g_home_dir, sizeof buf);
-    (void) sw_strcat(buf, SLASH,      sizeof buf);
-    (void) sw_strcat(buf, "swirc",    sizeof buf);
-    (void) sw_strcat(buf, g_config_filesuffix, sizeof buf);
-    config_do_save(buf, "w");
     titlebar(" %s ", g_active_window->title ? g_active_window->title : "");
     statusbar_update_display_beta();
+
     ctx.spec_type = TYPE_SPEC1_SUCCESS;
     printtext(&ctx, "theme activated");
+    ctx.spec_type = TYPE_SPEC1_FAILURE;
+
+    if ((errno = config_item_undef("theme")) != 0)
+	err_log(errno, "set_theme: config_item_undef");
+    if ((errno = config_item_install("theme", name)) != 0)
+	err_log(errno, "set_theme: config_item_install");
+
+    BZERO(buf, sizeof buf);
+
+    if ((errno = sw_strcpy(buf, g_home_dir, sizeof buf)) != 0 ||
+	(errno = sw_strcat(buf, SLASH, sizeof buf)) != 0 ||
+	(errno = sw_strcat(buf, "swirc", sizeof buf)) != 0 ||
+	(errno = sw_strcat(buf, g_config_filesuffix, sizeof buf)) != 0) {
+	printtext(&ctx, "error building path to swirc%s (errno=%d)",
+	    g_config_filesuffix, errno);
+	return;
+    } else if (!is_regular_file(buf)) {
+	printtext(&ctx, "error saving swirc%s", g_config_filesuffix);
+	return;
+    }
+
+    config_do_save(buf, "w");
 }
 
 /* usage: /theme [install | list-remote | set] [name] */
