813d90f33f6d9bd8f1af0b6f2108863a15aa9374
Markus Uhlin <markus.uhlin@bredband.net>
2021-03-11 22:46:01 +0100

    case_key_right: locked operation

--- src/readline.c
+++ src/readline.c
@@ -328,15 +328,21 @@ case_key_right(volatile struct readline_session_context *ctx)
 	magic_swap_panels(ctx, true);
     }
 
+    mutex_lock(&g_puts_mutex);
+
     ctx->bufpos++;
     yx = term_get_pos(ctx->act);
 
     if (wmove(ctx->act, yx.cury, yx.curx + 1) == ERR) {
+	mutex_unlock(&g_puts_mutex);
 	readline_error(EPERM, "wmove");
+	/* NOTREACHED */
     }
 
     update_panels();
     (void) doupdate();
+
+    mutex_unlock(&g_puts_mutex);
 }
 
 /**
