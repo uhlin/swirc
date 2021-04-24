d78dceee7a479b1f4e350e501e14380ddbf97fc4
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-24 18:03:37 +0200

    path_to_home: decreased buffer size

--- src/nestHome.c
+++ src/nestHome.c
@@ -1,5 +1,5 @@
 /* Create the home directory and read its configuration files
-   Copyright (C) 2012-2020 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2021 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -205,7 +205,7 @@ path_to_home(void)
 #elif defined(WIN32)
     const char *var = "APPDATA";
 #endif
-    static char buf[2000] = "";
+    static char buf[1000] = "";
 
 /*
  * getenv() is safe in this context
