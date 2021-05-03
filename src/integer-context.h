3d0cc17a0339e6c32285a87c703800cbc0662bd1
Markus Uhlin <markus.uhlin@bredband.net>
2021-05-03 22:06:38 +0200

    Added a constructor

--- src/integer-context.h
+++ src/integer-context.h
@@ -6,6 +6,17 @@ struct integer_context {
     long int	 lo_limit;      /* Low limit */
     long int	 hi_limit;      /* High limit */
     long int	 fallback_default;	/* If the range is exceeded  --  use this value */
+
+#ifdef __cplusplus
+    integer_context(char *setting_name, long int lo_limit, long int hi_limit,
+		    long int fallback_default)
+	{
+	    this->setting_name = setting_name;
+	    this->lo_limit = lo_limit;
+	    this->hi_limit = hi_limit;
+	    this->fallback_default = fallback_default;
+	}
+#endif
 };
 
 #endif
