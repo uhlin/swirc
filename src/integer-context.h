2da2a40f83257a0eccdae75dab514eeae624f8db
Markus Uhlin <markus.uhlin@bredband.net>
2021-05-03 23:00:44 +0200

    Made 'setting_name' const

--- src/integer-context.h
+++ src/integer-context.h
@@ -2,13 +2,13 @@
 #define INTEGER_CONTEXT_H
 
 struct integer_context {
-    char	*setting_name;  /* Name of the setting or item */
+    const char	*setting_name;  /* Name of the setting or item */
     long int	 lo_limit;      /* Low limit */
     long int	 hi_limit;      /* High limit */
     long int	 fallback_default;	/* If the range is exceeded  --  use this value */
 
 #ifdef __cplusplus
-    integer_context(char *setting_name, long int lo_limit, long int hi_limit,
+    integer_context(const char *setting_name, long int lo_limit, long int hi_limit,
 		    long int fallback_default)
 	{
 	    this->setting_name = setting_name;
