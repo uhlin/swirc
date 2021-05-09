1ebe6bfb55697714b6cb07af2fafa19f2fa9f49e
Markus Uhlin <markus.uhlin@bredband.net>
2021-05-09 20:29:37 +0200

    start_on_a_new_row: fixed function

--- src/printtext.cpp
+++ src/printtext.cpp
@@ -665,8 +665,7 @@ do_indent(WINDOW *win, const int indent, int *insert_count)
 static bool
 start_on_a_new_row(const ptrdiff_t sum, const WINDOW *win)
 {
-    //return (sum < (COLS - 1) ? false : true);
-    return (sum < win->_maxx ? false : true);
+	return (sum < win->_maxx ? false : true);
 }
 
 /**
