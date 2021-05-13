9535ad9282d1f4beef2b0acdd86008336afc572a
Markus Uhlin <markus.uhlin@bredband.net>
2021-05-13 19:01:43 +0200

    Fixed header

--- src/init_once.h
+++ src/init_once.h
@@ -2,13 +2,15 @@
 #define INIT_ONCE_H
 
 enum {
-    ONCE_INITIALIZER,
-    ONCE_EXCHANGE_VALUE
+	ONCE_INITIALIZER,
+	ONCE_EXCHANGE_VALUE
 };
 
 typedef volatile long int init_once_t;
 typedef void (*PTR_TO_INIT_ROUTINE)(void);
 
-int init_once(init_once_t *once_control, PTR_TO_INIT_ROUTINE init_routine);
+__SWIRC_BEGIN_DECLS
+int	init_once(init_once_t *, PTR_TO_INIT_ROUTINE);
+__SWIRC_END_DECLS
 
 #endif
