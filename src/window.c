d99ebc55814d79318df12332a3b5d0826d3376d1
Markus Uhlin <markus.uhlin@bredband.net>
2021-05-23 23:06:18 +0200

    change_window_by_label: added wrefresh() calls

--- src/window.c
+++ src/window.c
@@ -457,11 +457,14 @@ change_window_by_label(const char *label)
 		return ENOENT; /* window not found */
 	else if (window == g_active_window)
 		return 0; /* window already active */
-	else if (top_panel(window->pan) == ERR)
-		return EPERM; /* top_panel() error */
+	else if (top_panel(window->pan) == ERR ||
+		 wrefresh(panel_window(window->pan)) == ERR)
+		return EPERM;
 
-	if (window->nicklist.pan != NULL)
+	if (window->nicklist.pan != NULL) {
 		(void) top_panel(window->nicklist.pan);
+		(void) wrefresh(panel_window(window->nicklist.pan));
+	}
 
 	WINDOW *pwin = readline_get_active_pwin();
 	char *prompt = NULL;
