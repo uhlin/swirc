7787979e031bc5155d9b38c30c31c7113183e396
Markus Uhlin <markus.uhlin@bredband.net>
2021-06-13 17:22:03 +0200

    new_session: fixed function

--- src/readline.c
+++ src/readline.c
@@ -498,24 +498,24 @@ isInCirculationMode(const TAB_COMPLETION *tc)
 static struct readline_session_context *
 new_session(const char *prompt)
 {
-    struct readline_session_context *ctx = xcalloc(sizeof *ctx, 1);
-    char *prompt_copy = sw_strdup(prompt);
+	char *prompt_copy = sw_strdup(prompt);
+	struct readline_session_context *ctx = xcalloc(sizeof *ctx, 1);
 
-    ctx->buffer = xcalloc(readline_bufsize + 1, sizeof(wchar_t));
-    ctx->bufpos = 0;
-    ctx->n_insert = 0;
-    ctx->insert_mode = false;
-    ctx->no_bufspc = false;
-    ctx->prompt = sw_strdup(prompt);
-    ctx->prompt_size = (int) strlen(squeeze_text_deco(prompt_copy));
-    ctx->act = panel_window(readline_pan1);
-    ctx->tc = readline_tab_comp_ctx_new();
+	ctx->buffer      = xcalloc(readline_bufsize + 1, sizeof(wchar_t));
+	ctx->bufpos      = 0;
+	ctx->n_insert    = 0;
+	ctx->insert_mode = false;
+	ctx->no_bufspc   = false;
+	ctx->prompt      = sw_strdup(prompt);
+	ctx->prompt_size = (int) strlen(squeeze_text_deco(prompt_copy));
+	ctx->act         = panel_window(readline_pan1);
+	ctx->tc          = readline_tab_comp_ctx_new();
 
-    free(prompt_copy);
-    panel_state = PANEL1_ACTIVE;
-    readline_top_panel();
+	free(prompt_copy);
 
-    return ctx;
+	panel_state = PANEL1_ACTIVE;
+	readline_top_panel();
+	return ctx;
 }
 
 /**
