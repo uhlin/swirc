9488804a55534e6f451bf3ec9ce878e75ce6f20f
Markus Uhlin <markus.uhlin@bredband.net>
2021-06-14 23:08:18 +0200

    Deleted alignment

--- src/readline.h
+++ src/readline.h
@@ -24,13 +24,13 @@ typedef enum {
 } rl_active_panel_t;
 
 enum { /* custom, additional keys */
-    MY_KEY_BS	  = '\010',	/* ^H */
-    MY_KEY_DEL	  = '\177',	/* ^? */
-    MY_KEY_EOT	  = '\004',	/* ^D */
-    MY_KEY_ACK	  = '\006',	/* ^F */
-    MY_KEY_STX	  = '\002',	/* ^B */
-    MY_KEY_SO	  = '\016',	/* ^N */
-    MY_KEY_DLE	  = '\020',	/* ^P */
+    MY_KEY_BS = '\010', /* ^H */
+    MY_KEY_DEL = '\177', /* ^? */
+    MY_KEY_EOT = '\004', /* ^D */
+    MY_KEY_ACK = '\006', /* ^F */
+    MY_KEY_STX = '\002', /* ^B */
+    MY_KEY_SO = '\016', /* ^N */
+    MY_KEY_DLE = '\020', /* ^P */
     MY_KEY_RESIZE = '\033',
     CTRL_A = '\001',
     CTRL_E = '\005',
@@ -53,21 +53,21 @@ typedef struct tagTAB_COMPLETION {
 } TAB_COMPLETION, *PTAB_COMPLETION;
 
 struct readline_session_context {
-    wchar_t         *buffer;
-    int              bufpos;
-    int              n_insert;
-    bool             insert_mode;
-    bool             no_bufspc;
-    char            *prompt;
-    int              prompt_size;
-    WINDOW          *act;
-    PTAB_COMPLETION  tc;
+    wchar_t *buffer;
+    int bufpos;
+    int n_insert;
+    bool insert_mode;
+    bool no_bufspc;
+    char *prompt;
+    int prompt_size;
+    WINDOW *act;
+    PTAB_COMPLETION tc;
 };
 
 __SWIRC_BEGIN_DECLS
-extern bool	 g_readline_loop;
-extern bool	 g_resize_requested;
-extern jmp_buf	 g_readline_loc_info;
+extern bool g_readline_loop;
+extern bool g_resize_requested;
+extern jmp_buf g_readline_loc_info;
 
 extern bool g_hist_next;
 extern bool g_hist_prev;
