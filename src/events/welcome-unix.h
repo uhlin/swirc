8eba43c6047200f35852f12a4cd3dee71456d948
Markus Uhlin <markus.uhlin@bredband.net>
2021-06-19 00:26:47 +0200

    Fixed header

--- src/events/welcome-unix.h
+++ src/events/welcome-unix.h
@@ -1,17 +1,11 @@
 #ifndef WELCOME_UNIX_H
 #define WELCOME_UNIX_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-bool event_welcome_is_signaled  (void);
-void event_welcome_cond_init    (void);
-void event_welcome_cond_destroy (void);
-void event_welcome_signalit     (void);
-
-#ifdef __cplusplus
-}
-#endif
+__SWIRC_BEGIN_DECLS
+bool	event_welcome_is_signaled(void);
+void	event_welcome_cond_init(void);
+void	event_welcome_cond_destroy(void);
+void	event_welcome_signalit(void);
+__SWIRC_END_DECLS
 
 #endif
