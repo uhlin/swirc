18dfa7fb98acb95a501697bf60a46317ca97f75f
Markus Uhlin <markus.uhlin@bredband.net>
2021-06-22 16:16:09 +0200

    net_recv_plain: fixed function

--- src/net-unix.c
+++ src/net-unix.c
@@ -51,32 +51,33 @@ int g_socket = -1;
 static pthread_t listen_thread_id;
 
 int
-net_recv_plain(struct network_recv_context *ctx,
-	       char *recvbuf, int recvbuf_size)
+net_recv_plain(struct network_recv_context *ctx, char *recvbuf,
+    int recvbuf_size)
 {
-    const int      maxfdp1 = ctx->sock + 1;
-    fd_set         readset;
-    int            bytes_received;
-    struct timeval tv = {
-	.tv_sec  = ctx->sec,
-	.tv_usec = ctx->microsec,
-    };
+	const int	maxfdp1 = (ctx->sock + 1);
+	fd_set		readset;
+	int		bytes_received;
+	struct timeval tv = {
+		.tv_sec = ctx->sec,
+		.tv_usec = ctx->microsec,
+	};
 
-    FD_ZERO(&readset);
-    FD_SET(ctx->sock, &readset);
+	FD_ZERO(&readset);
+	FD_SET(ctx->sock, &readset);
 
-    errno = 0;
+	errno = 0;
 
-    if (select(maxfdp1, &readset, NULL, NULL, &tv) == -1)
-	return (errno == EINTR ? 0 : -1);
-    else if (!FD_ISSET(ctx->sock, &readset)) /* No data to recv() */
-	return 0;
-    else if (bytes_received = recv(ctx->sock,recvbuf,recvbuf_size,ctx->flags),
-	     bytes_received == -1)
-	return (errno==EAGAIN || errno==EWOULDBLOCK || errno==EINTR ? 0 : -1);
-    else if (bytes_received == 0) /* Disconnected! */
-	return -1;
-    return bytes_received;
+	if (select(maxfdp1, &readset, NULL, NULL, &tv) == -1)
+		return (errno == EINTR ? 0 : -1);
+	else if (!FD_ISSET(ctx->sock, &readset)) /* No data to recv() */
+		return 0;
+	else if ((bytes_received = recv(ctx->sock, recvbuf, recvbuf_size,
+	    ctx->flags)) == -1)
+		return (errno == EAGAIN || errno == EWOULDBLOCK ||
+		    errno == EINTR ? 0 : -1);
+	else if (bytes_received == 0) /* Disconnected! */
+		return -1;
+	return bytes_received;
 }
 
 int
