8c3e8434bd8e0be66720aeb7ec288fa2318d6434
Markus Uhlin <markus.uhlin@bredband.net>
2021-06-22 20:54:43 +0200

    Updated files

--- src/get_file.js
+++ src/get_file.js
@@ -14,6 +14,6 @@ function get_file(url, name)
 }
 
 get_file("https://curl.haxx.se/ca/cacert.pem", "src/trusted_roots.pem");
-get_file("https://nifty-networks.net/swirc/curl-7.67.0.cab", "curl-7.67.0.cab");
-get_file("https://nifty-networks.net/swirc/libressl-3.2.4-windows.cab", "libressl-3.2.4-windows.cab");
+get_file("https://nifty-networks.net/swirc/curl-7.77.0.cab", "curl-7.77.0.cab");
+get_file("https://nifty-networks.net/swirc/libressl-3.3.3-windows.cab", "libressl-3.3.3-windows.cab");
 get_file("https://nifty-networks.net/swirc/pdcurses-3.9.cab", "pdcurses-3.9.cab");
