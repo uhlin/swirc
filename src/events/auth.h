59d86723bda8ac626cfa52c0012817667ea615b5
Markus Uhlin <markus.uhlin@bredband.net>
2021-08-05 12:55:56 +0200

    Fixed header

--- src/events/auth.h
+++ src/events/auth.h
@@ -1,8 +1,10 @@
 #ifndef AUTH_H
 #define AUTH_H
 
-void event_authenticate(struct irc_message_compo *);
-void handle_sasl_auth_fail(struct irc_message_compo *);
-void sasl_auth_success(struct irc_message_compo *);
+__SWIRC_BEGIN_DECLS
+void	event_authenticate(struct irc_message_compo *);
+void	handle_sasl_auth_fail(struct irc_message_compo *);
+void	sasl_auth_success(struct irc_message_compo *);
+__SWIRC_END_DECLS
 
 #endif
