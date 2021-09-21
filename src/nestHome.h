b4127d5cb71714e4c708149feccd750eaab4964e
Markus Uhlin <markus.uhlin@bredband.net>
2021-09-21 16:11:11 +0200

    Fixed header

--- src/nestHome.h
+++ src/nestHome.h
@@ -1,29 +1,23 @@
 #ifndef NEST_HOME_H
 #define NEST_HOME_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+__SWIRC_BEGIN_DECLS
+extern char	*g_home_dir;
+extern char	*g_tmp_dir;
+extern char	*g_log_dir;
 
-extern char *g_home_dir;
-extern char *g_tmp_dir;
-extern char *g_log_dir;
+extern const char	g_config_filesuffix[];
+extern const char	g_theme_filesuffix[];
 
-extern const char g_config_filesuffix[];
-extern const char g_theme_filesuffix[];
+extern char	*g_config_file;
+extern char	*g_theme_file;
 
-extern char *g_config_file;
-extern char *g_theme_file;
-
-void nestHome_init   (void);
-void nestHome_deinit (void);
+void	nestHome_init(void);
+void	nestHome_deinit(void);
 
 /*lint -sem(path_to_home, r_null) */
 
-const char *path_to_home(void);
-
-#ifdef __cplusplus
-}
-#endif
+const char	*path_to_home(void);
+__SWIRC_END_DECLS
 
 #endif
