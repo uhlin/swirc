6a5ef0d5b182b8ed59d407ee7ea503bd59391c65
Markus Uhlin <markus.uhlin@bredband.net>
2021-09-24 21:47:32 +0200

    is_valid_hostname: fixed function

--- src/dataClassify.c
+++ src/dataClassify.c
@@ -1,5 +1,5 @@
 /* Data classification utilities
-   Copyright (C) 2012-2018 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2021 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -176,19 +176,17 @@ is_valid_real_name(const char *real_name)
 bool
 is_valid_hostname(const char *hostname)
 {
-    const char host_chars[] =
-	"abcdefghijklmnopqrstuvwxyz.0123456789-ABCDEFGHIJKLMNOPQRSTUVWXYZ:";
+	const char host_chars[] =
+	    "abcdefghijklmnopqrstuvwxyz.0123456789-ABCDEFGHIJKLMNOPQRSTUVWXYZ:";
 
-    if (hostname == NULL || *hostname == '\0' ||
-	strlen(hostname) > hostname_len_max) {
-	return false;
-    }
+	if (hostname == NULL || *hostname == '\0' ||
+	    strlen(hostname) > hostname_len_max)
+		return false;
 
-    for (const char *ccp = &hostname[0]; *ccp != '\0'; ccp++) {
-	if (strchr(host_chars, *ccp) == NULL) {
-	    return false;
+	for (const char *ccp = &hostname[0]; *ccp != '\0'; ccp++) {
+		if (strchr(host_chars, *ccp) == NULL)
+			return false;
 	}
-    }
 
-    return true;
+	return true;
 }
