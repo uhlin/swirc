a19cd48412c296da102479846ed1432dd5be1e64
Markus Uhlin <markus.uhlin@bredband.net>
2021-09-26 05:38:18 +0200

    Added command usage for /ignore and /unignore

--- src/commands/ignore.cpp
+++ src/commands/ignore.cpp
@@ -1,12 +1,18 @@
 #include "common.h"
 #include "ignore.h"
 
+/*
+ * usage: /ignore [regex]
+ */
 void
 cmd_ignore(const char *data)
 {
     (void) data;
 }
 
+/*
+ * usage: /unignore [#]
+ */
 void
 cmd_unignore(const char *data)
 {
