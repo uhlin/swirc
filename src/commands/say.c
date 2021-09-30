d684f4c1519b93d3b362d2aa7a3c95e4353336c3
Markus Uhlin <markus.uhlin@bredband.net>
2021-09-30 23:36:20 +0200

    Reindent

--- src/commands/say.c
+++ src/commands/say.c
@@ -6,18 +6,20 @@
 
 #include "say.h"
 
-/* usage: /say <message> */
+/*
+ * usage: /say <message>
+ */
 void
 cmd_say(const char *data)
 {
-    PRINTTEXT_CONTEXT ctx;
+	PRINTTEXT_CONTEXT	ctx;
 
-    printtext_context_init(&ctx, g_active_window, TYPE_SPEC1_FAILURE, true);
+	printtext_context_init(&ctx, g_active_window, TYPE_SPEC1_FAILURE, true);
 
-    if (strings_match(data, ""))
-	printtext(&ctx, "/say: missing arguments");
-    else if (strings_match_ignore_case(ACTWINLABEL, g_status_window_label))
-	printtext(&ctx, "/say: cannot say to status window");
-    else
-	transmit_user_input(ACTWINLABEL, data);
+	if (strings_match(data, ""))
+		printtext(&ctx, "/say: missing arguments");
+	else if (strings_match_ignore_case(ACTWINLABEL, g_status_window_label))
+		printtext(&ctx, "/say: cannot say to status window");
+	else
+		transmit_user_input(ACTWINLABEL, data);
 }
