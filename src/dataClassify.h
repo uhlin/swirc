71f36de50a0fb62710a280fac1f37f2423be87a9
Markus Uhlin <markus.uhlin@bredband.net>
2021-10-11 20:07:54 +0200

    Fixed header

--- src/dataClassify.h
+++ src/dataClassify.h
@@ -1,18 +1,16 @@
 #ifndef DATA_CLASSIFY_H
 #define DATA_CLASSIFY_H
 
-/*lint -sem(is_irc_channel, pure) */
-
 __SWIRC_BEGIN_DECLS
-bool	is_alphabetic      (const char *string);
-bool	is_numeric         (const char *string);
-bool	is_whiteSpace      (const char *string);
-bool	is_irc_channel     (const char *name) /* DOES_NOT_WRITE_GLOBAL_DATA */;
-bool	is_valid_uMode     (const char *modes);
-bool	is_valid_nickname  (const char *nickname);
-bool	is_valid_username  (const char *username);
-bool	is_valid_real_name (const char *real_name);
-bool	is_valid_hostname  (const char *hostname);
+bool	is_alphabetic(const char *);
+bool	is_numeric(const char *);
+bool	is_whiteSpace(const char *);
+bool	is_irc_channel(const char *);
+bool	is_valid_uMode(const char *);
+bool	is_valid_nickname(const char *);
+bool	is_valid_username(const char *);
+bool	is_valid_real_name(const char *);
+bool	is_valid_hostname(const char *);
 __SWIRC_END_DECLS
 
 /* Inline function definitions
@@ -22,7 +20,7 @@ __SWIRC_END_DECLS
 static SW_INLINE bool
 isNull(const void *data)
 {
-    return (data == NULL);
+	return (data == NULL);
 }
 #else
 #define isNull(_obj) ((_obj) == NULL)
@@ -31,7 +29,7 @@ isNull(const void *data)
 static SW_INLINE bool
 isEmpty(const char *data)
 {
-    return (*data == '\0');
+	return (*data == '\0');
 }
 
 #include <ctype.h>
@@ -41,43 +39,43 @@ isEmpty(const char *data)
 static SW_INLINE int
 sw_isalnum(int c)
 {
-    return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isalnum(c));
+	return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isalnum(c));
 }
 
 static SW_INLINE int
 sw_isalpha(int c)
 {
-    return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isalpha(c));
+	return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isalpha(c));
 }
 
 static SW_INLINE int
 sw_isdigit(int c)
 {
-    return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isdigit(c));
+	return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isdigit(c));
 }
 
 static SW_INLINE int
 sw_isspace(int c)
 {
-    return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isspace(c));
+	return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isspace(c));
 }
 
 static SW_INLINE int
 sw_isprint(int c)
 {
-    return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isprint(c));
+	return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isprint(c));
 }
 
 static SW_INLINE int
 sw_isupper(int c)
 {
-    return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isupper(c));
+	return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : isupper(c));
 }
 
 static SW_INLINE int
 sw_islower(int c)
 {
-    return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : islower(c));
+	return ((c == EOF || c < 0 || c > UCHAR_MAX) ? false : islower(c));
 }
 
 #endif /* DATA_CLASSIFY_H */
