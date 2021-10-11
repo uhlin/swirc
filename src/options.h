e8b1380981ac5e9e922cf504821c511b3bd4d4b6
Markus Uhlin <markus.uhlin@bredband.net>
2021-10-11 20:43:12 +0200

    Fixed header

--- src/options.h
+++ src/options.h
@@ -1,23 +1,17 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 enum {
-    UNRECOGNIZED_OPTION = '?',
-    OPTION_ARG_MISSING  = ':'
+	UNRECOGNIZED_OPTION = '?',
+	OPTION_ARG_MISSING = ':'
 };
 
+__SWIRC_BEGIN_DECLS
 extern int	 g_option_index;
 extern int	 g_option_save;
 extern char	*g_option_arg;
 
-int options(int argc, char *argv[], const char *optstring);
-
-#ifdef __cplusplus
-}
-#endif
+int	options(int, char *[], const char *);
+__SWIRC_END_DECLS
 
 #endif
