2c47ed73970ad8c651835512178c7ea0098a2ce6
Markus Uhlin <markus.uhlin@bredband.net>
2021-10-11 17:53:56 +0200

    textBuf_ins_prev: fixed function

--- src/textBuffer.c
+++ src/textBuffer.c
@@ -81,39 +81,40 @@ textBuf_ins_next(PTEXTBUF buf, PTEXTBUF_ELMT element,
 }
 
 int
-textBuf_ins_prev(PTEXTBUF buf, PTEXTBUF_ELMT element,
-		 const char *text, int indent)
+textBuf_ins_prev(PTEXTBUF buf, PTEXTBUF_ELMT element, const char *text,
+    int indent)
 {
-    PTEXTBUF_ELMT new_element;
+	PTEXTBUF_ELMT	new_element;
 
-    if (buf == NULL || text == NULL ||
-	(element == NULL && textBuf_size(buf) != 0)) {
-	return EINVAL;
-    }
+	if (buf == NULL || text == NULL ||
+	    (element == NULL && textBuf_size(buf) != 0))
+		return EINVAL;
 
-    new_element         = xcalloc(sizeof *new_element, 1);
-    new_element->text   = sw_strdup(text);
-    new_element->indent = indent;
+	new_element = xcalloc(sizeof *new_element, 1);
+	new_element->text = sw_strdup(text);
+	new_element->indent = indent;
 
-    if (textBuf_size(buf) == 0) {
-	buf->head       = new_element;
-	buf->head->prev = NULL;
-	buf->head->next = NULL;
-	buf->tail       = new_element;
-    } else {
-	sw_assert(element != NULL);
-	new_element->next = element;
-	new_element->prev = element->prev;
-	if (element->prev == NULL) {
-	    buf->head = new_element;
+	if (textBuf_size(buf) == 0) {
+		buf->head = new_element;
+		buf->head->prev = NULL;
+		buf->head->next = NULL;
+		buf->tail = new_element;
 	} else {
-	    element->prev->next = new_element;
-	}
-	element->prev = new_element;
-    }
+		sw_assert(element != NULL);
 
-    (buf->size)++;
-    return 0;
+		new_element->next = element;
+		new_element->prev = element->prev;
+
+		if (element->prev == NULL)
+			buf->head = new_element;
+		else
+			element->prev->next = new_element;
+
+		element->prev = new_element;
+	}
+
+	(buf->size)++;
+	return 0;
 }
 
 int
