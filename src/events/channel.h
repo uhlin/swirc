60f9ddf9da570480768cae23453d3cbc261ecccf
Markus Uhlin <markus.uhlin@bredband.net>
2021-10-16 12:27:51 +0200

    Fixed header

--- src/events/channel.h
+++ src/events/channel.h
@@ -1,29 +1,23 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef enum {
-    STATE_PLUS,
-    STATE_MINUS,
-    STATE_NEITHER_PM
+	STATE_PLUS,
+	STATE_MINUS,
+	STATE_NEITHER_PM
 } plus_minus_state_t;
 
-void event_chan_hp       (struct irc_message_compo *);
-void event_join          (struct irc_message_compo *);
-void event_kick          (struct irc_message_compo *);
-void event_mode          (struct irc_message_compo *);
-void event_nick          (struct irc_message_compo *);
-void event_part          (struct irc_message_compo *);
-void event_quit          (struct irc_message_compo *);
-void event_topic         (struct irc_message_compo *);
-void event_topic_chg     (struct irc_message_compo *);
-void event_topic_creator (struct irc_message_compo *);
-
-#ifdef __cplusplus
-}
-#endif
+__SWIRC_BEGIN_DECLS
+void	event_chan_hp(struct irc_message_compo *);
+void	event_join(struct irc_message_compo *);
+void	event_kick(struct irc_message_compo *);
+void	event_mode(struct irc_message_compo *);
+void	event_nick(struct irc_message_compo *);
+void	event_part(struct irc_message_compo *);
+void	event_quit(struct irc_message_compo *);
+void	event_topic(struct irc_message_compo *);
+void	event_topic_chg(struct irc_message_compo *);
+void	event_topic_creator(struct irc_message_compo *);
+__SWIRC_END_DECLS
 
 #endif
