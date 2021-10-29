efef9aa22b75ec64250470e01f1ebe8a4657ce6c
Markus Uhlin <markus.uhlin@bredband.net>
2021-10-29 23:36:32 +0200

    handle_open_msg_packet: fixed function

--- src/icb.c
+++ src/icb.c
@@ -136,24 +136,24 @@ login_ok(void)
 static void
 handle_open_msg_packet(const char *pktdata)
 {
-    char	*last         = "";
-    char	*message      = NULL;
-    char	*nickname     = NULL;
-    char	*pktdata_copy = sw_strdup(pktdata);
+	char	*last = "";
+	char	*nickname, *message;
+	char	*pktdata_copy = sw_strdup(pktdata);
 
-    nickname = strtok_r(pktdata_copy, ICB_FIELD_SEP, &last);
-    message = strtok_r(NULL, ICB_FIELD_SEP, &last);
+	if ((nickname = strtok_r(pktdata_copy, ICB_FIELD_SEP, &last)) == NULL ||
+	    (message = strtok_r(NULL, ICB_FIELD_SEP, &last)) == NULL) {
+		print_and_free("handle_open_msg_packet: too few tokens!",
+		    pktdata_copy);
+		return;
+	} else if (icb_group == NULL) {
+		print_and_free("handle_open_msg_packet: not in a group",
+		    pktdata_copy);
+		return;
+	}
 
-    if (isNull(nickname) || isNull(message)) {
-	print_and_free("handle_open_msg_packet: too few tokens!", pktdata_copy);
-	return;
-    } else if (isNull(icb_group)) {
-	print_and_free("handle_open_msg_packet: not in a group", pktdata_copy);
-	return;
-    }
+	process_event(":%s PRIVMSG #%s :%s\r\n", nickname, icb_group, message);
 
-    process_event(":%s PRIVMSG #%s :%s\r\n", nickname, icb_group, message);
-    free_and_null(&pktdata_copy);
+	free(pktdata_copy);
 }
 
 static void
