442d9dec765ee90b265db0cc207a5ca70c080649
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-01 21:23:08 +0100

    Reformatting

--- src/events/banlist.h
+++ src/events/banlist.h
@@ -1,15 +1,15 @@
 #ifndef GUARD_BANLIST_H
 #define GUARD_BANLIST_H
 
-#define event_inviteList event_banlist
-#define event_exceptList event_banlist
+#define event_inviteList	event_banlist
+#define event_exceptList	event_banlist
 
-#define event_eof_inviteList event_eof_banlist
-#define event_eof_exceptList event_eof_banlist
+#define event_eof_inviteList	event_eof_banlist
+#define event_eof_exceptList	event_eof_banlist
 
 __SWIRC_BEGIN_DECLS
-void event_banlist     (struct irc_message_compo *);
-void event_eof_banlist (struct irc_message_compo *);
+void	event_banlist(struct irc_message_compo *);
+void	event_eof_banlist(struct irc_message_compo *);
 __SWIRC_END_DECLS
 
 #endif
