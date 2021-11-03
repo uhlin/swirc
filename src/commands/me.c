abf3b999c197ac6b4b6cb1ff330cceae6fd7e229
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-03 02:10:24 +0100

    cmd_me: fixed function

--- src/commands/me.c
+++ src/commands/me.c
@@ -7,24 +7,27 @@
 
 #include "me.h"
 
-/* usage: /me <message> */
+/*
+ * usage: /me <message>
+ */
 void
 cmd_me(const char *data)
 {
-    PRINTTEXT_CONTEXT ctx;
+	PRINTTEXT_CONTEXT	ctx;
 
-    printtext_context_init(&ctx, g_active_window, TYPE_SPEC1_FAILURE, true);
+	printtext_context_init(&ctx, g_active_window, TYPE_SPEC1_FAILURE, true);
 
-    if (strings_match(data, "")) {
-	printtext(&ctx, "/me: missing arguments");
-    } else if (strings_match_ignore_case(ACTWINLABEL, g_status_window_label)) {
-	printtext(&ctx, "/me: cannot send to status window!");
-    } else {
-	ctx.spec_type = TYPE_SPEC_NONE;
+	if (strings_match(data, "")) {
+		printtext(&ctx, "/me: missing arguments");
+	} else if (strings_match_ignore_case(ACTWINLABEL,
+	    g_status_window_label)) {
+		printtext(&ctx, "/me: cannot send to status window!");
+	} else if (net_send("PRIVMSG %s :\001ACTION %s\001", ACTWINLABEL,
+	    data) < 0) {
+		printtext(&ctx, "/me: cannot send!");
+	} else {
+		ctx.spec_type = TYPE_SPEC_NONE;
 
-	if (net_send("PRIVMSG %s :\001ACTION %s\001", ACTWINLABEL, data) < 0)
-	    g_on_air = false;
-	else
-	    printtext(&ctx, " - %s %s", g_my_nickname, data);
-    }
+		printtext(&ctx, " - %s %s", g_my_nickname, data);
+	}
 }
