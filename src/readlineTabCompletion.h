3702fd3ff8eb01ed6eaf52022eadec698802a4b8
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-07 15:56:37 +0100

    Deleted parameter names

--- src/readlineTabCompletion.h
+++ src/readlineTabCompletion.h
@@ -3,8 +3,8 @@
 
 __SWIRC_BEGIN_DECLS
 PTAB_COMPLETION readline_tab_comp_ctx_new(void);
-void readline_tab_comp_ctx_destroy(PTAB_COMPLETION ctx);
-void readline_tab_comp_ctx_reset(PTAB_COMPLETION ctx);
+void readline_tab_comp_ctx_destroy(PTAB_COMPLETION);
+void readline_tab_comp_ctx_reset(PTAB_COMPLETION);
 
 void readline_handle_tab(volatile struct readline_session_context *);
 __SWIRC_END_DECLS
