28514d2cab59bbad21f10e2c05fac1f39b874330
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-09 14:13:46 +0100

    Fixed header

--- src/commands/services.h
+++ src/commands/services.h
@@ -1,7 +1,9 @@
 #ifndef CMD_SERVICES_H
 #define CMD_SERVICES_H
 
-void cmd_chanserv(const char *);
-void cmd_nickserv(const char *);
+__SWIRC_BEGIN_DECLS
+void	cmd_chanserv(const char *);
+void	cmd_nickserv(const char *);
+__SWIRC_END_DECLS
 
 #endif
