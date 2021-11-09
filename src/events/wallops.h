31638004de2270be7c05360155ca93e6221c64b4
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-09 22:27:52 +0100

    Fixed header

--- src/events/wallops.h
+++ src/events/wallops.h
@@ -1,14 +1,8 @@
 #ifndef WALLOPS_H
 #define WALLOPS_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void event_wallops(struct irc_message_compo *) PTR_ARGS_NONNULL;
-
-#ifdef __cplusplus
-}
-#endif
+__SWIRC_BEGIN_DECLS
+void	event_wallops(struct irc_message_compo *) PTR_ARGS_NONNULL;
+__SWIRC_END_DECLS
 
 #endif
