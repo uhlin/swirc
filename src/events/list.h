f7235a28b3beee8db1737c2ab39bbc4240c4c381
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-12 12:20:36 +0100

    Added begin/end decls

--- src/events/list.h
+++ src/events/list.h
@@ -1,7 +1,9 @@
 #ifndef GUARD_LIST_H
 #define GUARD_LIST_H
 
-void event_liststart (struct irc_message_compo *);
-void event_list      (struct irc_message_compo *);
+__SWIRC_BEGIN_DECLS
+void	event_liststart(struct irc_message_compo *);
+void	event_list(struct irc_message_compo *);
+__SWIRC_END_DECLS
 
 #endif
