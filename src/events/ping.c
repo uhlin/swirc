703629b0696113082310cfe7c5d608597684be98
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-12 11:51:13 +0100

    Reformatting

--- src/events/ping.c
+++ src/events/ping.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014, 2016 Markus Uhlin. All rights reserved. */
+/* Copyright (C) 2014-2021 Markus Uhlin. All rights reserved. */
 
 #include "common.h"
 
@@ -13,23 +13,21 @@
 void
 event_ping(struct irc_message_compo *compo)
 {
-    char *cp = &compo->params[0];
-    int n_sent;
+	char	*cp;
+	int	 n_sent;
 
-    if (*cp == ':') {
-	cp++;
-    }
+	if (*(cp = &compo->params[0]) == ':')
+		cp++;
 
-    if (strings_match(cp, "")) {
-	return;
-    } else if ((n_sent = net_send("PONG %s", cp)) == -1) {
-	g_on_air = false;
-    } else if (n_sent > 0 && config_bool("show_ping_pong", true)) {
-	PRINTTEXT_CONTEXT ctx;
+	if (strings_match(cp, "")) {
+		return;
+	} else if ((n_sent = net_send("PONG %s", cp)) == -1) {
+		g_on_air = false;
+	} else if (n_sent > 0 && config_bool("show_ping_pong", true)) {
+		PRINTTEXT_CONTEXT	ctx;
 
-	printtext_context_init(&ctx, g_status_window, TYPE_SPEC_NONE, true);
-	printtext(&ctx, "PING? PONG!");
-    } else {
-	/* do nothing */;
-    }
+		printtext_context_init(&ctx, g_status_window, TYPE_SPEC_NONE,
+		    true);
+		printtext(&ctx, "PING? PONG!");
+	}
 }
