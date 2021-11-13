ebdb75322515a31c997cf74715eb18f3a27b6756
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-13 16:46:38 +0100

    event_noop: fixed function

--- src/events/noop.c
+++ src/events/noop.c
@@ -8,11 +8,13 @@
 void
 event_noop(struct irc_message_compo *compo)
 {
-    PRINTTEXT_CONTEXT ctx;
+	PRINTTEXT_CONTEXT	ctx;
 
-    printtext_context_init(&ctx, g_status_window, TYPE_SPEC3, true);
-    printtext(&ctx, "Got event %s. Currently a noop (aka no operation)",
-	      compo->command);
-    printtext(&ctx, "params = %s", compo->params);
-    printtext(&ctx, "prefix = %s", compo->prefix ? compo->prefix : "none");
+	printtext_context_init(&ctx, g_status_window, TYPE_SPEC3, true);
+
+	printtext(&ctx, "Got event %s. Currently a noop (aka no operation)",
+	    compo->command);
+	printtext(&ctx, "params = %s", compo->params);
+	printtext(&ctx, "prefix = %s", (compo->prefix ? compo->prefix :
+	    "none"));
 }
