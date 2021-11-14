11017ae720d8e975d21336a5be0f10f3a9dc5616
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-14 13:10:47 +0100

    Added test

--- tests/xstrnlen.c
+++ tests/xstrnlen.c
@@ -5,13 +5,23 @@
 
 #include "strHand.h"
 
+static size_t ret;
+
 static void
 nullString_test(void **state)
 {
-	size_t ret;
-
 	ret = xstrnlen(NULL, SIZE_MAX);
 	assert_true(ret == 0);
+}
+
+static void
+noNullChar_test(void **state)
+{
+	char buf[10];
+
+	memset(buf, 'Z', sizeof buf);
+	ret = xstrnlen(buf, sizeof buf);
+	assert_true(ret == sizeof buf);
 }
 
 int
@@ -19,6 +29,7 @@ main(void)
 {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(nullString_test),
+		cmocka_unit_test(noNullChar_test),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
