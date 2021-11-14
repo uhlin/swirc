3aa221e068f0e5814a2cfb5707e5c8ba36e26fe3
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-14 20:30:04 +0100

    event_motd: indent using tabs

--- src/events/motd.c
+++ src/events/motd.c
@@ -8,9 +8,7 @@
 void
 event_motd(struct irc_message_compo *compo)
 {
-    if (config_bool("skip_motd", false)) {
-	return;
-    }
-
-    irc_extract_msg(compo, g_status_window, 1, false);
+	if (config_bool("skip_motd", false))
+		return;
+	irc_extract_msg(compo, g_status_window, 1, false);
 }
