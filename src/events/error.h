43d2b65c3eedcba7cd2b829b978670d8ff3cc9b1
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-17 21:26:01 +0100

    Added begin+end decls

--- src/events/error.h
+++ src/events/error.h
@@ -1,6 +1,8 @@
 #ifndef GUARD_ERROR_H
 #define GUARD_ERROR_H
 
-void event_error(struct irc_message_compo *);
+__SWIRC_BEGIN_DECLS
+void	event_error(struct irc_message_compo *);
+__SWIRC_END_DECLS
 
 #endif
