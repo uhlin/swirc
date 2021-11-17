9e768521d5f9f4e119e40a3a6f5b153c2678aa31
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-18 00:40:18 +0100

    Changed header file

--- src/events/cap.h
+++ src/events/cap.h
@@ -1,16 +1,10 @@
 #ifndef CAP_H
 #define CAP_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-bool		 is_sasl_mechanism_supported(const char *mechanism);
+__SWIRC_BEGIN_DECLS
+bool		 is_sasl_mechanism_supported(const char *);
 const char	*get_sasl_mechanism(void);
 void		 event_cap(struct irc_message_compo *);
-
-#ifdef __cplusplus
-}
-#endif
+__SWIRC_END_DECLS
 
 #endif
