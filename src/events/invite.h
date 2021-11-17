92a8d3dfd2d5029dc10bdbafce30f044dbae2eb2
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-17 21:19:02 +0100

    Added alignment

--- src/events/invite.h
+++ src/events/invite.h
@@ -2,8 +2,8 @@
 #define GUARD_INVITE_H
 
 __SWIRC_BEGIN_DECLS
-void event_inviting(struct irc_message_compo *);
-void event_invite(struct irc_message_compo *);
+void	event_inviting(struct irc_message_compo *);
+void	event_invite(struct irc_message_compo *);
 __SWIRC_END_DECLS
 
 #endif
