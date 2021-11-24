037d55324e02157e1885d77fa388763c6f3e0f50
Markus Uhlin <markus.uhlin@bredband.net>
2021-11-24 13:55:40 +0100

    Fixed header file

--- src/commands/sasl-scram-sha.h
+++ src/commands/sasl-scram-sha.h
@@ -1,20 +1,14 @@
 #ifndef SASL_SCRAM_SHA_H
 #define SASL_SCRAM_SHA_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+__SWIRC_BEGIN_DECLS
+extern volatile bool	g_sasl_scram_sha_got_first_msg;
 
-extern volatile bool g_sasl_scram_sha_got_first_msg;
+int	sasl_scram_sha_send_client_first_msg(void);
+int	sasl_scram_sha_send_client_final_msg(const char *proof);
 
-int	 sasl_scram_sha_send_client_first_msg(void);
-int	 sasl_scram_sha_send_client_final_msg(const char *proof);
-
-int	 sasl_scram_sha_handle_serv_first_msg(const char *);
-int	 sasl_scram_sha_handle_serv_final_msg(const char *);
-
-#ifdef __cplusplus
-}
-#endif
+int	sasl_scram_sha_handle_serv_first_msg(const char *);
+int	sasl_scram_sha_handle_serv_final_msg(const char *);
+__SWIRC_END_DECLS
 
 #endif
