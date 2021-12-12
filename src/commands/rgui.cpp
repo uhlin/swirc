db847aca4d4856455e3ec6ca959b02e35f2f537b
Markus Uhlin <markus.uhlin@bredband.net>
2021-12-12 02:40:47 +0100

    Added temp behavior

--- src/commands/rgui.cpp
+++ src/commands/rgui.cpp
@@ -1,8 +1,15 @@
 #include "common.h"
+
+#include "../tls-server.h"
+
+#include "atomicops.h"
 #include "rgui.h"
 
 void
 cmd_rgui(const char *data)
 {
-	(void) data;
+	if (atomic_load_bool(&g_accepting_new_connections))
+		tls_server_end();
+	else
+		tls_server_begin(atoi(data));
 }
