68703681d6de7235c3e260779412f19373bfb90d
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-07 02:03:58 +0100

    already_is_in_names_hash: fixed function

--- src/events/names.cpp
+++ src/events/names.cpp
@@ -131,14 +131,14 @@ got_hits(const IRC_WINDOW *window, const char *search_var)
 static inline bool
 already_is_in_names_hash(const char *nick, PIRC_WINDOW window)
 {
-    for (PNAMES names = window->names_hash[hash(nick)];
-	 names != NULL;
-	 names = names->next) {
-	if (strings_match_ignore_case(nick, names->nick))
-	    return true;
-    }
+	for (PNAMES names = window->names_hash[hash(nick)];
+	    names != NULL;
+	    names = names->next) {
+		if (strings_match_ignore_case(nick, names->nick))
+			return true;
+	}
 
-    return false;
+	return false;
 }
 
 static int
