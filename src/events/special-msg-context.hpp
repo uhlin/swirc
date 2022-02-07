fc5b82255c312af8c7baa5df4418f125a2e5e723
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-07 13:33:53 +0100

    Reformatted struct

--- src/events/special-msg-context.hpp
+++ src/events/special-msg-context.hpp
@@ -2,29 +2,29 @@
 #define SPECIAL_MSG_CONTEXT_HPP
 
 struct special_msg_context {
-    char *nick;
-    char *user;
-    char *host;
-    char *dest;
-    char *msg;
+	char	*nick;
+	char	*user;
+	char	*host;
+	char	*dest;
+	char	*msg;
 
-    special_msg_context()
+	special_msg_context()
 	{
-	    this->nick = NULL;
-	    this->user = NULL;
-	    this->host = NULL;
-	    this->dest = NULL;
-	    this->msg = NULL;
+		this->nick	= NULL;
+		this->user	= NULL;
+		this->host	= NULL;
+		this->dest	= NULL;
+		this->msg	= NULL;
 	}
 
-    special_msg_context(char *nick, char *user, char *host, char *dest,
-			char *msg)
+	special_msg_context(char *nick, char *user, char *host, char *dest,
+	    char *msg)
 	{
-	    this->nick = nick;
-	    this->user = user;
-	    this->host = host;
-	    this->dest = dest;
-	    this->msg = msg;
+		this->nick	= nick;
+		this->user	= user;
+		this->host	= host;
+		this->dest	= dest;
+		this->msg	= msg;
 	}
 };
 
