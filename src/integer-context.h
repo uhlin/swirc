13eccbfb34043b0a8f27f6c8e788547ffcc10e72
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-07 12:51:27 +0100

    Reformatted struct

--- src/integer-context.h
+++ src/integer-context.h
@@ -2,19 +2,19 @@
 #define INTEGER_CONTEXT_H
 
 struct integer_context {
-    const char	*setting_name;  /* Name of the setting or item */
-    long int	 lo_limit;      /* Low limit */
-    long int	 hi_limit;      /* High limit */
-    long int	 fallback_default;	/* If the range is exceeded  --  use this value */
+	const char	*setting_name; /* Name of the setting or item */
+	long int	 lo_limit; /* Low limit */
+	long int	 hi_limit; /* High limit */
+	long int	 fallback_default; /* If the range is exceeded  --  use this value */
 
 #ifdef __cplusplus
-    integer_context(const char *setting_name, long int lo_limit, long int hi_limit,
-		    long int fallback_default)
+	integer_context(const char *setting_name,
+	    long int lo_limit, long int hi_limit, long int fallback_default)
 	{
-	    this->setting_name = setting_name;
-	    this->lo_limit = lo_limit;
-	    this->hi_limit = hi_limit;
-	    this->fallback_default = fallback_default;
+		this->setting_name = setting_name;
+		this->lo_limit = lo_limit;
+		this->hi_limit = hi_limit;
+		this->fallback_default = fallback_default;
 	}
 #endif
 };
