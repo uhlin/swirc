3bca88575bdc4a7cdd391dc0539925e89af763b2
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-09 23:05:47 +0100

    init_fg_on_bg_case2: fixed function

--- src/cursesInit.c
+++ src/cursesInit.c
@@ -88,17 +88,16 @@ static int
 init_fg_on_bg_case2(short int *pair_n)
 {
 #if LINUX
-    FOREACH_FOREGROUND_ANSI() {
-	FOREACH_BACKGROUND_EXTENDED() {
-	    if (init_pair(++ (*pair_n), *fg, *bg) == ERR)
-		return ERR;
+	FOREACH_FOREGROUND_ANSI() {
+		FOREACH_BACKGROUND_EXTENDED() {
+			if (init_pair(++ (*pair_n), *fg, *bg) == ERR)
+				return ERR;
+		}
 	}
-    }
 #else
-    (void) pair_n;
+	(void) pair_n;
 #endif
-
-    return OK;
+	return OK;
 }
 
 static int
