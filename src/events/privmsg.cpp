c242d8d706050d632952c33d00a06660f7eb7e68
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-11 01:06:53 +0100

    event_privmsg: made 'e' const ref

--- src/events/privmsg.cpp
+++ src/events/privmsg.cpp
@@ -407,7 +407,7 @@ event_privmsg(struct irc_message_compo *compo)
 
 			handle_chan_msgs(&ctx, nick, dest, msg);
 		}
-	} catch (std::runtime_error& e) {
+	} catch (const std::runtime_error& e) {
 		printtext_context_init(&ctx, g_status_window, TYPE_SPEC1_WARN,
 		    true);
 		printtext(&ctx, "event_privmsg: error: %s", e.what());
