6e5f4232b95e50e01dfe2d52db1f873182aca80c
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-21 15:23:41 +0100

    Changed file

--- src/commands/cleartoasts.cpp
+++ src/commands/cleartoasts.cpp
@@ -1,16 +1,29 @@
 #include "common.h"
+
 #ifdef WIN32
 #include "../DesktopNotificationManagerCompat.hpp"
 #include "../ToastsAPI.hpp"
 #endif
+
+#include "../errHand.h"
+#include "../printtext.h"
+#include "../strHand.h"
+
 #include "cleartoasts.h"
 
-/* usage: /cleartoasts */
+/*
+ * usage: /cleartoasts
+ */
 void
 cmd_cleartoasts(const char *data)
 {
+	if (!strings_match(data, "")) {
+		print_and_free("/cleartoasts: implicit trailing data", NULL);
+		return;
+	}
+
 #if defined(UNIX)
-	(void) 0;
+	debug("cmd_cleartoasts() called (currently a no-op)");
 #elif defined(WIN32)
 	Toasts::ClearToasts();
 #endif
