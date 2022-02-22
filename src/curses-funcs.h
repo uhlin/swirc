a673def57f5ec6a861c0a540b8d7390da9356e6c
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-22 16:06:18 +0100

    Changed error directive

--- src/curses-funcs.h
+++ src/curses-funcs.h
@@ -8,7 +8,7 @@
 #elif WIN32
 #include "pdcurses/curses.h"
 #else
-#error "Cannot determine curses header file!"
+#error Cannot determine curses header file!
 #endif
 
 typedef int (*PTR_TO_ENDWIN)(void);
