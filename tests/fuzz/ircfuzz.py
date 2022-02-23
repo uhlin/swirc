208e9922497d47589f075d51bb1f89d18c766cc7
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-23 19:01:34 +0100

    Moved 'from _thread'

--- tests/fuzz/ircfuzz.py
+++ tests/fuzz/ircfuzz.py
@@ -20,13 +20,13 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from _thread import *
 import os
 import random
 import socket
 import string
 import sys
 import time
+from _thread import *
 
 
 # change my_nick to reflect the client's nick
