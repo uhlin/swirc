2776f9668bb4429de86a99fc7e0351800fa20bde
Markus Uhlin <markus.uhlin@bredband.net>
2022-02-25 18:46:38 +0100

    Added alignment

--- src/events/names-htbl-modify.h
+++ src/events/names-htbl-modify.h
@@ -2,12 +2,13 @@
 #define NAMES_HTBL_MODIFY_H
 
 #ifdef __cplusplus
-namespace names_htbl_modify {
-	int owner(const char *, const char *, bool);
-	int superop(const char *, const char *, bool);
-	int op(const char *, const char *, bool);
-	int halfop(const char *, const char *, bool);
-	int voice(const char *, const char *, bool);
+namespace names_htbl_modify
+{
+	int	owner(const char *, const char *, bool);
+	int	superop(const char *, const char *, bool);
+	int	op(const char *, const char *, bool);
+	int	halfop(const char *, const char *, bool);
+	int	voice(const char *, const char *, bool);
 }
 #endif
 
