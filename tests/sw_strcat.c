f8f1971a672c8de3566b7ebac764672c4d0c17a1
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-04 04:04:05 +0100

    Reformatted files

--- tests/sw_strcat.c
+++ tests/sw_strcat.c
@@ -5,68 +5,68 @@
 
 #include "strHand.h"
 
-static char buf[7] = "foo";
-static int ret = 0;
+static char	buf[7] = "foo";
+static int	ret = 0;
 
 static void
 handlesInvalidArguments_test1(void **state)
 {
-    ret = sw_strcat(NULL, "bar", sizeof buf);
-    assert_int_equal(ret, EINVAL);
-    assert_string_equal(buf, "foo");
+	ret = sw_strcat(NULL, "bar", sizeof buf);
+	assert_int_equal(ret, EINVAL);
+	assert_string_equal(buf, "foo");
 }
 
 static void
 handlesInvalidArguments_test2(void **state)
 {
-    ret = sw_strcat(buf, NULL, sizeof buf);
-    assert_int_equal(ret, EINVAL);
-    assert_string_equal(buf, "foo");
+	ret = sw_strcat(buf, NULL, sizeof buf);
+	assert_int_equal(ret, EINVAL);
+	assert_string_equal(buf, "foo");
 }
 
 static void
 handlesInvalidArguments_test3(void **state)
 {
-    ret = sw_strcat(buf, "bar", 0);
-    assert_int_equal(ret, EINVAL);
-    assert_string_equal(buf, "foo");
+	ret = sw_strcat(buf, "bar", 0);
+	assert_int_equal(ret, EINVAL);
+	assert_string_equal(buf, "foo");
 }
 
 static void
 doesBoundsChecking_test1(void **state)
 {
-    ret = sw_strcat(buf, "bar!", sizeof buf);
-    assert_int_equal(ret, ERANGE);
-    assert_string_equal(buf, "foo");
+	ret = sw_strcat(buf, "bar!", sizeof buf);
+	assert_int_equal(ret, ERANGE);
+	assert_string_equal(buf, "foo");
 }
 
 static void
 doesBoundsChecking_test2(void **state)
 {
-    ret = sw_strcat(buf, "barbaz", sizeof buf);
-    assert_int_equal(ret, ERANGE);
-    assert_string_equal(buf, "foo");
+	ret = sw_strcat(buf, "barbaz", sizeof buf);
+	assert_int_equal(ret, ERANGE);
+	assert_string_equal(buf, "foo");
 }
 
 static void
 canConcatenate_test1(void **state)
 {
-    ret = sw_strcat(buf, "...", sizeof buf);
-    assert_int_equal(ret, 0);
-    assert_string_equal(buf, "foo...");
+	ret = sw_strcat(buf, "...", sizeof buf);
+	assert_int_equal(ret, 0);
+	assert_string_equal(buf, "foo...");
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(handlesInvalidArguments_test1),
-	cmocka_unit_test(handlesInvalidArguments_test2),
-	cmocka_unit_test(handlesInvalidArguments_test3),
-	cmocka_unit_test(doesBoundsChecking_test1),
-	cmocka_unit_test(doesBoundsChecking_test2),
-	cmocka_unit_test(canConcatenate_test1),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(handlesInvalidArguments_test1),
+		cmocka_unit_test(handlesInvalidArguments_test2),
+		cmocka_unit_test(handlesInvalidArguments_test3),
+		cmocka_unit_test(doesBoundsChecking_test1),
+		cmocka_unit_test(doesBoundsChecking_test2),
+		cmocka_unit_test(canConcatenate_test1),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
--- tests/sw_strcpy.c
+++ tests/sw_strcpy.c
@@ -5,68 +5,68 @@
 
 #include "strHand.h"
 
-static char buf[12] = "";
-static int ret = 0;
+static char	buf[12] = { '\0' };
+static int	ret = 0;
 
 static void
 handlesInvalidArguments_test1(void **state)
 {
-    ret = sw_strcpy(NULL, "some string", sizeof buf);
-    assert_int_equal(ret, EINVAL);
-    assert_string_equal(buf, "");
+	ret = sw_strcpy(NULL, "some string", sizeof buf);
+	assert_int_equal(ret, EINVAL);
+	assert_string_equal(buf, "");
 }
 
 static void
 handlesInvalidArguments_test2(void **state)
 {
-    ret = sw_strcpy(buf, NULL, sizeof buf);
-    assert_int_equal(ret, EINVAL);
-    assert_string_equal(buf, "");
+	ret = sw_strcpy(buf, NULL, sizeof buf);
+	assert_int_equal(ret, EINVAL);
+	assert_string_equal(buf, "");
 }
 
 static void
 handlesInvalidArguments_test3(void **state)
 {
-    ret = sw_strcpy(buf, "some string", 0);
-    assert_int_equal(ret, EINVAL);
-    assert_string_equal(buf, "");
+	ret = sw_strcpy(buf, "some string", 0);
+	assert_int_equal(ret, EINVAL);
+	assert_string_equal(buf, "");
 }
 
 static void
 doesBoundsChecking_test1(void **state)
 {
-    ret = sw_strcpy(buf, "some string!", sizeof buf);
-    assert_int_equal(ret, ERANGE);
-    assert_string_equal(buf, "");
+	ret = sw_strcpy(buf, "some string!", sizeof buf);
+	assert_int_equal(ret, ERANGE);
+	assert_string_equal(buf, "");
 }
 
 static void
 doesBoundsChecking_test2(void **state)
 {
-    ret = sw_strcpy(buf, "some string...", sizeof buf);
-    assert_int_equal(ret, ERANGE);
-    assert_string_equal(buf, "");
+	ret = sw_strcpy(buf, "some string...", sizeof buf);
+	assert_int_equal(ret, ERANGE);
+	assert_string_equal(buf, "");
 }
 
 static void
 canCopy_test1(void **state)
 {
-    ret = sw_strcpy(buf, "some string", sizeof buf);
-    assert_int_equal(ret, 0);
-    assert_string_equal(buf, "some string");
+	ret = sw_strcpy(buf, "some string", sizeof buf);
+	assert_int_equal(ret, 0);
+	assert_string_equal(buf, "some string");
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(handlesInvalidArguments_test1),
-	cmocka_unit_test(handlesInvalidArguments_test2),
-	cmocka_unit_test(handlesInvalidArguments_test3),
-	cmocka_unit_test(doesBoundsChecking_test1),
-	cmocka_unit_test(doesBoundsChecking_test2),
-	cmocka_unit_test(canCopy_test1),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(handlesInvalidArguments_test1),
+		cmocka_unit_test(handlesInvalidArguments_test2),
+		cmocka_unit_test(handlesInvalidArguments_test3),
+		cmocka_unit_test(doesBoundsChecking_test1),
+		cmocka_unit_test(doesBoundsChecking_test2),
+		cmocka_unit_test(canCopy_test1),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
