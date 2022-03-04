be90c3265e29f7b21dc0b69e5ae82ea279df68b2
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-04 04:18:57 +0100

    Reformatted file

--- tests/sw_strdup.c
+++ tests/sw_strdup.c
@@ -11,30 +11,30 @@
 static void
 canDuplicateString_test1(void **state)
 {
-    const char *string = "Swirc IRC client";
-    char *string_copy = sw_strdup(string);
+	const char	*string = "Swirc IRC client";
+	char		*string_copy = sw_strdup(string);
 
-    assert_string_equal(string_copy, string);
-    free(string_copy);
+	assert_string_equal(string_copy, string);
+	free(string_copy);
 }
 
 static void
 canDuplicateString_test2(void **state)
 {
-    const char *string = "\f\n\r\t\v0123456789";
-    char *string_copy = sw_strdup(string);
+	const char	*string = "\f\n\r\t\v0123456789";
+	char		*string_copy = sw_strdup(string);
 
-    assert_string_equal(string_copy, string);
-    free(string_copy);
+	assert_string_equal(string_copy, string);
+	free(string_copy);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canDuplicateString_test1),
-	cmocka_unit_test(canDuplicateString_test2),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canDuplicateString_test1),
+		cmocka_unit_test(canDuplicateString_test2),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
