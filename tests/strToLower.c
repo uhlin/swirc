36d1cf9a6135a8c4a22d11bb7daf9fd38f1c8292
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-04 05:25:37 +0100

    Reformatted files

--- tests/strToLower.c
+++ tests/strToLower.c
@@ -8,39 +8,41 @@
 static void
 canLowerString_test1(void **state)
 {
-    char str[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ";
-    const char expected[] = "abcdefghijklmnopqrstuvwxyz";
+	char		str[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ";
+	const char	expected[] = "abcdefghijklmnopqrstuvwxyz";
 
-    assert_string_not_equal(str, expected);
-    assert_string_equal(strToLower(str), expected);
+	assert_string_not_equal(str, expected);
+	assert_string_equal(strToLower(str), expected);
+	UNUSED_PARAM(state);
 }
 
 static void
 leavesStringUnchanged_test1(void **state)
 {
-    char str[] = "abcdefghijklmnopqrstuvwxyz";
-    const char expected[] = "abcdefghijklmnopqrstuvwxyz";
+	char		str[] = "abcdefghijklmnopqrstuvwxyz";
+	const char	expected[] = "abcdefghijklmnopqrstuvwxyz";
 
-    assert_string_equal(strToLower(str), expected);
+	assert_string_equal(strToLower(str), expected);
+	UNUSED_PARAM(state);
 }
 
 static void
 leavesStringUnchanged_test2(void **state)
 {
-    char str[] = "0123456789...";
-    const char expected[] = "0123456789...";
+	char		str[] = "0123456789...";
+	const char	expected[] = "0123456789...";
 
-    assert_string_equal(strToLower(str), expected);
+	assert_string_equal(strToLower(str), expected);
+	UNUSED_PARAM(state);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canLowerString_test1),
-	cmocka_unit_test(leavesStringUnchanged_test1),
-	cmocka_unit_test(leavesStringUnchanged_test2),
-    };
-
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canLowerString_test1),
+		cmocka_unit_test(leavesStringUnchanged_test1),
+		cmocka_unit_test(leavesStringUnchanged_test2),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
--- tests/strToUpper.c
+++ tests/strToUpper.c
@@ -8,39 +8,41 @@
 static void
 canUpperString_test1(void **state)
 {
-    char str[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ";
-    const char expected[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char		str[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ";
+	const char	expected[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    assert_string_not_equal(str, expected);
-    assert_string_equal(strToUpper(str), expected);
+	assert_string_not_equal(str, expected);
+	assert_string_equal(strToUpper(str), expected);
+	UNUSED_PARAM(state);
 }
 
 static void
 leavesStringUnchanged_test1(void **state)
 {
-    char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const char expected[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char		str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const char	expected[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    assert_string_equal(strToUpper(str), expected);
+	assert_string_equal(strToUpper(str), expected);
+	UNUSED_PARAM(state);
 }
 
 static void
 leavesStringUnchanged_test2(void **state)
 {
-    char str[] = "0123456789...";
-    const char expected[] = "0123456789...";
+	char		str[] = "0123456789...";
+	const char	expected[] = "0123456789...";
 
-    assert_string_equal(strToUpper(str), expected);
+	assert_string_equal(strToUpper(str), expected);
+	UNUSED_PARAM(state);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canUpperString_test1),
-	cmocka_unit_test(leavesStringUnchanged_test1),
-	cmocka_unit_test(leavesStringUnchanged_test2),
-    };
-
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canUpperString_test1),
+		cmocka_unit_test(leavesStringUnchanged_test1),
+		cmocka_unit_test(leavesStringUnchanged_test2),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
