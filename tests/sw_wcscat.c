5f67890c37d2a40c89ec469eb6e0ca00bd7cbe56
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-04 03:48:18 +0100

    Reformatted files

--- tests/sw_wcscat.c
+++ tests/sw_wcscat.c
@@ -1,52 +1,52 @@
 #include "common.h"
 
 #include <setjmp.h>
-#include <wchar.h> /* wcscmp() */
 #include <cmocka.h>
+#include <wchar.h> /* wcscmp() */
 
 #include "strHand.h"
 
 static void
 handlesBufferOverflow_test1(void **state)
 {
-    wchar_t dest[4] = L"abc";
+	wchar_t dest[4] = L"abc";
 
-    if (sw_wcscat(dest, L"d", ARRAY_SIZE(dest)) != ERANGE)
-	fail();
-    else if (wcscmp(dest, L"abc") != 0)
-	fail();
+	if (sw_wcscat(dest, L"d", ARRAY_SIZE(dest)) != ERANGE)
+		fail();
+	else if (wcscmp(dest, L"abc") != 0)
+		fail();
 }
 
 static void
 canConcatenate_test1(void **state)
 {
-    wchar_t dest[5] = L"abc";
+	wchar_t dest[5] = L"abc";
 
-    if (sw_wcscat(dest, L"d", ARRAY_SIZE(dest)) != 0)
-	fail();
-    else if (wcscmp(dest, L"abcd") != 0)
-	fail();
+	if (sw_wcscat(dest, L"d", ARRAY_SIZE(dest)) != 0)
+		fail();
+	else if (wcscmp(dest, L"abcd") != 0)
+		fail();
 }
 
 static void
 canConcatenate_test2(void **state)
 {
-    wchar_t dest[7] = L"FOO";
+	wchar_t dest[7] = L"FOO";
 
-    if (sw_wcscat(dest, L"BAR", ARRAY_SIZE(dest)) != 0)
-	fail();
-    else if (wcscmp(dest, L"FOOBAR") != 0)
-	fail();
+	if (sw_wcscat(dest, L"BAR", ARRAY_SIZE(dest)) != 0)
+		fail();
+	else if (wcscmp(dest, L"FOOBAR") != 0)
+		fail();
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(handlesBufferOverflow_test1),
-	cmocka_unit_test(canConcatenate_test1),
-	cmocka_unit_test(canConcatenate_test2),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(handlesBufferOverflow_test1),
+		cmocka_unit_test(canConcatenate_test1),
+		cmocka_unit_test(canConcatenate_test2),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
