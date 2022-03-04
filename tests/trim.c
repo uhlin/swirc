66e153ba35310802119c61c2dd26d73fb9d9e72c
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-04 03:34:39 +0100

    Reformatted file

--- tests/trim.c
+++ tests/trim.c
@@ -8,58 +8,56 @@
 static void
 canTrim_test1(void **state)
 {
-    char str[] = "          ";
-    const char *expected = "";
+	char str[] = "          ";
 
-    assert_string_equal(trim(str), expected);
+	assert_string_equal(trim(str), "");
 }
 
 static void
 canTrim_test2(void **state)
 {
-    char str[] = " \f\n\r\t\v";
-    const char *expected = "";
+	char str[] = " \f\n\r\t\v";
 
-    assert_string_equal(trim(str), expected);
+	assert_string_equal(trim(str), "");
 }
 
 static void
 canTrim_test3(void **state)
 {
-    char str[] = "Swirc IRC client          ";
-    const char *expected = "Swirc IRC client";
+	char		 str[] = "Swirc IRC client          ";
+	const char	*expected = "Swirc IRC client";
 
-    assert_string_equal(trim(str), expected);
+	assert_string_equal(trim(str), expected);
 }
 
 static void
 canTrim_test4(void **state)
 {
-    char str[] = "          Hello World!          ";
-    const char *expected = "          Hello World!";
+	char		 str[] = "          Hello World!          ";
+	const char	*expected = "          Hello World!";
 
-    assert_string_equal(trim(str), expected);
+	assert_string_equal(trim(str), expected);
 }
 
 static void
 leavesStringUnchanged_test1(void **state)
 {
-    char str[] = "          Diablo";
-    const char *expected = "          Diablo";
+	char		 str[] = "          Diablo";
+	const char	*expected = "          Diablo";
 
-    assert_string_equal(trim(str), expected);
+	assert_string_equal(trim(str), expected);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canTrim_test1),
-	cmocka_unit_test(canTrim_test2),
-	cmocka_unit_test(canTrim_test3),
-	cmocka_unit_test(canTrim_test4),
-	cmocka_unit_test(leavesStringUnchanged_test1),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canTrim_test1),
+		cmocka_unit_test(canTrim_test2),
+		cmocka_unit_test(canTrim_test3),
+		cmocka_unit_test(canTrim_test4),
+		cmocka_unit_test(leavesStringUnchanged_test1),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
