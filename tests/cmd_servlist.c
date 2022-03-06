8a19829f5fd6934611b6090dbd98eac5b2a9a25b
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-06 22:37:02 +0100

    Reformatted file

--- tests/cmd_servlist.c
+++ tests/cmd_servlist.c
@@ -13,40 +13,43 @@
 static int
 setup(void **state)
 {
-    (void) state;
-    net_send = net_send_fake;
-    return 0;
+	net_send = net_send_fake;
+	UNUSED_PARAM(state);
+	return 0;
 }
 
 static void
 sendsExpected_test1(void **state)
 {
-    cmd_servlist("");
-    assert_string_equal(g_sent, "SERVLIST");
+	cmd_servlist("");
+	assert_string_equal(g_sent, "SERVLIST");
+	UNUSED_PARAM(state);
 }
 
 static void
 sendsExpected_test2(void **state)
 {
-    cmd_servlist("*");
-    assert_string_equal(g_sent, "SERVLIST *");
+	cmd_servlist("*");
+	assert_string_equal(g_sent, "SERVLIST *");
+	UNUSED_PARAM(state);
 }
 
 static void
 sendsExpected_test3(void **state)
 {
-    cmd_servlist("* 0xD000");
-    assert_string_equal(g_sent, "SERVLIST * 0xD000");
+	cmd_servlist("* 0xD000");
+	assert_string_equal(g_sent, "SERVLIST * 0xD000");
+	UNUSED_PARAM(state);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(sendsExpected_test1),
-	cmocka_unit_test(sendsExpected_test2),
-	cmocka_unit_test(sendsExpected_test3),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(sendsExpected_test1),
+		cmocka_unit_test(sendsExpected_test2),
+		cmocka_unit_test(sendsExpected_test3),
+	};
 
-    return cmocka_run_group_tests(tests, setup, NULL);
+	return cmocka_run_group_tests(tests, setup, NULL);
 }
