ab78c8e7c7f64247aff2a3791b3568e97b2ed9ef
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-06 21:18:50 +0100

    Reformatted file

--- tests/cmd_znc.c
+++ tests/cmd_znc.c
@@ -9,40 +9,43 @@
 static int
 setup(void **state)
 {
-    (void) state;
-    net_send = net_send_fake;
-    return 0;
+	net_send = net_send_fake;
+	UNUSED_PARAM(state);
+	return 0;
 }
 
 static void
 sendsExpected_test1(void **state)
 {
-    cmd_znc("SetBuffer #ChatZone 300");
-    assert_string_equal(g_sent, "PRIVMSG *status :SetBuffer #ChatZone 300");
+	cmd_znc("SetBuffer #ChatZone 300");
+	assert_string_equal(g_sent, "PRIVMSG *status :SetBuffer #ChatZone 300");
+	UNUSED_PARAM(state);
 }
 
 static void
 sendsExpected_test2(void **state)
 {
-    cmd_znc("*status SeTbUfFeR #ChatZone 300");
-    assert_string_equal(g_sent, "PRIVMSG *status :SeTbUfFeR #ChatZone 300");
+	cmd_znc("*status SeTbUfFeR #ChatZone 300");
+	assert_string_equal(g_sent, "PRIVMSG *status :SeTbUfFeR #ChatZone 300");
+	UNUSED_PARAM(state);
 }
 
 static void
 sendsExpected_test3(void **state)
 {
-    cmd_znc("*ControlPanel help");
-    assert_string_equal(g_sent, "PRIVMSG *controlpanel :help");
+	cmd_znc("*ControlPanel help");
+	assert_string_equal(g_sent, "PRIVMSG *controlpanel :help");
+	UNUSED_PARAM(state);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(sendsExpected_test1),
-	cmocka_unit_test(sendsExpected_test2),
-	cmocka_unit_test(sendsExpected_test3),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(sendsExpected_test1),
+		cmocka_unit_test(sendsExpected_test2),
+		cmocka_unit_test(sendsExpected_test3),
+	};
 
-    return cmocka_run_group_tests(tests, setup, NULL);
+	return cmocka_run_group_tests(tests, setup, NULL);
 }
