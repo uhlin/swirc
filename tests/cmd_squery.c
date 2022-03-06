a26a415a1ae1c5f69869f35de27a69892c0a917b
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-06 22:31:17 +0100

    Reformatted files

--- tests/cmd_squery.c
+++ tests/cmd_squery.c
@@ -9,32 +9,34 @@
 static int
 setup(void **state)
 {
-    (void) state;
-    net_send = net_send_fake;
-    return 0;
+	net_send = net_send_fake;
+	UNUSED_PARAM(state);
+	return 0;
 }
 
 static void
 sendsExpected_test1(void **state)
 {
-    cmd_squery("Alis HELP");
-    assert_string_equal(g_sent, "SQUERY Alis :HELP");
+	cmd_squery("Alis HELP");
+	assert_string_equal(g_sent, "SQUERY Alis :HELP");
+	UNUSED_PARAM(state);
 }
 
 static void
 sendsExpected_test2(void **state)
 {
-    cmd_squery("alis help list");
-    assert_string_equal(g_sent, "SQUERY alis :help list");
+	cmd_squery("alis help list");
+	assert_string_equal(g_sent, "SQUERY alis :help list");
+	UNUSED_PARAM(state);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(sendsExpected_test1),
-	cmocka_unit_test(sendsExpected_test2),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(sendsExpected_test1),
+		cmocka_unit_test(sendsExpected_test2),
+	};
 
-    return cmocka_run_group_tests(tests, setup, NULL);
+	return cmocka_run_group_tests(tests, setup, NULL);
 }
