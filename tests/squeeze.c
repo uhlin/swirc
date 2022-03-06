46e8099f21a110feba9806d4ee2cc235f65a4e70
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-06 19:30:59 +0100

    Reformatted file

--- tests/squeeze.c
+++ tests/squeeze.c
@@ -8,56 +8,56 @@
 static void
 canSqueezeCharacters_test1(void **state)
 {
-    char buf[] = "ABCABCABCABCABCABCABCABCABCABC";
-    const char *expected = "ABABABABABABABABABAB";
+	char		 buf[] = "ABCABCABCABCABCABCABCABCABCABC";
+	const char	*expected = "ABABABABABABABABABAB";
 
-    assert_string_not_equal(buf, expected);
-    squeeze(buf, "C");
-    assert_string_equal(buf, expected);
+	assert_string_not_equal(buf, expected);
+	squeeze(buf, "C");
+	assert_string_equal(buf, expected);
 }
 
 static void
 canSqueezeCharacters_test2(void **state)
 {
-    char buf[] = "ABCABCABCABCABCABCABCABCABCABC";
-    const char *expected = "AAAAAAAAAA";
+	char		 buf[] = "ABCABCABCABCABCABCABCABCABCABC";
+	const char	*expected = "AAAAAAAAAA";
 
-    assert_string_not_equal(buf, expected);
-    squeeze(buf, "BC");
-    assert_string_equal(buf, expected);
+	assert_string_not_equal(buf, expected);
+	squeeze(buf, "BC");
+	assert_string_equal(buf, expected);
 }
 
 static void
 canSqueezeCharacters_test3(void **state)
 {
-    char buf[] = "ABCABCABCABCABCABCABCABCABCABC";
-    const char *expected = "";
+	char		 buf[] = "ABCABCABCABCABCABCABCABCABCABC";
+	const char	*expected = "";
 
-    assert_string_not_equal(buf, expected);
-    squeeze(buf, "ABC");
-    assert_string_equal(buf, expected);
+	assert_string_not_equal(buf, expected);
+	squeeze(buf, "ABC");
+	assert_string_equal(buf, expected);
 }
 
 static void
 canSqueezeCharacters_test4(void **state)
 {
-    char buf[] = "ZzZzZzZzZzZzZzZzZzZz";
-    const char *expected = "zzzzzzzzzz";
+	char		 buf[] = "ZzZzZzZzZzZzZzZzZzZz";
+	const char	*expected = "zzzzzzzzzz";
 
-    assert_string_not_equal(buf, expected);
-    squeeze(buf, "Z");
-    assert_string_equal(buf, expected);
+	assert_string_not_equal(buf, expected);
+	squeeze(buf, "Z");
+	assert_string_equal(buf, expected);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canSqueezeCharacters_test1),
-	cmocka_unit_test(canSqueezeCharacters_test2),
-	cmocka_unit_test(canSqueezeCharacters_test3),
-	cmocka_unit_test(canSqueezeCharacters_test4),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canSqueezeCharacters_test1),
+		cmocka_unit_test(canSqueezeCharacters_test2),
+		cmocka_unit_test(canSqueezeCharacters_test3),
+		cmocka_unit_test(canSqueezeCharacters_test4),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
