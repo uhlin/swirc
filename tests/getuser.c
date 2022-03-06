e6875c369d264de078e43f8e625851c93898743c
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-06 21:00:23 +0100

    Reformatted file

--- tests/getuser.c
+++ tests/getuser.c
@@ -8,26 +8,28 @@
 static void
 returnsExpected_test1(void **state)
 {
-    if (setenv("USER", "maxxe", 1) != 0)
-	fail();
-    assert_string_equal(getuser(), "maxxe");
+	if (setenv("USER", "maxxe", 1) != 0)
+		fail();
+	assert_string_equal(getuser(), "maxxe");
+	UNUSED_PARAM(state);
 }
 
 static void
 returnsExpected_test2(void **state)
 {
-    if (setenv("USER", "Markus Uhlin", 1) != 0)
-	fail();
-    assert_string_equal(getuser(), "Markus");
+	if (setenv("USER", "Markus Uhlin", 1) != 0)
+		fail();
+	assert_string_equal(getuser(), "Markus");
+	UNUSED_PARAM(state);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(returnsExpected_test1),
-	cmocka_unit_test(returnsExpected_test2),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(returnsExpected_test1),
+		cmocka_unit_test(returnsExpected_test2),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
