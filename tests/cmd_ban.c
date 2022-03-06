c0ff529896efd224f39c39675a2a9d70670cba32
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-06 22:59:53 +0100

    Reformatted files

--- tests/cmd_ban.c
+++ tests/cmd_ban.c
@@ -6,31 +6,37 @@
 #include "network.h"
 #include "commands/ban.h"
 
-static int setup(void **state)
+static int
+setup(void **state)
 {
-    (void) state;
-    net_send = net_send_fake;
-    return 0;
+	net_send = net_send_fake;
+	UNUSED_PARAM(state);
+	return 0;
 }
 
-static void canBan_test1(void **state)
+static void
+canBan_test1(void **state)
 {
-    cmd_ban("*hitler*!*@*");
-    assert_string_equal(g_sent, "MODE #chatzone +b *hitler*!*@*");
+	cmd_ban("*hitler*!*@*");
+	assert_string_equal(g_sent, "MODE #chatzone +b *hitler*!*@*");
+	UNUSED_PARAM(state);
 }
 
-static void canBan_test2(void **state)
+static void
+canBan_test2(void **state)
 {
-    cmd_ban("*!*@spammers.com");
-    assert_string_equal(g_sent, "MODE #chatzone +b *!*@spammers.com");
+	cmd_ban("*!*@spammers.com");
+	assert_string_equal(g_sent, "MODE #chatzone +b *!*@spammers.com");
+	UNUSED_PARAM(state);
 }
 
-int main(void)
+int
+main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canBan_test1),
-	cmocka_unit_test(canBan_test2),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canBan_test1),
+		cmocka_unit_test(canBan_test2),
+	};
 
-    return cmocka_run_group_tests(tests, setup, NULL);
+	return cmocka_run_group_tests(tests, setup, NULL);
 }
--- tests/cmd_deop.c
+++ tests/cmd_deop.c
@@ -6,24 +6,28 @@
 #include "network.h"
 #include "commands/op.h"
 
-static int setup(void **state)
+static int
+setup(void **state)
 {
-    (void) state;
-    net_send = net_send_fake;
-    return 0;
+	net_send = net_send_fake;
+	UNUSED_PARAM(state);
+	return 0;
 }
 
-static void canTakeOp_test1(void **state)
+static void
+canTakeOp_test1(void **state)
 {
-    cmd_deop("victim");
-    assert_string_equal(g_sent, "MODE #chatzone -o victim");
+	cmd_deop("victim");
+	assert_string_equal(g_sent, "MODE #chatzone -o victim");
+	UNUSED_PARAM(state);
 }
 
-int main(void)
+int
+main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canTakeOp_test1),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canTakeOp_test1),
+	};
 
-    return cmocka_run_group_tests(tests, setup, NULL);
+	return cmocka_run_group_tests(tests, setup, NULL);
 }
