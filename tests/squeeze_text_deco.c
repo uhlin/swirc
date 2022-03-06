6aa32a43e2c2bdad989c59c654eb0a40225da5e3
Markus Uhlin <markus.uhlin@bredband.net>
2022-03-06 18:03:05 +0100

    Switched to usage of sw_snprintf()

--- tests/squeeze_text_deco.c
+++ tests/squeeze_text_deco.c
@@ -4,13 +4,15 @@
 #include <cmocka.h>
 
 #include "printtext.h"
+#include "strHand.h"
 
 static char	buf[300] = { '\0' };
 
 static void
 canSqueezeTextDecoration_test1(void **state)
 {
-	snprintf(buf, sizeof buf, "foo%c%c%cbar%c%c%cbaz", BLINK, BOLD, COLOR, NORMAL, REVERSE, UNDERLINE);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "foo%c%c%cbar%c%c%cbaz",
+	    BLINK, BOLD, COLOR, NORMAL, REVERSE, UNDERLINE);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, "foobarbaz");
 	UNUSED_PARAM(state);
@@ -19,7 +21,8 @@ canSqueezeTextDecoration_test1(void **state)
 static void
 canSqueezeTextDecoration_test2(void **state)
 {
-	snprintf(buf, sizeof buf, "%c%c%c%c%c%c", BLINK, BOLD, COLOR, NORMAL, REVERSE, UNDERLINE);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "%c%c%c%c%c%c",
+	    BLINK, BOLD, COLOR, NORMAL, REVERSE, UNDERLINE);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, "");
 	UNUSED_PARAM(state);
@@ -28,7 +31,7 @@ canSqueezeTextDecoration_test2(void **state)
 static void
 canSqueezeTextDecoration_test3(void **state)
 {
-	snprintf(buf, sizeof buf, "%c123", COLOR);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "%c123", COLOR);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, "3");
 	UNUSED_PARAM(state);
@@ -37,7 +40,7 @@ canSqueezeTextDecoration_test3(void **state)
 static void
 canSqueezeTextDecoration_test4(void **state)
 {
-	snprintf(buf, sizeof buf, "%c1,test", COLOR);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "%c1,test", COLOR);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, ",test");
 	UNUSED_PARAM(state);
@@ -46,7 +49,7 @@ canSqueezeTextDecoration_test4(void **state)
 static void
 canSqueezeTextDecoration_test5(void **state)
 {
-	snprintf(buf, sizeof buf, "%c12,test", COLOR);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "%c12,test", COLOR);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, ",test");
 	UNUSED_PARAM(state);
@@ -55,7 +58,7 @@ canSqueezeTextDecoration_test5(void **state)
 static void
 canSqueezeTextDecoration_test6(void **state)
 {
-	snprintf(buf, sizeof buf, "%c1,2,3,test", COLOR);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "%c1,2,3,test", COLOR);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, ",3,test");
 	UNUSED_PARAM(state);
@@ -64,7 +67,7 @@ canSqueezeTextDecoration_test6(void **state)
 static void
 canSqueezeTextDecoration_test7(void **state)
 {
-	snprintf(buf, sizeof buf, "%c1,234", COLOR);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "%c1,234", COLOR);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, "4");
 	UNUSED_PARAM(state);
@@ -73,7 +76,8 @@ canSqueezeTextDecoration_test7(void **state)
 static void
 canSqueezeTextDecoration_test8(void **state)
 {
-	snprintf(buf, sizeof buf, "%c1,1%c2,22foo%c33,3bar", COLOR, COLOR, COLOR);
+	sw_snprintf(buf, ARRAY_SIZE(buf), "%c1,1%c2,22foo%c33,3bar",
+	    COLOR, COLOR, COLOR);
 	squeeze_text_deco(buf);
 	assert_string_equal(buf, "foobar");
 	UNUSED_PARAM(state);
