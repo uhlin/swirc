6619c2b5bd25dd9f1ef5e75ae9fc9bd4620e98bd
Markus Uhlin <markus.uhlin@bredband.net>
2022-05-01 18:44:31 +0200

    Added GNU libidn

--- options.w32.mk
+++ options.w32.mk
@@ -1,5 +1,6 @@
 CC = cl
 CFLAGS = -DHAVE_ATLSTR_H=1\
+	-DHAVE_LIBIDN=1\
 	-DHAVE_LIBINTL_H=1\
 	-DNDEBUG=1\
 	-DPDC_EXP_EXTRAS=1\
@@ -20,6 +21,7 @@ CXXFLAGS = $(CFLAGS)\
 
 # Versions
 CURL_VERSION = 7.83.0
+IDN_VERSION = 1.36
 INTL_VERSION = 0.19.8.1
 LIBRESSL_VERSION = 3.5.2
 PDCURSES_VERSION = 3.9
@@ -32,16 +34,19 @@ Q = @
 
 MACHINE = x64
 NAME_libcrypto = crypto-49
+NAME_libidn = libidn-12
 NAME_libintl = libintl-8
 NAME_libssl = ssl-52
 
 LDFLAGS = -LIBPATH:curl-$(CURL_VERSION)/$(MACHINE)\
+	-LIBPATH:idn-$(IDN_VERSION)/$(MACHINE)\
 	-LIBPATH:intl-$(INTL_VERSION)/$(MACHINE)\
 	-LIBPATH:libressl-$(LIBRESSL_VERSION)-windows/$(MACHINE)\
 	-LIBPATH:pdcurses-$(PDCURSES_VERSION)/$(MACHINE)\
 	-NODEFAULTLIB:MSVCRTD
 
 LDLIBS = $(NAME_libcrypto).lib\
+	$(NAME_libidn).lib\
 	$(NAME_libintl).lib\
 	$(NAME_libssl).lib\
 	advapi32.lib\
