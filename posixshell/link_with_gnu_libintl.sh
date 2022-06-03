91cd60f2e64891bcc43711335264070f0ebba07d
Markus Uhlin <markus.uhlin@bredband.net>
2022-06-03 17:54:50 +0200

    check_intl_setlocale: added code

--- posixshell/link_with_gnu_libintl.sh
+++ posixshell/link_with_gnu_libintl.sh
@@ -48,7 +48,50 @@ EOF
 }
 
 check_intl_setlocale () {
-	true
+	local _tmpfile _srcfile _out _libs
+
+	echo -n "creating temp file..."
+	_tmpfile=$(mktemp) || { echo "error"; exit 1; }
+	echo "ok"
+
+	_srcfile="${_tmpfile}.c"
+	_out="${_tmpfile}.out"
+	cat <<EOF >"$_srcfile"
+#include <libintl.h>
+#include <locale.h>
+
+int
+main(void)
+{
+	libintl_setlocale(LC_ALL, "");
+	return 0;
+}
+EOF
+	if [ ! -f "$_srcfile" ]; then
+		echo "failed to create $_srcfile"
+		exit 1
+	fi
+	echo -n "checking for libintl_setlocale()..."
+	if [ "$(uname -s)" = "Linux" ]; then
+		_libs=""
+	else
+		_libs="-lintl"
+	fi
+	$CC $CFLAGS "$_srcfile" -o "$_out" $LDFLAGS $_libs >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "yes"
+		cat <<EOF >>$MAKE_DEF_FILE
+CFLAGS += -DHAVE_LIBINTL_SETLOCALE=1
+CXXFLAGS += -DHAVE_LIBINTL_SETLOCALE=1
+EOF
+	else
+		echo "no"
+	fi
+
+	echo "cleaning..."
+	test -f "$_tmpfile" && rm -f "$_tmpfile"
+	test -f "$_srcfile" && rm -f "$_srcfile"
+	test -f "$_out" && rm -f "$_out"
 }
 
 link_with_gnu_libintl () {
