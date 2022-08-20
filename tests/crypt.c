6a6bb422c1a4d25baef7649a4be7cb361c207c1e
Markus Uhlin <markus.uhlin@bredband.net>
2022-08-20 14:23:47 +0200

    Added a test

--- tests/crypt.c
+++ tests/crypt.c
@@ -23,11 +23,37 @@ canEncrypt_test1(void **state)
 	UNUSED_PARAM(state);
 }
 
+static void
+canEncryptAndDecrypt_test1(void **state)
+{
+#define TEXT "Hello World!"
+	const cryptarray_t str = TEXT;
+	const cryptarray_t password = "Encryption pass 321";
+	char *ret1, *ret2;
+
+	ret1 = ret2 = NULL;
+
+	if ((ret1 = crypt_encrypt_str(addrof(str[0]), addrof(password[0]),
+	    false)) == NULL)
+		fail();
+	else if ((ret2 = crypt_decrypt_str(ret1, addrof(password[0]), false)) ==
+	    NULL)
+		fail();
+
+	assert_string_equal(ret2, TEXT);
+
+	free(ret1);
+	free(ret2);
+
+	UNUSED_PARAM(state);
+}
+
 int
 main(void)
 {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(canEncrypt_test1),
+		cmocka_unit_test(canEncryptAndDecrypt_test1),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
