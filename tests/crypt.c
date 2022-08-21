7767520ae89d5af0798e8f8d77ee0ed8572b4324
Markus Uhlin <markus.uhlin@bredband.net>
2022-08-21 21:57:34 +0200

    Added test

--- tests/crypt.c
+++ tests/crypt.c
@@ -46,6 +46,33 @@ canEncryptAndDecrypt_test1(void **state)
 	free(ret2);
 
 	UNUSED_PARAM(state);
+#undef TEXT
+}
+
+static void
+canEncryptAndDecrypt_test2(void **state)
+{
+#define TEXT "Swirc IRC client"
+	const cryptarray_t str = TEXT;
+	const cryptarray_t password = "><><987 ENC PASS.!.";
+	char *ret1, *ret2;
+
+	ret1 = ret2 = NULL;
+
+	if ((ret1 = crypt_encrypt_str(addrof(str[0]), addrof(password[0]),
+	    true)) == NULL)
+		fail();
+	else if ((ret2 = crypt_decrypt_str(ret1, addrof(password[0]), true)) ==
+	    NULL)
+		fail();
+
+	assert_string_equal(ret2, TEXT);
+
+	free(ret1);
+	free(ret2);
+
+	UNUSED_PARAM(state);
+#undef TEXT
 }
 
 int
@@ -54,6 +81,7 @@ main(void)
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(canEncrypt_test1),
 		cmocka_unit_test(canEncryptAndDecrypt_test1),
+		cmocka_unit_test(canEncryptAndDecrypt_test2),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
