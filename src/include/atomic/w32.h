a157b01c555ca5d6b2a66c3fec08e680809e7f01
Markus Uhlin <markus.uhlin@bredband.net>
2022-08-26 13:04:41 +0200

    Changed to tab indentation

--- src/include/atomic/w32.h
+++ src/include/atomic/w32.h
@@ -6,15 +6,15 @@
 static inline bool
 atomic_load_bool(volatile bool *obj)
 {
-    static_assert(sizeof(bool) == 1, "bool not 8 bits");
-    return (_InterlockedCompareExchange8((volatile char *) obj, false, false));
+	static_assert(sizeof(bool) == 1, "bool not 8 bits");
+	return _InterlockedCompareExchange8((volatile char *) obj, false, false);
 }
 
 static inline bool
 atomic_swap_bool(volatile bool *obj, bool desired)
 {
-    static_assert(sizeof(bool) == 1, "bool not 8 bits");
-    return (_InterlockedExchange8((volatile char *) obj, desired));
+	static_assert(sizeof(bool) == 1, "bool not 8 bits");
+	return _InterlockedExchange8((volatile char *) obj, desired);
 }
 
 #endif
