fbefbb2f7299a89eb7d8ae5d685923956a097f1d
Markus Uhlin <markus.uhlin@bredband.net>
2022-08-26 13:13:18 +0200

    Defined and made use of 'VCP_OBJ()'

--- src/include/atomic/w32.h
+++ src/include/atomic/w32.h
@@ -3,18 +3,24 @@
 
 #include <intrin.h>
 
+#ifdef __cplusplus
+#define VCP_OBJ(_o)	(reinterpret_cast<volatile char *>(_o))
+#else
+#define VCP_OBJ(_o)	((volatile char *) (_o))
+#endif
+
 static inline bool
 atomic_load_bool(volatile bool *obj)
 {
 	static_assert(sizeof(bool) == 1, "bool not 8 bits");
-	return _InterlockedCompareExchange8((volatile char *) obj, false, false);
+	return (_InterlockedCompareExchange8(VCP_OBJ(obj), false, false));
 }
 
 static inline bool
 atomic_swap_bool(volatile bool *obj, bool desired)
 {
 	static_assert(sizeof(bool) == 1, "bool not 8 bits");
-	return _InterlockedExchange8((volatile char *) obj, desired);
+	return (_InterlockedExchange8(VCP_OBJ(obj), desired));
 }
 
 #endif
