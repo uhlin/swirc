b92eefc5024d0d9bcdfcc964280b169a0d2d2106
Markus Uhlin <markus.uhlin@bredband.net>
2022-08-26 12:52:26 +0200

    'SW_INLINE' -> 'inline'

--- src/include/atomic/w32.h
+++ src/include/atomic/w32.h
@@ -3,14 +3,14 @@
 
 #include <intrin.h>
 
-static SW_INLINE bool
+static inline bool
 atomic_load_bool(volatile bool *obj)
 {
     static_assert(sizeof(bool) == 1, "bool not 8 bits");
     return (_InterlockedCompareExchange8((volatile char *) obj, false, false));
 }
 
-static SW_INLINE bool
+static inline bool
 atomic_swap_bool(volatile bool *obj, bool desired)
 {
     static_assert(sizeof(bool) == 1, "bool not 8 bits");
