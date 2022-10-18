530ba4f7ec9425644774f3a8fbb18ee851c7abf6
Markus Uhlin <markus.uhlin@bredband.net>
2022-10-19 01:05:46 +0200

    edit_vc_makefile: set REVISION to nothing

--- bash/edit_vc_makefile.sh
+++ bash/edit_vc_makefile.sh
@@ -1,10 +1,12 @@
 edit_vc_makefile () {
-	local _expr="s/PRODUCT_VERSION = .*/PRODUCT_VERSION = $VERSION/"
+	local _expr1="s/PRODUCT_VERSION = .*/PRODUCT_VERSION = $VERSION/"
+	local _expr2="s/REVISION =.*/REVISION =/"
 
 	printf "  - Editing %s..." "$1"
 
 	if [ -f "$1" ]; then
-		sed -i "$_expr" "$1" || { echo "error"; exit 1; }
+		sed -i "$_expr1" "$1" || { echo "error"; exit 1; }
+		sed -i "$_expr2" "$1" || { echo "error"; exit 1; }
 		echo "ok"
 	else
 		echo "not found"
