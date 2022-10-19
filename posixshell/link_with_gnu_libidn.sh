6835fbc7130513bbee2bfa76955769717e52a78b
Markus Uhlin <markus.uhlin@bredband.net>
2022-10-19 02:23:09 +0200

    link_with_gnu_libidn: added code

--- posixshell/link_with_gnu_libidn.sh
+++ posixshell/link_with_gnu_libidn.sh
@@ -1,8 +1,49 @@
 link_with_gnu_libidn () {
-	cat <<EOF >>$MAKE_DEF_FILE
+	local _tmpfile _srcfile _out _libs
+
+	echo -n "creating temp file..."
+	_tmpfile=$(mktemp) || { echo "error"; exit 1; }
+	echo "ok"
+
+	_srcfile="${_tmpfile}.c"
+	_out="${_tmpfile}.out"
+	_libs="-lidn"
+	cat <<EOF >"$_srcfile"
+#include <idn-free.h>
+#include <stdio.h>
+#include <stringprep.h>
+
+int
+main(void)
+{
+	const char *cp;
+
+	idn_free(NULL);
+	if ((cp = stringprep_check_version(NULL)) != NULL)
+		puts(cp);
+	return 0;
+}
+EOF
+	if [ ! -f "$_srcfile" ]; then
+		echo "failed to create $_srcfile"
+		exit 1
+	fi
+	echo -n "checking for gnu libidn..."
+	$CC $CFLAGS -Werror "$_srcfile" -o "$_out" $LDFLAGS $_libs >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "yes"
+		cat <<EOF >>$MAKE_DEF_FILE
 CFLAGS += -DHAVE_LIBIDN=1
 CXXFLAGS += -DHAVE_LIBIDN=1
 
-LDLIBS += -lidn
+LDLIBS += $_libs
 EOF
+	else
+		echo "no"
+	fi
+
+	echo "cleaning..."
+	test -f "$_tmpfile" && rm -f "$_tmpfile"
+	test -f "$_srcfile" && rm -f "$_srcfile"
+	test -f "$_out" && rm -f "$_out"
 }
