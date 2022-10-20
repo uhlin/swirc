439fff1f6c9d0bc4070fa2482f70f485430adf0d
Markus Uhlin <markus.uhlin@bredband.net>
2022-10-21 00:26:03 +0200

    Build package script

---
+++ build-package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]; then
+	echo "./build-package.sh source|any|all|binary|full [args]"
+	exit 1
+fi
+
+BUILD=$1
+shift
+
+source "vars.sh"
+
+source "include/clean-up.sh"
+source "include/prep-build-dir.sh"
+
+clean_up
+prep_build_dir
+
+cd ${BUILD_DIR}
+fakeroot -- dpkg-buildpackage --build=$BUILD --no-sign "$@"
+cd ..
