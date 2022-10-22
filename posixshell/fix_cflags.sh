bebdf14c2b3504f05fbd170b00eda9e98894a615
Markus Uhlin <markus.uhlin@bredband.net>
2022-10-22 13:26:46 +0200

    Declare and assign separately

--- posixshell/fix_cflags.sh
+++ posixshell/fix_cflags.sh
@@ -1,12 +1,14 @@
 fix_cflags () {
-	local _helper_scripts="
+	local _cf _helper_scripts
+
+	_helper_scripts="
 ncursesw6-config
 ncursesw5-config
 "
 
 	for s in $_helper_scripts; do
 		if [ -x "$(which "$s")" ]; then
-			local _cf="$($s --cflags)"
+			_cf="$($s --cflags)"
 
 			if [ -n "$_cf" ]; then
 				echo "CFLAGS += ${_cf}" >>$MAKE_DEF_FILE
@@ -19,8 +21,10 @@ ncursesw5-config
 }
 
 fix_cflags_with_pkg_config () {
+	local _hdr_search_path
+
 	if [ -x "$(which pkg-config)" ]; then
-		local _hdr_search_path="$(pkg-config --cflags-only-I ncursesw)"
+		_hdr_search_path="$(pkg-config --cflags-only-I ncursesw)"
 
 		if [ -n "$_hdr_search_path" ]; then
 			echo "CFLAGS += ${_hdr_search_path}" >>$MAKE_DEF_FILE
