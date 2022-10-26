507020e490d806422824038caffaf178efb700b0
Markus Uhlin <markus.uhlin@bredband.net>
2022-10-26 07:24:01 +0200

    Added event 413 and 414

--- src/irc.c
+++ src/irc.c
@@ -196,6 +196,8 @@ static struct numeric_events_tag {
 	{ "407", "ERR_TOOMANYTARGETS",      ACTIVE_WINDOW,  2, NULL },
 	{ "408", "ERR_NOSUCHSERVICE",       ACTIVE_WINDOW,  2, NULL },
 	{ "412", "ERR_NOTEXTTOSEND",        ACTIVE_WINDOW,  1, NULL },
+	{ "413", "ERR_NOTOPLEVEL",          ACTIVE_WINDOW,  2, NULL },
+	{ "414", "ERR_WILDTOPLEVEL",        ACTIVE_WINDOW,  2, NULL },
 	{ "415", "ERR_BADMASK",             ACTIVE_WINDOW,  2, NULL },
 	{ "416", "ERR_",                    ACTIVE_WINDOW,  2, NULL },
 	{ "421", "ERR_UNKNOWNCOMMAND",      ACTIVE_WINDOW,  2, NULL },
