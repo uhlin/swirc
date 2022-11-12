406ab644aaeda6c98a1c4e0da859824dce90eaa0
Markus Uhlin <markus.uhlin@bredband.net>
2022-11-12 08:17:43 +0100

    Added blank lines

--- posixshell/link_with_gnu_libidn.sh
+++ posixshell/link_with_gnu_libidn.sh
@@ -31,13 +31,17 @@ main(void)
 	return 0;
 }
 EOF
+
 	if [ ! -f "$_srcfile" ]; then
 		echo "failed to create $_srcfile"
 		exit 1
 	fi
+
 	printf "checking for gnu libidn..."
+
 	${CC} ${CFLAGS} -Werror "$_srcfile" -o "$_out" ${LDFLAGS} ${_libs} \
 	    >/dev/null 2>&1
+
 	if [ $? -eq 0 ]; then
 		echo "yes"
 		cat <<EOF >>$MAKE_DEF_FILE
