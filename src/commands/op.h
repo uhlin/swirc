a6277023eadb05ea5cb7dafd2c3c21e4341caf1b
Markus Uhlin <markus.uhlin@bredband.net>
2022-11-26 21:37:41 +0100

    Fixed header

--- src/commands/op.h
+++ src/commands/op.h
@@ -1,7 +1,9 @@
 #ifndef CMD_OP_H
 #define CMD_OP_H
 
-void cmd_op(const char *);
-void cmd_deop(const char *);
+__SWIRC_BEGIN_DECLS
+void	cmd_op(const char *);
+void	cmd_deop(const char *);
+__SWIRC_END_DECLS
 
 #endif
