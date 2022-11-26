1bd4f205a7f8f31e2f6185b19943e82a25f84bc8
Markus Uhlin <markus.uhlin@bredband.net>
2022-11-26 19:41:30 +0100

    Did reformatting

--- src/commands/op.c
+++ src/commands/op.c
@@ -16,38 +16,42 @@
 #define INVALID_NICK		"invalid nickname"
 #define NOT_AN_IRC_CHANNEL	"active window isn't an irc channel"
 
-/* usage: /op <nick> */
+/*
+ * usage: /op <nick>
+ */
 void
 cmd_op(const char *data)
 {
-    if (strings_match(data, "")) {
-	print_and_free("/op: " MISSING_ARGS, NULL);
-	return;
-    } else if (!is_valid_nickname(data)) {
-	print_and_free("/op: " INVALID_NICK, NULL);
-	return;
-    } else if (!is_irc_channel(ACTWINLABEL)) {
-	print_and_free("/op: " NOT_AN_IRC_CHANNEL, NULL);
-	return;
-    }
+	if (strings_match(data, "")) {
+		print_and_free("/op: " MISSING_ARGS, NULL);
+		return;
+	} else if (!is_valid_nickname(data)) {
+		print_and_free("/op: " INVALID_NICK, NULL);
+		return;
+	} else if (!is_irc_channel(ACTWINLABEL)) {
+		print_and_free("/op: " NOT_AN_IRC_CHANNEL, NULL);
+		return;
+	}
 
-    (void) net_send("MODE %s +o %s", ACTWINLABEL, data);
+	(void) net_send("MODE %s +o %s", ACTWINLABEL, data);
 }
 
-/* usage: /deop <nick> */
+/*
+ * usage: /deop <nick>
+ */
 void
 cmd_deop(const char *data)
 {
-    if (strings_match(data, "")) {
-	print_and_free("/deop: " MISSING_ARGS, NULL);
-	return;
-    } else if (!is_valid_nickname(data)) {
-	print_and_free("/deop: " INVALID_NICK, NULL);
-	return;
-    } else if (!is_irc_channel(ACTWINLABEL)) {
-	print_and_free("/deop: " NOT_AN_IRC_CHANNEL, NULL);
-	return;
-    }
+	if (strings_match(data, "")) {
+		print_and_free("/deop: " MISSING_ARGS, NULL);
+		return;
+	} else if (!is_valid_nickname(data)) {
+		print_and_free("/deop: " INVALID_NICK, NULL);
+		return;
+	} else if (!is_irc_channel(ACTWINLABEL)) {
+		print_and_free("/deop: " NOT_AN_IRC_CHANNEL, NULL);
+		return;
+	}
 
-    (void) net_send("MODE %s -o %s", ACTWINLABEL, data);
+	(void) net_send("MODE %s -o %s", ACTWINLABEL, data);
 }
