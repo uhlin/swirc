f1b19f7d62ce9fe9cc52e5d9d515e5e5af2b38bd
Markus Uhlin <markus.uhlin@bredband.net>
2022-11-26 19:21:21 +0100

    Did reformatting

--- src/commands/ban.c
+++ src/commands/ban.c
@@ -15,32 +15,36 @@
 #define NO_MASK "no mask"
 #define NOT_AN_IRC_CHANNEL "active window isn't an irc channel"
 
-/* usage: /ban <mask> */
+/*
+ * usage: /ban <mask>
+ */
 void
 cmd_ban(const char *data)
 {
-    if (strings_match(data, "")) {
-	print_and_free("/ban: " NO_MASK, NULL);
-	return;
-    } else if (!is_irc_channel(ACTWINLABEL)) {
-	print_and_free("/ban: " NOT_AN_IRC_CHANNEL, NULL);
-	return;
-    }
+	if (strings_match(data, "")) {
+		print_and_free("/ban: " NO_MASK, NULL);
+		return;
+	} else if (!is_irc_channel(ACTWINLABEL)) {
+		print_and_free("/ban: " NOT_AN_IRC_CHANNEL, NULL);
+		return;
+	}
 
-    (void) net_send("MODE %s +b %s", ACTWINLABEL, data);
+	(void) net_send("MODE %s +b %s", ACTWINLABEL, data);
 }
 
-/* usage: /unban <mask> */
+/*
+ * usage: /unban <mask>
+ */
 void
 cmd_unban(const char *data)
 {
-    if (strings_match(data, "")) {
-	print_and_free("/unban: " NO_MASK, NULL);
-	return;
-    } else if (!is_irc_channel(ACTWINLABEL)) {
-	print_and_free("/unban: " NOT_AN_IRC_CHANNEL, NULL);
-	return;
-    }
+	if (strings_match(data, "")) {
+		print_and_free("/unban: " NO_MASK, NULL);
+		return;
+	} else if (!is_irc_channel(ACTWINLABEL)) {
+		print_and_free("/unban: " NOT_AN_IRC_CHANNEL, NULL);
+		return;
+	}
 
-    (void) net_send("MODE %s -b %s", ACTWINLABEL, data);
+	(void) net_send("MODE %s -b %s", ACTWINLABEL, data);
 }
