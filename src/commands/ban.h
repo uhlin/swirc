9f3c2ddb133537fbf9d424760a624a275334bdbb
Markus Uhlin <markus.uhlin@bredband.net>
2022-11-26 21:33:52 +0100

    Fixed header

--- src/commands/ban.h
+++ src/commands/ban.h
@@ -1,7 +1,9 @@
 #ifndef CMD_BAN_H
 #define CMD_BAN_H
 
-void cmd_ban(const char *);
-void cmd_unban(const char *);
+__SWIRC_BEGIN_DECLS
+void	cmd_ban(const char *);
+void	cmd_unban(const char *);
+__SWIRC_END_DECLS
 
 #endif
