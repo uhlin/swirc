282c8e459ffc9a89a02b760ac908ad57e68a7267
Markus Uhlin <markus.uhlin@bredband.net>
2022-12-28 01:09:56 +0100

    Added alignment

--- src/printtext.cpp
+++ src/printtext.cpp
@@ -996,10 +996,10 @@ windows_convert_to_utf8(const char *buf)
 static wchar_t *
 try_convert_buf_with_cs(const char *buf, const char *codeset)
 {
-	char *original_locale = NULL;
-	char *tmp_locale = NULL;
-	struct locale_info *li = NULL;
-	wchar_t *out = NULL;
+	char			*original_locale = NULL;
+	char			*tmp_locale = NULL;
+	struct locale_info	*li = NULL;
+	wchar_t			*out = NULL;
 
 	try {
 		size_t bytes_convert = 0;
