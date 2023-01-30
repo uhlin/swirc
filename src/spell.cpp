762e309682fd699169a390e76924dcdcfdbabe18
Markus Uhlin <markus.uhlin@bredband.net>
2023-01-30 16:22:39 +0100

    Added stuff

--- src/spell.cpp
+++ src/spell.cpp
@@ -1,5 +1,15 @@
 #include "common.h"
+
+#include <hunspell/hunspell.hxx>
+
+#include "config.h"
+#include "nestHome.h"
 #include "spell.h"
+
+const char g_aff_suffix[] = ".aff";
+const char g_dic_suffix[] = ".dic";
+
+static Hunspell *hs = nullptr;
 
 void
 spell_init(void)
@@ -9,4 +19,6 @@ spell_init(void)
 void
 spell_deinit(void)
 {
+	delete hs;
+	hs = nullptr;
 }
