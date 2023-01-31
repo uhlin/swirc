3f118777d719baba60fd42d118d04dc0abde2db5
Markus Uhlin <markus.uhlin@bredband.net>
2023-01-31 16:11:03 +0100

    Switch to usage of the C API

--- src/spell.cpp
+++ src/spell.cpp
@@ -1,6 +1,6 @@
 #include "common.h"
 
-#include <hunspell/hunspell.hxx>
+#include <hunspell/hunspell.h>
 
 #include "config.h"
 #include "nestHome.h"
@@ -9,7 +9,7 @@
 const char g_aff_suffix[] = ".aff";
 const char g_dic_suffix[] = ".dic";
 
-static Hunspell *hs = nullptr;
+static Hunhandle *hh = nullptr;
 
 void
 spell_init(void)
@@ -19,6 +19,7 @@ spell_init(void)
 void
 spell_deinit(void)
 {
-	delete hs;
-	hs = nullptr;
+	if (hh)
+		Hunspell_destroy(hh);
+	hh = nullptr;
 }
