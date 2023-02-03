c9bbc4d675cb91d320cda89319d64e2705d08968
Markus Uhlin <markus.uhlin@bredband.net>
2023-02-03 08:21:32 +0100

    LibreSSL 3.7.0

--- src/get_file.js
+++ src/get_file.js
@@ -17,6 +17,6 @@ get_file("https://curl.haxx.se/ca/cacert.pem", "src/trusted_roots.pem");
 get_file("https://www.nifty-networks.net/swirc/curl-7.87.0.cab", "curl-7.87.0.cab");
 get_file("https://www.nifty-networks.net/swirc/gnu-bundle-202205.cab", "gnu-bundle-202205.cab");
 get_file("https://www.nifty-networks.net/swirc/hunspell-1.7.2.cab", "hunspell-1.7.2.cab");
-get_file("https://www.nifty-networks.net/swirc/libressl-3.6.1-windows.cab", "libressl-3.6.1-windows.cab");
+get_file("https://www.nifty-networks.net/swirc/libressl-3.7.0.cab", "libressl-3.7.0.cab");
 get_file("https://www.nifty-networks.net/swirc/pdcurses-3.9.cab", "pdcurses-3.9.cab");
 get_file("https://www.nifty-networks.net/swirc/swirc-locales-20230106.cab", "swirc-locales-20230106.cab");
