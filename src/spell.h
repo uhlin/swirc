c146b6a207e0c4b9ed5e63553cafba777d293a01
Markus Uhlin <markus.uhlin@bredband.net>
2023-02-04 16:38:59 +0100

    Added alignment

--- src/spell.h
+++ src/spell.h
@@ -33,16 +33,17 @@ void	 spell_init(void);
 void	 spell_deinit(void);
 
 #ifdef __cplusplus
-void spell_destroy_suggs(std::vector<sugg_ptr> *);
-std::vector<sugg_ptr> *spell_get_suggs(const char *, const wchar_t *);
+void	 spell_destroy_suggs(std::vector<sugg_ptr> *);
+std::vector<sugg_ptr> *
+	 spell_get_suggs(const char *, const wchar_t *);
 #endif
 
-void spell_test1(const char *);
-void spell_test2(const wchar_t *);
+void	 spell_test1(const char *);
+void	 spell_test2(const wchar_t *);
 
-bool spell_word(const char *);
-void spell_word_readline(volatile struct readline_session_context *);
-bool spell_wide_word(const wchar_t *);
+bool	 spell_word(const char *);
+void	 spell_word_readline(volatile struct readline_session_context *);
+bool	 spell_wide_word(const wchar_t *);
 __SWIRC_END_DECLS
 
 #endif
