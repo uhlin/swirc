006bb3b73b3237c2b739f9326d7f53feff92e23b
Markus Uhlin <markus.uhlin@bredband.net>
2023-02-06 04:39:41 +0100

    Indent using tabs

--- src/get_file.js
+++ src/get_file.js
@@ -1,16 +1,16 @@
 function get_file(url, name)
 {
-    var http_req = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-    var stream   = new ActiveXObject("ADODB.Stream");
+	var http_req = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+	var stream   = new ActiveXObject("ADODB.Stream");
 
-    http_req.Open("GET", url, false);
-    http_req.Send();
-    http_req.WaitForResponse();
+	http_req.Open("GET", url, false);
+	http_req.Send();
+	http_req.WaitForResponse();
 
-    stream.Type = 1;
-    stream.Open();
-    stream.Write(http_req.ResponseBody);
-    stream.SaveToFile(name);
+	stream.Type = 1;
+	stream.Open();
+	stream.Write(http_req.ResponseBody);
+	stream.SaveToFile(name);
 }
 
 get_file("https://curl.haxx.se/ca/cacert.pem", "src/trusted_roots.pem");
