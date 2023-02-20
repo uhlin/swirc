9f9a2e3513e6167e3c0bfa5f512f768d4571f7cf
Markus Uhlin <markus.uhlin@bredband.net>
2023-02-21 00:19:09 +0100

    Changed the return type

--- src/init_once.h
+++ src/init_once.h
@@ -10,7 +10,7 @@ typedef volatile long int init_once_t;
 typedef void (*PTR_TO_INIT_ROUTINE)(void);
 
 __SWIRC_BEGIN_DECLS
-int	init_once(init_once_t *, PTR_TO_INIT_ROUTINE);
+errno_t init_once(init_once_t *, PTR_TO_INIT_ROUTINE);
 __SWIRC_END_DECLS
 
 #endif
