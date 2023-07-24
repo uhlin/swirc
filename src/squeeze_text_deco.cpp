89f26b30bf94cbc8118810da41f548376990d47f
Markus Uhlin <markus.uhlin@bredband.net>
2023-07-25 00:28:22 +0200

    Casted retvals to Void

--- src/squeeze_text_deco.cpp
+++ src/squeeze_text_deco.cpp
@@ -204,14 +204,14 @@ squeeze_text_deco_wide(wchar_t *buffer)
 	newlen = num = 0;
 
 	try {
-		wstr.assign(buffer);
-		str.assign(wstr.begin(), wstr.end());
+		(void) wstr.assign(buffer);
+		(void) str.assign(wstr.begin(), wstr.end());
 
 		strncpy(str_copy, str.c_str(), sizeof str_copy - 1);
 		str_copy[sizeof str_copy - 1] = '\0';
 
-		str.assign(squeeze_text_deco(str_copy));
-		wstr.assign(str.begin(), str.end());
+		(void) str.assign(squeeze_text_deco(str_copy));
+		(void) wstr.assign(str.begin(), str.end());
 
 		newlen = wstr.size();
 		num = newlen < buflen ? newlen : buflen;
