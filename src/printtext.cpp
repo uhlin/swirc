9e219195baf8f884ad8296a802bbe2fcfc3f1c9e
Markus Uhlin <markus.uhlin@bredband.net>
2023-07-24 21:31:58 +0200

    Bounds checking

--- src/printtext.cpp
+++ src/printtext.cpp
@@ -1533,7 +1533,8 @@ printtext_puts(WINDOW *pwin, CSTRING buf, int indent, int max_lines,
 			    NULL) {
 				static wchar_t str[4096] = { L'\0' };
 
-				diff = (wcp - wc_bufp);
+				if ((diff = (wcp - wc_bufp)) > ARRAY_SIZE(str) - 1)
+					diff = ARRAY_SIZE(str) - 1;
 				sw_assert(diff > 0);
 				(void) wcsncpy(str, wc_bufp, diff);
 				str[diff] = L'\0';
