7d5c60e69714b6cf51db2217c049ecfc7f946c50
Markus Uhlin <markus.uhlin@bredband.net>
2023-08-26 13:54:38 +0200

    Added alignment

--- src/commands/admin.h
+++ src/commands/admin.h
@@ -2,12 +2,12 @@
 #define CMDS_ADMIN_H
 
 __SWIRC_BEGIN_DECLS
-void cmd_die(const char *);
-void cmd_gline(const char *);
-void cmd_kline(const char *);
-void cmd_rehash(const char *);
-void cmd_restart(const char *);
-void cmd_wallops(const char *);
+void	cmd_die(const char *);
+void	cmd_gline(const char *);
+void	cmd_kline(const char *);
+void	cmd_rehash(const char *);
+void	cmd_restart(const char *);
+void	cmd_wallops(const char *);
 __SWIRC_END_DECLS
 
 #endif
