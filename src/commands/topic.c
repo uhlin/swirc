58d65fd5171f28455e5303be0338bec60f2272cc
Markus Uhlin <markus.uhlin@bredband.net>
2023-09-13 23:06:31 +0200

    Changed indentation

--- src/commands/topic.c
+++ src/commands/topic.c
@@ -9,25 +9,28 @@
 
 #include "topic.h"
 
-/* usage: /topic [new topic] */
+/*
+ * usage: /topic [new topic]
+ */
 void
 cmd_topic(const char *data)
 {
-    if (strings_match(data, "") && is_irc_channel(ACTWINLABEL)) {
-	if (g_icb_mode)
-	    icb_send_topic("");
-	else
-	    (void) net_send("TOPIC %s", ACTWINLABEL);
-    } else if (!strings_match(data, "") && is_irc_channel(ACTWINLABEL)) {
-	if (g_icb_mode)
-	    icb_send_topic(data);
-	else
-	    (void) net_send("TOPIC %s :%s", ACTWINLABEL, data);
-    } else {
-	PRINTTEXT_CONTEXT ctx;
+	if (strings_match(data, "") && is_irc_channel(ACTWINLABEL)) {
+		if (g_icb_mode)
+			icb_send_topic("");
+		else
+			(void) net_send("TOPIC %s", ACTWINLABEL);
+	} else if (!strings_match(data, "") && is_irc_channel(ACTWINLABEL)) {
+		if (g_icb_mode)
+			icb_send_topic(data);
+		else
+			(void) net_send("TOPIC %s :%s", ACTWINLABEL, data);
+	} else {
+		PRINTTEXT_CONTEXT ctx;
 
-	printtext_context_init(&ctx, g_active_window, TYPE_SPEC1_FAILURE, true);
-	printtext(&ctx, "/topic: "
-	    "switch to a channel in order to set a new topic for it");
-    }
+		printtext_context_init(&ctx, g_active_window,
+		    TYPE_SPEC1_FAILURE, true);
+		printtext(&ctx, "/topic: switch to a channel in order to set a "
+		    "new topic for it");
+	}
 }
