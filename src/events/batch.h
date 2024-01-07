a7c71a76a09a5ae3308117ad9de423b67ec507b1
Markus Uhlin <markus@nifty-networks.net>
2024-01-07 15:24:12 +0100

    Added 'batch_t'

--- src/events/batch.h
+++ src/events/batch.h
@@ -1,6 +1,14 @@
 #ifndef GUARD_BATCH_H
 #define GUARD_BATCH_H
 
+typedef enum {
+	BATCH_CHATHISTORY,
+	BATCH_NETJOIN,
+	BATCH_NETSPLIT,
+	BATCH_ZNC_IN_PLAYBACK,
+	BATCH_UNKNOWN
+} batch_t;
+
 __SWIRC_BEGIN_DECLS
 void event_batch(struct irc_message_compo *);
 __SWIRC_END_DECLS
