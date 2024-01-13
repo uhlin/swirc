665c25f08282a2142115582c4549898a9fabe5f3
Markus Uhlin <markus@nifty-networks.net>
2024-01-13 12:44:14 +0100

    Fixed checking of a wait condition detected by Coverity

--- src/events/welcome-unix.c
+++ src/events/welcome-unix.c
@@ -1,14 +1,17 @@
-/* Copyright (C) 2014-2022 Markus Uhlin. All rights reserved. */
+/* Copyright (C) 2014-2024 Markus Uhlin. All rights reserved. */
 
 #include "common.h"
 
 #include <sys/time.h>
 
+#include "../assertAPI.h"
 #include "../config.h"
 #include "../errHand.h"
 #include "../pthrMutex.h"
 
 #include "welcome-unix.h"
+
+static bool is_signaled = false;
 
 static pthread_mutex_t	foo_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t	welcome_cond;
@@ -16,7 +19,7 @@ static pthread_cond_t	welcome_cond;
 bool
 event_welcome_is_signaled(void)
 {
-	bool			is_signaled = false; /* initial state */
+	int			ret;
 	struct integer_context	intctx = { 0 };
 	struct timespec		ts = { 0 };
 	struct timeval		tv = { 0 };
@@ -33,8 +36,21 @@ event_welcome_is_signaled(void)
 	ts.tv_nsec = tv.tv_usec;
 
 	mutex_lock(&foo_mutex);
-	if (pthread_cond_timedwait(&welcome_cond, &foo_mutex, &ts) == 0)
-		is_signaled = true;
+	while (!is_signaled) {
+		ret = pthread_cond_timedwait(&welcome_cond, &foo_mutex, &ts);
+
+		if (ret == 0) {
+			is_signaled = true;
+		} else if (ret == EINVAL) {
+			err_exit(EINVAL, "%s: pthread_cond_timedwait",
+			    __func__);
+		} else if (ret == ETIMEDOUT) {
+			is_signaled = false;
+			break;
+		} else {
+			sw_assert_not_reached();
+		}
+	}
 	mutex_unlock(&foo_mutex);
 
 	return is_signaled;
@@ -45,6 +61,7 @@ event_welcome_cond_init(void)
 {
 	if ((errno = pthread_cond_init(&welcome_cond, NULL)) != 0)
 		err_sys("%s: pthread_cond_init", __func__);
+	is_signaled = false;
 }
 
 void
