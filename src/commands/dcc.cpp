a66d02ddd7d561bb2ef86628f2142bf6500b32fa
Markus Uhlin <markus@nifty-networks.net>
2024-03-03 01:27:19 +0100

    Added const

--- src/commands/dcc.cpp
+++ src/commands/dcc.cpp
@@ -677,7 +677,7 @@ dup_check_get(const char *nick, const char *file)
 static void
 dup_check_send(const char *nick, const char *file)
 {
-	std::string str(nick);
+	const std::string str(nick);
 	std::vector<dcc_send>::size_type pos = 0;
 
 	if (!find_send_obj(str, file, pos))
