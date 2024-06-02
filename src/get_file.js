b327016de973da24e0591085d5234722fa89c06e
Markus Uhlin <markus@nifty-networks.net>
2024-06-02 02:15:04 +0200

    LIBRESSL_VERSION: 3.9.2

--- src/get_file.js
+++ src/get_file.js
@@ -18,6 +18,6 @@ get_file("https://www.nifty-networks.net/swirc/curl-8.4.0.cab", "curl-8.4.0.cab"
 get_file("https://www.nifty-networks.net/swirc/gnu-bundle-202205.cab", "gnu-bundle-202205.cab");
 get_file("https://www.nifty-networks.net/swirc/hunspell-1.7.2-dlls.cab", "hunspell-1.7.2.cab");
 get_file("https://www.nifty-networks.net/swirc/hunspell-en-us.cab", "hunspell-en-us.cab");
-get_file("https://www.nifty-networks.net/swirc/libressl-3.8.2.cab", "libressl-3.8.2.cab");
+get_file("https://www.nifty-networks.net/swirc/libressl-3.9.2.cab", "libressl-3.9.2.cab");
 get_file("https://www.nifty-networks.net/swirc/pdcurses-3.9-utf8-colors.cab", "pdcurses-3.9.cab");
 get_file("https://www.nifty-networks.net/swirc/swirc-locales-20240601.cab", "swirc-locales-20240601.cab");
