43142e4639c74751a87a43a17fc5b999a166cf55
Markus Uhlin <markus@nifty-networks.net>
2024-08-22 11:21:47 +0200

    INPUTFILES: added nestHome.c

--- po/common.mk
+++ po/common.mk
@@ -30,6 +30,7 @@ INPUTFILES = $(COMMANDS_DIR)colormap.cpp\
 	$(SRC_DIR)io-loop.c\
 	$(SRC_DIR)log.c\
 	$(SRC_DIR)main.cpp\
+	$(SRC_DIR)nestHome.c\
 	$(SRC_DIR)network.cpp\
 	$(SRC_DIR)readline.c\
 	$(SRC_DIR)sig-w32.c\
