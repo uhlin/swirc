69420aaf849fbf3369d4581aa3ccbcec94ce281c
Markus Uhlin <markus@nifty-networks.net>
2024-12-25 23:28:53 +0100

    Added includes

--- src/commands/ftp.cpp
+++ src/commands/ftp.cpp
@@ -1,5 +1,15 @@
 #include "common.h"
 
+#if __OpenBSD__
+#include <sys/param.h>
+#endif
+
+#include "../config.h"
+#include "../filePred.h"
+#include "../libUtils.h"
+#include "../nestHome.h"
+#include "../strHand.h"
+
 #include "ftp.h"
 
 void
