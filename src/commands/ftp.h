bbcc87dbed03d2daacf2d5d8de8d7bd48852fc71
Markus Uhlin <markus@nifty-networks.net>
2024-12-25 22:28:15 +0100

    Define 'SOCKET'

--- src/commands/ftp.h
+++ src/commands/ftp.h
@@ -1,6 +1,11 @@
 #ifndef SRC_COMMANDS_FTP_H_
 #define SRC_COMMANDS_FTP_H_
 
+#if defined(UNIX) && !defined(_SOCKET_DEFINED)
+#define _SOCKET_DEFINED 1
+typedef int SOCKET;
+#endif
+
 __SWIRC_BEGIN_DECLS
 void	cmd_ftp(CSTRING);
 __SWIRC_END_DECLS
