4129f6a9c279e24882f88e7cfe3de182a4515c6a
Markus Uhlin <markus@nifty-networks.net>
2025-01-01 00:16:41 +0100

    Added a copyright year

--- src/commands/ftp.cpp
+++ src/commands/ftp.cpp
@@ -1,5 +1,5 @@
 /* The FTP command
-   Copyright (C) 2024 Markus Uhlin. All rights reserved.
+   Copyright (C) 2024, 2025 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
