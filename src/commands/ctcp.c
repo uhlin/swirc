caadd503720da9a8b0ded164ab16254e5575eed0
Markus Uhlin <markus@nifty-networks.net>
2025-01-22 21:42:34 +0100

    Added includes

--- src/commands/ctcp.c
+++ src/commands/ctcp.c
@@ -1,6 +1,13 @@
 #include "common.h"
 
+#include "../dataClassify.h"
+#include "../irc.h"
+#include "../main.h"
+#include "../printtext.h"
+#include "../strHand.h"
+
 #include "ctcp.h"
+#include "i18n.h"
 
 void
 cmd_ctcp(CSTRING data)
