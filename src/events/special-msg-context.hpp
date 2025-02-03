d891e1b17aa55fe8e4d739db0a7868968a9edbd6
Markus Uhlin <markus@nifty-networks.net>
2025-02-03 19:49:34 +0100

    Changed variable type

--- src/events/special-msg-context.hpp
+++ src/events/special-msg-context.hpp
@@ -2,11 +2,9 @@
 #define SPECIAL_MSG_CONTEXT_HPP
 
 struct special_msg_context {
-	char	*nick;
-	char	*user;
-	char	*host;
-	char	*dest;
-	char	*msg;
+	CSTRING nick, user, host;
+	CSTRING dest;
+	CSTRING msg;
 
 	special_msg_context() : nick(NULL)
 	    , user(NULL)
@@ -17,8 +15,8 @@ struct special_msg_context {
 		/* empty */;
 	}
 
-	special_msg_context(char *p_nick, char *p_user, char *p_host,
-	    char *p_dest, char *p_msg) : nick(p_nick)
+	special_msg_context(CSTRING p_nick, CSTRING p_user, CSTRING p_host,
+	    CSTRING p_dest, CSTRING p_msg) : nick(p_nick)
 	    , user(p_user)
 	    , host(p_host)
 	    , dest(p_dest)
