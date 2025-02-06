becf6a89752b49544adbe196aed275eb66e6c30f
Markus Uhlin <markus@nifty-networks.net>
2025-02-06 18:28:02 +0100

    struct notice_context: changed member types

--- src/events/notice.cpp
+++ src/events/notice.cpp
@@ -51,9 +51,9 @@
 #define NCOLOR2		Theme("notice_color2")
 
 struct notice_context {
-	char	*srv_name;
-	char	*dest;
-	char	*msg;
+	CSTRING srv_name;
+	CSTRING dest;
+	CSTRING msg;
 
 	notice_context()
 	    : srv_name(NULL)
