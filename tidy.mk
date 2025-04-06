48ac01690028b0745c460e9a2619cf40618ce63d
Markus Uhlin <markus@nifty-networks.net>
2025-04-06 11:38:29 +0200

    Added tidy.mk

---
+++ tidy.mk
@@ -0,0 +1,9 @@
+# The 'tidy' target
+
+TIDY = clang-tidy
+TIDYFLAGS = -checks=-clang-analyzer-security.insecureAPI.strcpy,-clang-analyzer-optin.performance.Padding,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling -quiet
+SW_CLANG_TIDYFLAGS ?=
+
+tidy: $(SRC_DIR)include/swircpaths.h
+	$(TIDY) $(COMMANDS_SRCS) $(EVENTS_SRCS) $(SRCS) $(TIDYFLAGS) \
+	$(SW_CLANG_TIDYFLAGS) $(TIDYFLAGS) -- $(CPPFLAGS)
