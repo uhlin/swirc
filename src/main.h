b48042379170b48d63e7e4f69c7bd65c2ce658cc
Markus Uhlin <markus@nifty-networks.net>
2025-04-23 16:32:57 +0200

    Sorted the typedefs

--- src/main.h
+++ src/main.h
@@ -61,8 +61,8 @@ struct cmdline_opt_values {
 };
 
 typedef char *(*SETLOCALE_FN)(int, const char *);
-typedef const char chararray_t[];
 typedef const char *stringarray_t[];
+typedef const char chararray_t[];
 typedef int (*SSCANF_FN)(const char *, const char *, ...);
 
 /*lint -sem(xsetlocale, r_null) */
