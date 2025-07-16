73af7d4a6eaa577994c2dd277765f394470417c4
Markus Uhlin <markus@nifty-networks.net>
2025-07-16 21:00:55 +0200

    Updated the copyright years

--- src/network.cpp
+++ src/network.cpp
@@ -1,5 +1,5 @@
 /* Platform independent networking routines
-   Copyright (C) 2014-2024 Markus Uhlin. All rights reserved.
+   Copyright (C) 2014-2025 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
