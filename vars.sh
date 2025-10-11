19ea93b2702f94f56c348bf7fda5bab71295fa8d
Markus Uhlin <markus@nifty-networks.net>
2025-10-11 11:33:03 +0200

    UPSTREAM_VER: 3.5.6

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 BUILD_DIR=build
 DEB_VERSION=`dpkg-parsechangelog --show-field Version`
 RELEASES_URL="https://www.nifty-networks.net/swirc/releases/"
-UPSTREAM_VER=3.5.5
+UPSTREAM_VER=3.5.6
 VERSION=`dpkg-parsechangelog --show-field Version | cut -f1 -d'-'`
