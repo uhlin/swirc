5ac550923aefa3568ef43b10a78627b2e94b28a8
Markus Uhlin <markus@nifty-networks.net>
2025-12-26 16:20:07 +0100

    New script

---
+++ runpvs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+make clean
+./configure --with-libnotify
+pvs-studio-analyzer trace -- make
+pvs-studio-analyzer analyze -a "GA;64;OP;CS;OWASP" --security-related-issues --intermodular
+plog-converter -o tmp -a "GA:1,2;64:1;OP:1,2,3;CS:1;OWASP:1" -t fullhtml PVS-Studio.log
