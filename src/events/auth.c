302e94ef732cd8af85ef9d50b33f1db6226cef07
Markus Uhlin <markus@nifty-networks.net>
2025-12-30 23:09:07 +0100

    Changed cast

--- src/events/auth.c
+++ src/events/auth.c
@@ -69,8 +69,8 @@ get_b64_encoded_username(void)
 	encoded_username = xmalloc(namesize + 1);
 	encoded_username[namesize] = '\0';
 
-	if (b64_encode((uint8_t *)username, strlen(username), encoded_username,
-	    namesize) == -1) {
+	if (b64_encode((uint8_t const *)username, strlen(username),
+	    encoded_username, namesize) == -1) {
 		free(encoded_username);
 		return NULL;
 	}
