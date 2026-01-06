afbd4a9fc750f449bd7403a6d46aaff40aecf4af
Markus Uhlin <markus@nifty-networks.net>
2026-01-06 20:10:14 +0100

    Added alignment

--- src/commands/log.h
+++ src/commands/log.h
@@ -6,8 +6,8 @@
 //lint -sem(get_list_of_matching_log_cmds, r_null)
 
 __SWIRC_BEGIN_DECLS
-void cmd_log(CSTRING);
-PTEXTBUF get_list_of_matching_log_cmds(CSTRING) NONNULL;
+void		cmd_log(CSTRING);
+PTEXTBUF	get_list_of_matching_log_cmds(CSTRING) NONNULL;
 __SWIRC_END_DECLS
 
 #endif
