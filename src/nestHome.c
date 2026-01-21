7ec1f950d1836af3eecf4094fac14d5dff52837d
Markus Uhlin <markus@nifty-networks.net>
2026-01-21 02:50:55 +0100

    prompt_for_decryption: discard return values  --  writing to and flushing stdout should be safe

--- src/nestHome.c
+++ src/nestHome.c
@@ -1,5 +1,5 @@
 /* Create the home directory and read its configuration files
-   Copyright (C) 2012-2025 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2026 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -183,8 +183,8 @@ prompt_for_decryption(const char *str)
 		char	 pass[400] = { '\0' };
 		int	 errno_save;
 
-		fputs(_("Password: "), stdout);
-		fflush(stdout);
+		(void) fputs(_("Password: "), stdout);
+		(void) fflush(stdout);
 
 		term_toggle_echo(OFF);
 		errno = 0;
@@ -192,7 +192,7 @@ prompt_for_decryption(const char *str)
 		errno_save = errno;
 		term_toggle_echo(ON);
 
-		putchar('\n');
+		(void) putchar('\n');
 
 		if (fgets_error) {
 			err_exit(errno_save, "%s: fgets", __func__);
