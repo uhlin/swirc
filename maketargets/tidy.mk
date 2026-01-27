b527d4fdc874286746272d57ca400c5f0ec115db
Markus Uhlin <markus@nifty-networks.net>
2026-01-27 18:27:52 +0100

    Did put checks into a variable

--- maketargets/tidy.mk
+++ maketargets/tidy.mk
@@ -1,7 +1,20 @@
 # The 'tidy' target
 
+CHKS = cert-*,$\
+	-clang-analyzer-security.insecureAPI.strcpy,$\
+	-clang-analyzer-optin.performance.Padding,$\
+	-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,$\
+	hicpp-deprecated-headers,$\
+	hicpp-member-init,$\
+	hicpp-multiway-paths-covered,$\
+	hicpp-special-member-functions,$\
+	hicpp-use-auto,$\
+	hicpp-use-emplace,$\
+	performance-*,$\
+	portability-*
+
 TIDY = clang-tidy
-TIDYFLAGS = -checks=cert-*,-clang-analyzer-security.insecureAPI.strcpy,-clang-analyzer-optin.performance.Padding,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,performance-*,portability-* -quiet
+TIDYFLAGS = -checks=$(CHKS) -quiet
 SW_CLANG_TIDYFLAGS ?=
 
 tidy: $(SRC_DIR)include/swircpaths.h
