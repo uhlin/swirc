14b07a2718d8c8efa0a5b3efba4588929c1ea112
Markus Uhlin <markus@nifty-networks.net>
2026-02-17 19:35:53 +0100

    titlebar_recreate: made 'newsize' const

--- src/titlebar.c
+++ src/titlebar.c
@@ -1,5 +1,5 @@
 /* Swirc titlebar
-   Copyright (C) 2012-2025 Markus Uhlin. All rights reserved.
+   Copyright (C) 2012-2026 Markus Uhlin. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
@@ -110,7 +110,7 @@ titlebar(const char *fmt, ...)
 void
 titlebar_recreate(int cols)
 {
-	struct term_window_size newsize = {
+	const struct term_window_size newsize = {
 		.rows = 1,
 		.cols = cols,
 		.start_row = 0,
